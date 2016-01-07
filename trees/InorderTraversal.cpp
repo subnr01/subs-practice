c17780205c545b039da74df523064b3d485c2db0
Subbu <subnr01@gmail.com>
2016-01-07 06:31:08 -0500

    Update InorderTraversal.cpp

--- trees/InorderTraversal.cpp
+++ trees/InorderTraversal.cpp
@@ -15,3 +15,33 @@ return [1,3,2].
 Note: Recursive solution is trivial, could you do it iteratively?
 
 */
+
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        TreeNode *curr = NULL;
+        stack<TreeNode*> s;
+        vector<int> inorder;
+        curr = root;
+        
+        /* 
+         * Need to check stack is not empty
+         * as  if the input is only
+         * root, then current will be null
+         * but stack will have one element
+         * that will not be traversed.
+         */
+        while (curr || !s.empty()) {
+            if(curr) {
+                s.push(curr);
+                curr = curr->left;
+            } else {
+                inorder.push_back(s.top()->val);
+                curr = s.top()->right;
+                s.pop();
+            }
+        }
+        return inorder;
+    }
+};
