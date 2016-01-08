820a8537884d016a1edd4eb2c6f33526f66b98f5
Subbu <subnr01@gmail.com>
2016-01-08 06:44:24 -0500

    Create minDepth.cpp

---
+++ trees/minDepth.cpp
@@ -0,0 +1,61 @@
+/*
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+
+*/
+
+
+class Solution {
+public:
+    int minDepth_1(TreeNode* root) {
+        
+        int mindepth = 0;
+        minDepth(root, mindepth, 0);
+        return mindepth;
+    }
+    
+    void minDepth(TreeNode *root, int &mindepth, int depth) {
+        
+        if(!root) {
+            return;
+        }
+        
+        depth+=1;
+        
+        if(!root->left && !root->right) {
+            if (mindepth == 0) {
+                mindepth = depth;
+            } else {
+                mindepth = min(mindepth, depth);
+            }
+            return;
+        }
+        
+        minDepth(root->left, mindepth, depth);
+        minDepth(root->right, mindepth, depth);
+        
+    }
+    
+    int minDepth(TreeNode *root) {
+        if (!root) return 0;
+        queue<TreeNode *> q;
+        q.push(root);
+        q.push(NULL);
+        int depth = 1;
+        while (true)
+        {
+            TreeNode *node = q.front();
+            q.pop();
+            if (!node) {
+                depth++;
+                q.push(NULL);
+            } else {
+                if (!node->left && !node->right) return depth;
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+        }
+    }
+};
