99a254e4fe293f4907f59ef5dbb5a5a1d37306c1
Subbu <subnr01@gmail.com>
2016-01-10 17:13:18 -0500

    Update majorityElement.cpp

--- arrays/majorityElement.cpp
+++ arrays/majorityElement.cpp
@@ -9,3 +9,20 @@ majority element always exist in the array.
 */
 
 
+class Solution {
+public:
+    int majorityElement(vector<int>& num) {
+        int major=num[0], count = 1;
+        for(int i=1; i<num.size();i++){
+            if(count==0){
+                count++;
+                major=num[i];
+            }else if(major==num[i]){
+                count++;
+            }else count--;
+
+        }
+        return major;
+        
+    }
+};
