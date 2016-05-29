a1fed7477403f413e58db2184bfa262e642ffe66
Subbu <subnr01@gmail.com>
2016-05-29 18:28:42 -0400

    Create isomorphic_strings.cpp

---
+++ hashmap/isomorphic_strings.cpp
@@ -0,0 +1,57 @@
+/*
+
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character but a character may map to itself.
+
+For example,
+Given "egg", "add", return true.
+
+Given "foo", "bar", return false.
+
+Given "paper", "title", return true.
+
+
+
+*/
+
+/*
+The solution can also be done AS
+bool isIsomorphic(char* s, char* t) {
+    char charArrS[256] = { 0 };
+    char charArrT[256] = { 0 };
+    int i = 0;
+    while (s[i] !=0)
+    {
+        if (charArrS[s[i]] == 0 && charArrT[t[i]] == 0)
+        {
+            charArrS[s[i]] = t[i];
+            charArrT[t[i]] = s[i];
+        }
+        else
+        if (charArrS[s[i]] != t[i] || charArrT[t[i]] != s[i])
+            return false;
+        i++;
+    }
+
+    return true;
+}
+*/
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        char map_s[128] = { 0 };
+        char map_t[128] = { 0 };
+        int len = s.size();
+        for (int i = 0; i < len; ++i)
+        {
+            if (map_s[s[i]]!=map_t[t[i]]) return false;
+            map_s[s[i]] = i+1;
+            map_t[t[i]] = i+1;
+        }
+        return true;    
+    }
+};
