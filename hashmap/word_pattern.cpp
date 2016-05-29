7c8c53187d64e8dd116792767c8e76c7af410c78
Subbu <subnr01@gmail.com>
2016-05-29 08:45:21 -0400

    Create word_pattern.cpp

---
+++ hashmap/word_pattern.cpp
@@ -0,0 +1,26 @@
+/*
+Given a pattern and a string str, find if str follows the same pattern.
+
+Examples:
+pattern = "abba", str = "dog cat cat dog" should return true.
+pattern = "abba", str = "dog cat cat fish" should return false.
+pattern = "aaaa", str = "dog cat cat dog" should return false.
+pattern = "abba", str = "dog dog dog dog" should return false.
+
+*/
+
+class Solution {
+public:
+   bool wordPattern(string pattern, string str) {
+    map<char, int> p2i;
+    map<string, int> w2i;
+    istringstream in(str);
+    int i = 0, n = pattern.size();
+    for (string word; in >> word; ++i) {
+        if (i == n || p2i[pattern[i]] != w2i[word])
+            return false;
+        p2i[pattern[i]] = w2i[word] = i + 1;
+    }
+    return i == n;
+}
+};
