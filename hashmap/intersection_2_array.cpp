aed3a763785f529dc39e7959e6250c5a6e785f8e
Subbu <subnr01@gmail.com>
2016-05-29 07:43:08 -0400

    Create intersection_2_array.cpp

---
+++ hashmap/intersection_2_array.cpp
@@ -0,0 +1,33 @@
+/*
+Given two arrays, write a function to compute their intersection.
+
+Example:
+Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
+
+*/
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+
+        unordered_map<int,int> map;
+        vector<int> result;
+
+        for(int i=0;i<nums1.size();i++)
+        {
+            map[nums1[i]]++;
+        }
+
+        for(int i=0;i<nums2.size();i++)
+        {
+            if(map[nums2[i]] > 0)
+            {
+                result.push_back(nums2[i]);
+                map[nums2[i]] = 0;
+            }
+        }
+
+        return result;
+
+    }
+};
