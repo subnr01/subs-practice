39efc9bd2e7064735ebb08631ac8b031c6aa75a2
Subbu <subnr01@gmail.com>
2016-09-27 21:08:10 -0400

    Create time_buy_sell_stock.cpp

---
+++ arrays/time_buy_sell_stock.cpp
@@ -0,0 +1,27 @@
+/*
+
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Example 1:
+Input: [7, 1, 5, 3, 6, 4]
+Output: 5
+
+max. difference = 6-1 = 5 (not 7-1 = 6, as selling price needs to be larger than buying price)
+
+
+*/
+
+class Solution {
+public:
+  int maxProfit(vector<int> &prices) {
+    int maxPro = 0;
+    int minPrice = INT_MAX;
+    for(int i = 0; i < prices.size(); i++){
+        minPrice = min(minPrice, prices[i]);
+        maxPro = max(maxPro, prices[i] - minPrice);
+    }
+    return maxPro;
+}
+};
