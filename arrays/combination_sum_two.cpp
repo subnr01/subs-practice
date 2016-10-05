956f8585b30e0a24a5b50db23975555bfeaf6375
Subbu <subnr01@gmail.com>
2016-10-05 18:31:03 -0400

    Create combination_sum_two.cpp

---
+++ arrays/combination_sum_two.cpp
@@ -0,0 +1,47 @@
+/*
+
+
+Given a collection of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+
+Each number in C may only be used once in the combination.
+
+Note:
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+For example, given candidate set [10, 1, 2, 7, 6, 1, 5] and target 8, 
+A solution set is: 
+[
+  [1, 7],
+  [1, 2, 5],
+  [2, 6],
+  [1, 1, 6]
+]
+*/
+
+
+class Solution {
+public:
+    std::vector<std::vector<int> > combinationSum2(std::vector<int> &candidates, int target) {
+		std::sort(candidates.begin(), candidates.end());
+        std::vector<std::vector<int> > res;
+        std::vector<int> combination;
+        combinationSum2(candidates, target, res, combination, 0);
+        return res;
+    }
+private:
+    void combinationSum2(std::vector<int> &candidates, int target, std::vector<std::vector<int> > &res, std::vector<int> &combination, int begin) {
+		if  (!target) {
+			res.push_back(combination);
+			return;
+		}
+		int i;
+		
+        for (i = begin; i != candidates.size() && target >= candidates[i]; ++i) {
+            if (i == begin ||  candidates[i] != candidates[i - 1]) {
+                combination.push_back(candidates[i]);
+                combinationSum2(candidates, target - candidates[i], res, combination, i + 1);
+                combination.pop_back();
+            }
+        }
+    }
+};
