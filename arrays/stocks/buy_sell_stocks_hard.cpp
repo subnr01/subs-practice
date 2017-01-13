7d45d1b3a146af1db4d9737d09601644221182cd
Subbu <subnr01@gmail.com>
2017-01-13 07:16:43 -0500

    Update buy_sell_stocks_hard.cpp

--- arrays/stocks/buy_sell_stocks_hard.cpp
+++ arrays/stocks/buy_sell_stocks_hard.cpp
@@ -54,17 +54,61 @@ public:
 
 
 
-/* Java solution */
-public class Solution {
-    public int maxProfit(int[] prices) {
-        int hold1 = Integer.MIN_VALUE, hold2 = Integer.MIN_VALUE;
-        int release1 = 0, release2 = 0;
-        for(int i:prices){                              // Assume we only have 0 money at first
-            release2 = Math.max(release2, hold2+i);     // The maximum if we've just sold 2nd stock so far.
-            hold2    = Math.max(hold2,    release1-i);  // The maximum if we've just buy  2nd stock so far.
-            release1 = Math.max(release1, hold1+i);     // The maximum if we've just sold 1nd stock so far.
-            hold1    = Math.max(hold1,    -i);          // The maximum if we've just buy  1st stock so far. 
-        }
-        return release2; ///Since release1 is initiated as 0, so release2 will always higher than release1.
-    }
+/* Java solution -- program creek*/
+public int maxProfit(int[] prices) {
+	if (prices == null || prices.length < 2) {
+		return 0;
+	}
+ 
+	//highest profit in 0 ... i
+	int[] left = new int[prices.length];
+	int[] right = new int[prices.length];
+ 
+	// DP from left to right
+	left[0] = 0; 
+	int min = prices[0];
+	for (int i = 1; i < prices.length; i++) {
+		min = Math.min(min, prices[i]);
+		left[i] = Math.max(left[i - 1], prices[i] - min);
+	}
+ 
+	// DP from right to left
+	right[prices.length - 1] = 0;
+	int max = prices[prices.length - 1];
+	for (int i = prices.length - 2; i >= 0; i--) {
+		max = Math.max(max, prices[i]);
+		right[i] = Math.max(right[i + 1], max - prices[i]);
+	}
+ 
+	int profit = 0;
+	for (int i = 0; i < prices.length; i++) {
+		profit = Math.max(profit, left[i] + right[i]);
+	}
+ 
+	return profit;
+}
+
+
+
+/* Program creek at most k transactions */
+public int maxProfit(int k, int[] prices) {
+	if (prices.length < 2 || k <= 0)
+		return 0;
+ 
+	//pass leetcode online judge (can be ignored)
+	if (k == 1000000000)
+		return 1648961;
+ 
+	int[] local = new int[k + 1];
+	int[] global = new int[k + 1];
+ 
+	for (int i = 0; i < prices.length - 1; i++) {
+		int diff = prices[i + 1] - prices[i];
+		for (int j = k; j >= 1; j--) {
+			local[j] = Math.max(global[j - 1] + Math.max(diff, 0), local[j] + diff);
+			global[j] = Math.max(local[j], global[j]);
+		}
+	}
+ 
+	return global[k];
 }
