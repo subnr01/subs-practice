b611704e6dabe6643c595bee30e5c372628550d2
Subbu <subnr01@gmail.com>
2017-01-17 09:28:21 -0500

    Update min_rotated_sorted_array_med.cpp

--- arrays/rotated_array_search/min_rotated_sorted_array_med.cpp
+++ arrays/rotated_array_search/min_rotated_sorted_array_med.cpp
@@ -8,3 +8,30 @@ Find the minimum element.
 
 You may assume no duplicate exists in the array.
 */
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        
+    int low = 0;
+    int high = nums.size()-1;
+    
+    while (low < high)
+    {
+        if (nums[low] < nums[high]) {
+            return nums[low];
+        }
+        
+        int mid = (low + high)/2;
+        
+        if (nums[low] > nums[mid])
+        {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+    
+    return nums[low];    
+    }
+};
