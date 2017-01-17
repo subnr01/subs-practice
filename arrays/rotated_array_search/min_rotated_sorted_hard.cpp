46c56963c5790453848cc428c74178a7cd05f625
Subbu <subnr01@gmail.com>
2017-01-17 09:52:03 -0500

    Update min_rotated_sorted_hard.cpp

--- arrays/rotated_array_search/min_rotated_sorted_hard.cpp
+++ arrays/rotated_array_search/min_rotated_sorted_hard.cpp
@@ -12,3 +12,34 @@ Find the minimum element.
 
 
 */
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        
+    int low = 0;
+    int high = nums.size()-1;
+    
+    while (low < high && nums[low] == nums[high]) {
+            high -= 1;  // or left += 1; if index is not asked
+    }
+    
+    while (low < high)
+    {
+        if (nums[low] < nums[high]) {
+            return nums[low];
+        }
+        
+        int mid = (low + high)/2;
+        
+        if (nums[low] > nums[mid])
+        {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+    
+    return nums[low];    
+    }
+};
