1921d279759049895a771e8f0615f5d89d4d20dd
Subbu <subnr01@gmail.com>
2017-01-28 09:43:21 -0500

    Create inorder_postorder.cpp

---
+++ arrays/create_tree/inorder_postorder.cpp
@@ -0,0 +1,38 @@
+/*
+
+Given inorder and postorder traversal of a tree, construct the binary tree.
+
+*/
+
+class Solution {
+public:
+   TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
+    int post_order = postorder.size() - 1;
+    return create(inorder, postorder, 0, inorder.size() - 1, &post_order);
+}
+
+TreeNode* create(vector<int> &inorder, vector<int> &postorder, int is, int ie, int *pe){
+    
+    if(is > ie) {
+        return nullptr;
+    }
+    TreeNode* node = new TreeNode(postorder[*pe]);
+    (*pe)--;
+    
+    if (is == ie) {
+        return node;
+    }
+    
+    int pos;
+    for(int i = is; i <= ie; i++){
+        if(inorder[i] == node->val){
+            pos = i;
+            break;
+        }
+    }
+    node->right = create(inorder, postorder, pos + 1, ie, pe);
+    node->left = create(inorder, postorder, is, pos - 1, pe);
+    
+    return node;
+}
+};
