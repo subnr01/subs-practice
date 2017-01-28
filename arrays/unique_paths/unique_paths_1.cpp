592454009670f0d2336ba92bee0eb6c797f01523
Subbu <subnr01@gmail.com>
2017-01-28 13:41:52 -0500

    Update unique_paths_1.cpp

--- arrays/unique_paths/unique_paths_1.cpp
+++ arrays/unique_paths/unique_paths_1.cpp
@@ -40,28 +40,24 @@ public int dfs(int i, int j, int m, int n){
 Using dynamic programming
 */
 
-public int uniquePaths(int m, int n) {
-    if(m==0 || n==0) return 0;
-    if(m==1 || n==1) return 1;
- 
-    int[][] dp = new int[m][n];
- 
-    //left column
-    for(int i=0; i<m; i++){
-        dp[i][0] = 1;
-    }
- 
-    //top row
-    for(int j=0; j<n; j++){
-        dp[0][j] = 1;
-    }
- 
-    //fill up the dp table
-    for(int i=1; i<m; i++){
-        for(int j=1; j<n; j++){
-            dp[i][j] = dp[i-1][j] + dp[i][j-1];
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int a[m][n];
+        
+        for(int i=0; i<m; i++) {
+            a[i][0] = 1;
         }
+        
+        for(int i=0; i<n; i++) {
+            a[0][i] = 1;
+        }
+        
+        for (int i=1; i<m; i++) {
+            for(int j=1; j<n; j++) {
+                a[i][j]=a[i-1][j] + a[i][j-1];
+            }
+        }
+        return a[m-1][n-1];
     }
- 
-    return dp[m-1][n-1];
-}
+};
