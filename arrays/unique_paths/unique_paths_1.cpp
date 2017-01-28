16044eafc44e528ef675870715f6b19382fb4074
Subbu <subnr01@gmail.com>
2017-01-28 13:51:45 -0500

    Update unique_paths_1.cpp

--- arrays/unique_paths/unique_paths_1.cpp
+++ arrays/unique_paths/unique_paths_1.cpp
@@ -37,6 +37,21 @@ public int dfs(int i, int j, int m, int n){
 }
 
 /*
+Using dynamic programming (one array)
+*/
+class Solution {
+    public:
+    int uniquePaths(int m, int n) {
+        if (m > n) return uniquePaths(n, m);
+        vector<int> cur(m, 1);
+        for (int j = 1; j < n; j++)
+            for (int i = 1; i < m; i++)
+                cur[i] += cur[i - 1]; 
+        return cur[m - 1];
+    }
+}; 
+
+/*
 Using dynamic programming
 */
 
