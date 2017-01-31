4df811068b027bc9e0fc24726c20f91491548873
Subbu <subnr01@gmail.com>
2017-01-30 21:24:15 -0500

    Update subsets_dup.cpp

--- backtracking/subsets_dup.cpp
+++ backtracking/subsets_dup.cpp
@@ -64,3 +64,24 @@ private:
 	}
 };
 
+
+//Subsets without duplicates
+class Solution {
+public:
+    std::vector<std::vector<int> > subsets(std::vector<int> &nums) {
+		std::sort(nums.begin(), nums.end());
+        std::vector<std::vector<int> > res;
+		std::vector<int> vec;
+		subsets(res, nums, vec, 0);
+		return res;
+    }
+private:
+	void subsets(std::vector<std::vector<int> > &res, std::vector<int> &nums, std::vector<int> &vec, int begin) {
+		res.push_back(vec);
+		for (int i = begin; i != nums.size(); ++i) {
+			vec.push_back(nums[i]);
+			subsets(res, nums, vec, i + 1);
+			vec.pop_back();
+		}
+	}
+};
