5d5085fda27dc23904f5322e4f24d71522d4504b
Subbu <subnr01@gmail.com>
2017-02-01 12:47:11 -0500

    Update peeking_iterator.cpp

--- Design/peeking_iterator.cpp
+++ Design/peeking_iterator.cpp
@@ -11,7 +11,71 @@ You call next() the final time and it returns 3, the last element. Calling hasNe
 
 */
 
+// Below is the interface for Iterator, which is already defined for you.
+// **DO NOT** modify the interface for Iterator.
+class Iterator {
+    struct Data;
+	Data* data;
+public:
+	Iterator(const vector<int>& nums);
+	Iterator(const Iterator& iter);
+	virtual ~Iterator();
+	// Returns the next element in the iteration.
+	int next();
+	// Returns true if the iteration has more elements.
+	bool hasNext() const;
+};
 
+/*
+class PeekingIterator : public Iterator {
+private:
+    int m_next;
+    bool m_hasnext;
+public:
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+	    m_hasnext = Iterator::hasNext();
+	    if (m_hasnext) m_next = Iterator::next();
+	}
+
+	int peek() {
+        return m_next;
+	}
+
+	int next() {
+	    int t = m_next;
+	    m_hasnext = Iterator::hasNext();
+	    if (m_hasnext) m_next = Iterator::next();
+	    return t;
+	}
+
+	bool hasNext() const {
+	    return m_hasnext;
+	}
+};
+*/
+
+class PeekingIterator : public Iterator
+{
+public:
+    PeekingIterator(const vector<int> &nums) : Iterator(nums)
+    {
+    }
+
+    int peek()
+    {
+        return Iterator(*this).next();
+    }
+
+    int next()
+    {
+        return Iterator::next();
+    }
+
+    bool hasNext() const
+    {
+        return Iterator::hasNext();
+    }
+};
 
 
 
