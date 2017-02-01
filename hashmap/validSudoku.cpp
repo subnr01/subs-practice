289f9164544272305fd5a55ce36f7d641ded7a72
Subbu <subnr01@gmail.com>
2017-02-01 08:44:18 -0500

    Create validSudoku.cpp

---
+++ hashmap/validSudoku.cpp
@@ -0,0 +1,24 @@
+/*
+Determine if a Sudoku is valid, according to: Sudoku Puzzles - The Rules.
+
+*/
+
+
+
+class Solution {
+public:
+
+    bool isValidSudoku(vector<vector<char>>& board) {
+        bool row[9][9] = {false}, col[9][9] = {false}, box[9][9] = {false};
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '0' - 1, k = i / 3 * 3 + j / 3;
+                    if (row[i][num] || col[j][num] || box[k][num]) return false;
+                    row[i][num] = col[j][num] = box[k][num] = true;
+                }
+            }
+        }
+        return true;
+    }
+};
