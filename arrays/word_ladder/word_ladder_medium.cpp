f5c54380fde479076eb554bbd10a687f269dbe96
Subbu <subnr01@gmail.com>
2017-01-31 22:05:39 -0500

    Create word_ladder_medium.cpp

---
+++ arrays/word_ladder/word_ladder_medium.cpp
@@ -0,0 +1,65 @@
+/*
+
+Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest transformation sequence from beginWord to endWord, such that:
+
+Only one letter can be changed at a time.
+Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
+For example,
+
+Given:
+beginWord = "hit"
+endWord = "cog"
+wordList = ["hot","dot","dog","lot","log","cog"]
+As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+return its length 5
+
+*/
+
+
+
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        
+        unordered_set<string> wordDict(wordList.begin(), wordList.end());
+        if (wordDict.find(endWord) == wordDict.end()) {
+            return 0;
+        }
+  
+        unordered_set<string> head, tail, *phead, *ptail;
+        head.insert(beginWord);
+        tail.insert(endWord);
+        int dist = 2;
+        while (!head.empty() && !tail.empty()) {
+            if (head.size() < tail.size()) {
+                phead = &head;
+                ptail = &tail;
+            }
+            else {
+                phead = &tail; 
+                ptail = &head;
+            }
+            unordered_set<string> temp; 
+            for (auto itr = phead -> begin(); itr != phead -> end(); itr++) {
+                string word = *itr;
+                wordDict.erase(word);
+                for (int p = 0; p < (int)word.length(); p++) {
+                    char letter = word[p];
+                    for (int k = 0; k < 26; k++) {
+                        word[p] = 'a' + k;
+                        if (ptail -> find(word) != ptail -> end())
+                            return dist;
+                        if (wordDict.find(word) != wordDict.end()) {
+                            temp.insert(word);
+                            wordDict.erase(word);
+                        }
+                    }
+                    word[p] = letter;
+                }
+            }
+            dist++;
+            swap(*phead, temp);
+        }
+        return 0; 
+    }
+};
