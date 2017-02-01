cbedca0ccc72f55c148aa5fbff74e04a384a83f0
Subbu <subnr01@gmail.com>
2017-02-01 10:27:26 -0500

    Update peeking_iterator.cpp

--- Design/peeking_iterator.cpp
+++ Design/peeking_iterator.cpp
@@ -13,25 +13,5 @@ You call next() the final time and it returns 3, the last element. Calling hasNe
 
 
 
-//Normal approach
 
-class Solution {
-public:
-   bool searchMatrix(vector<vector<int>>& matrix, int target) {
-    int m = matrix.size();
-    if (m == 0) return false;
-    int n = matrix[0].size();
-
-    int i = 0, j = n - 1;
-    while (i < m && j >= 0) {
-        if (matrix[i][j] == target)
-            return true;
-        else if (matrix[i][j] > target) {
-            j--;
-        } else 
-            i++;
-    }
-    return false;
-}
-};
 
