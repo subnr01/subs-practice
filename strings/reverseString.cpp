0df4bdef654cd8f171f464016f7d3de5e82fded7
Subbu <subnr01@gmail.com>
2017-02-03 18:59:22 -0500

    Create reverseString.cpp

---
+++ strings/reverseString.cpp
@@ -0,0 +1,17 @@
+/*
+
+Reverse a string
+
+*/
+
+
+class Solution {
+public:
+    string reverseString(string s) {
+        int n = s.size();
+        for(int i = 0; i < n/2; i++) {
+            swap(s[i], s[(n - 1) - i]);
+        }
+        return s;
+    }
+};
