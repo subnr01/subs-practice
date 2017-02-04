5ca28f2bef657e088c87d92ec36b8910816c5660
Subbu <subnr01@gmail.com>
2017-02-04 00:46:17 -0500

    Create longest_common_prefix.cpp

---
+++ strings/longest_common_prefix.cpp
@@ -0,0 +1,16 @@
+/*
+Write a function to find the longest common prefix string amongst an array of strings.
+
+*/
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string prefix = "";
+        for(int idx=0; strs.size()>0; prefix+=strs[0][idx], idx++)
+            for(int i=0; i<strs.size(); i++)
+                if(idx >= strs[i].size() ||(i > 0 && strs[i][idx] != strs[i-1][idx]))
+                    return prefix;
+        return prefix;
+    }
+};
