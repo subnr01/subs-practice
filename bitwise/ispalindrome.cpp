0cf9a388c4e0f29a3ceadbeacf4a190adf56fd58
Subbu <subnr01@gmail.com>
2017-02-03 23:22:46 -0500

    Create ispalindrome.cpp

---
+++ bitwise/ispalindrome.cpp
@@ -0,0 +1,43 @@
+/*
+
+Is this a palindrome number
+
+*/
+
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0|| (x!=0 &&x%10==0)) return false;
+        int sum=0;
+        while(x>sum)
+        {
+            sum = sum*10+x%10;
+            x = x/10;
+        }
+        return (x==sum)||(x==sum/10);
+    }
+};
+
+/* another solution */
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+    if(x<0)
+        return false;
+
+    int num=x;
+    int a=0;
+    while(x)
+    {
+        a=a*10 + x%10;
+        x=x/10;
+    }
+    if(a==num)
+        return true;
+    else
+        return false;
+    
+}
+};
