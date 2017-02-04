5c4245497b36f105c6e9b61768da1aab9016983e
Subbu <subnr01@gmail.com>
2017-02-03 22:43:52 -0500

    Create rev_bits.cpp

---
+++ bitwise/rev_bits.cpp
@@ -0,0 +1,20 @@
+/*
+Reverse bits of a given 32 bits unsigned integer.
+
+For example, given input 43261596 (represented in binary as 00000010100101000001111010011100), 
+return 964176192 (represented in binary as 00111001011110000010100101000000).
+
+*/
+
+
+
+class Solution {
+public:
+    uint32_t  reverseBits(uint32_t n) {
+        uint32_t result= 0;
+        for(int i=0; i<32; i++)
+            result = (result<<1) + (n>>i &1);
+        
+        return result;
+    }
+};
