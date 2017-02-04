6191c1811f94c75203b0ff489d72c334e11eefc6
Subbu <subnr01@gmail.com>
2017-02-03 23:17:08 -0500

    Update binaryTreePaths.cpp

--- trees/binaryTreePaths.cpp
+++ trees/binaryTreePaths.cpp
@@ -14,3 +14,23 @@ All root-to-leaf paths are:
 ["1->2->5", "1->3"]
 
 */
+
+
+
+void binaryTreePaths(vector<string>& result, TreeNode* root, string t) {
+    if(!root->left && !root->right) {
+        result.push_back(t);
+        return;
+    }
+
+    if(root->left) binaryTreePaths(result, root->left, t + "->" + to_string(root->left->val));
+    if(root->right) binaryTreePaths(result, root->right, t + "->" + to_string(root->right->val));
+}
+
+vector<string> binaryTreePaths(TreeNode* root) {
+    vector<string> result;
+    if(!root) return result;
+    
+    binaryTreePaths(result, root, to_string(root->val));
+    return result;
+}
