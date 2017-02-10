e2ddc0f864efc7981e4c34404fb98ce115d8ecfb
Subbu <subnr01@gmail.com>
2017-02-10 16:31:28 -0500

    Create trie.cpp

---
+++ Design/trie.cpp
@@ -0,0 +1,71 @@
+/*
+Implement a trie with insert, search, and startsWith methods.
+
+Note:
+You may assume that all inputs are consist of lowercase letters a-z.
+
+*/
+
+class TrieNode
+{
+public:
+    TrieNode *next[26];
+    bool is_word;
+    
+    // Initialize your data structure here.
+    TrieNode(bool b = false)
+    {
+        memset(next, 0, sizeof(next));
+        is_word = b;
+    }
+};
+
+class Trie
+{
+    TrieNode *root;
+public:
+    Trie()
+    {
+        root = new TrieNode();
+    }
+
+    // Inserts a word into the trie.
+    void insert(string s)
+    {
+        TrieNode *p = root;
+        for(int i = 0; i < s.size(); ++ i)
+        {
+            int ch = s[i] - 'a';
+            if(p -> next[ch] == NULL)
+                p -> next[ch] = new TrieNode();
+            p = p -> next[ch];
+        }
+        p -> is_word = true;
+    }
+
+    // Returns if the word is in the trie.
+    bool search(string key)
+    {
+        TrieNode *p = find(key);
+        return p != NULL && p -> is_word;
+    }
+
+    // Returns if there is any word in the trie
+    // that starts with the given prefix.
+    bool startsWith(string prefix)
+    {
+        return find(prefix) != NULL;
+    }
+
+private:
+    TrieNode* find(string key)
+    {
+        TrieNode *p = root;
+        for(int i = 0; i < key.size() && p != NULL; ++ i)
+        {
+            int ch = key[i] - 'a';
+            p = p -> next[key[i] - 'a'];
+        }
+        return p;
+    }
+};
