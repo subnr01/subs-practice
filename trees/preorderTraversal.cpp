7100e2b8937692b7668173b76854d561519286cf
Subbu <subnr01@gmail.com>
2017-02-10 13:51:28 -0500

    Update preorderTraversal.cpp

--- trees/preorderTraversal.cpp
+++ trees/preorderTraversal.cpp
@@ -12,3 +12,29 @@ Given binary tree {1,#,2,3},
 return [1,2,3].
 
 */
+
+
+class Solution {
+public:
+    
+ vector<int> preorderTraversal(TreeNode *root) {
+    if (root==NULL) {
+        return vector<int>();
+    }
+    vector<int> result;
+    stack<TreeNode *> treeStack;
+    treeStack.push(root);
+    while (!treeStack.empty()) {
+        TreeNode *temp = treeStack.top();
+        result.push_back(temp->val);
+        treeStack.pop();
+        if (temp->right!=NULL) {
+            treeStack.push(temp->right);
+        }
+        if (temp->left!=NULL) {
+            treeStack.push(temp->left);
+        }
+    }
+    return result;
+    }
+};
