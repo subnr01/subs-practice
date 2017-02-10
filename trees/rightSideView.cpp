a3535e108208a16570585a29f50883d7d182668f
Subbu <subnr01@gmail.com>
2017-02-10 11:04:02 -0500

    Update rightSideView.cpp

--- trees/rightSideView.cpp
+++ trees/rightSideView.cpp
@@ -12,3 +12,21 @@ Given the following binary tree,
 You should return [1, 3, 4].
 
 */
+
+
+class Solution {
+public:
+    void dfs(TreeNode* root, int lv, vector<int> &res){
+        if(!root)   return;
+        if(lv>=res.size()) res.push_back(root->val);
+        dfs(root->right,lv+1,res);
+        dfs(root->left,lv+1,res);
+        
+    }
+
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        dfs(root, 0, res);
+        return res;
+    }
+};
