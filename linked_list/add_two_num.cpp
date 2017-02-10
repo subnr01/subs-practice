73687a617c76d6031ad39974537c363b34e7f28c
Subbu <subnr01@gmail.com>
2017-02-10 13:59:58 -0500

    Update add_two_num.cpp

--- linked_list/add_two_num.cpp
+++ linked_list/add_two_num.cpp
@@ -1,6 +1,7 @@
 /*
 
-You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order 
+and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
 
 Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
@@ -17,19 +18,30 @@ Output: 7 -> 0 -> 8
  * };
  */
 class Solution {
-public:
-    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-    ListNode preHead(0), *p = &preHead;
-    int carry = 0;
-    while (l1 || l2 || carry) {
-        int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
-        carry   = sum / 10;
-        p->next = new ListNode(sum % 10);
-        p = p->next;
-        l1 = l1 ? l1->next : l1;
-        l2 = l2 ? l2->next : l2;
+    vector<int> convertToVector(ListNode *head) {
+        vector<int> res;
+        while (head) {
+            res.push_back(head->val);
+            head = head->next;
+        }
+        return res;
     }
-    return preHead.next;
+    ListNode* addTwoVector(vector<int> v1, vector<int> v2) {
+        ListNode *dummy = new ListNode(0), *node = NULL;
+        int i = v1.size() - 1, j = v2.size() - 1, sum = 0;
+        while (j >= 0 || i >= 0 || sum) {
+            if (i >= 0) sum += v1[i--];
+            if (j >= 0) sum += v2[j--];
+            node = new ListNode(sum % 10);
+            sum /= 10;
+            node->next = dummy->next;
+            dummy->next = node;
+        }
+        return dummy->next;
+    }
+public:
+    ListNode* addTwoNumbers(ListNode *l1, ListNode *l2) {
+        return addTwoVector(convertToVector(l1), convertToVector(l2));
     }
 };
 
