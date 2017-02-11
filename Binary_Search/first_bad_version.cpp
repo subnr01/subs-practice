6e64e4e90f4686da2f11330a328fa97cf30d9ca0
Subbu <subnr01@gmail.com>
2017-02-11 17:49:55 -0500

    Create first_bad_version.cpp

---
+++ Binary_Search/first_bad_version.cpp
@@ -0,0 +1,18 @@
+/*
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+*/
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+    int start = 1, end = n;
+    while (start < end) {
+        int mid = start + (end-start) / 2;
+        if (!isBadVersion(mid)) start = mid + 1;
+        else end = mid;            
+    }        
+    return start;
+}
+}
