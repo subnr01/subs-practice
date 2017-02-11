2b129a871086650b8a4d85176beb6be0cfcaf296
Subbu <subnr01@gmail.com>
2017-02-11 17:28:54 -0500

    Create repeat_DNA_Seq.cpp

---
+++ strings/repeat_DNA_Seq.cpp
@@ -0,0 +1,29 @@
+/*
+All DNA is composed of a series of nucleotides abbreviated as A, C, G, and T, for example: "ACGAATTCCG". When studying DNA, it is sometimes useful to identify repeated sequences within the DNA.
+
+Write a function to find all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule.
+
+For example,
+
+Given s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT",
+
+Return:
+["AAAAACCCCC", "CCCCCAAAAA"].
+
+*/
+
+//https://discuss.leetcode.com/topic/8487/i-did-it-in-10-lines-of-c
+
+
+
+vector<string> findRepeatedDnaSequences(string s) {
+    unordered_map<int, int> m;
+    vector<string> r;
+    int t = 0, i = 0, ss = s.size();
+    while (i < 9)
+        t = t << 3 | s[i++] & 7;
+    while (i < ss)
+        if (m[t = t << 3 & 0x3FFFFFFF | s[i++] & 7]++ == 1)
+            r.push_back(s.substr(i - 10, 10));
+    return r;
+}
