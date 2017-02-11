fc7c6726d958f82d98dfdcbbaf82372d112e80b6
Subbu <subnr01@gmail.com>
2017-02-11 08:43:23 -0500

    Create permutations_1.cpp

---
+++ arrays/permutations/permutations_1.cpp
@@ -0,0 +1,44 @@
+/*
+Given a collection of distinct numbers, return all possible permutations.
+
+For example,
+[1,2,3] have the following permutations:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+
+*/
+
+//https://discuss.leetcode.com/topic/46162/a-general-approach-to-backtracking-questions-in-java-subsets-permutations-combination-sum-palindrome-partioning
+
+class Solution {
+public:
+    vector<vector<int> > permute(vector<int> &num) {
+	    vector<vector<int> > result;
+	    
+	    permuteRecursive(num, 0, result);
+	    return result;
+    }
+    
+    // permute num[begin..end]
+    // invariant: num[0..begin-1] have been fixed/permuted
+	void permuteRecursive(vector<int> &num, int begin, vector<vector<int> > &result)	{
+		if (begin >= num.size()) {
+		    // one permutation instance
+		    result.push_back(num);
+		    return;
+		}
+		
+		for (int i = begin; i < num.size(); i++) {
+		    swap(num[begin], num[i]);
+		    permuteRecursive(num, begin + 1, result);
+		    // reset
+		    swap(num[begin], num[i]);
+		}
+    }
+};  
