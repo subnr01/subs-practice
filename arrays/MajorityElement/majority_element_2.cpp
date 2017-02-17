d586d9ca83d2486f655925c44264256524510fff
Subbu <subnr01@gmail.com>
2017-02-17 18:44:56 -0500

    Update majority_element_2.cpp

--- arrays/MajorityElement/majority_element_2.cpp
+++ arrays/MajorityElement/majority_element_2.cpp
@@ -31,4 +31,4 @@ public:
     return r;
     
   }
-};Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times. The algorithm should run in linear time and in O(1) space.
+};
