99368870af6827f31f128e83d29954db81a3d412
Subbu <subnr01@gmail.com>
2017-02-20 06:41:43 -0500

    Update RandomizedSet.cpp

--- arrays/Random_data_structure/RandomizedSet.cpp
+++ arrays/Random_data_structure/RandomizedSet.cpp
@@ -7,9 +7,8 @@ remove(val): Removes an item val from the set if present.
 getRandom: Returns a random element from current set of elements. Each element must have the same probability of being returned.
 
 */
-
 /*
-Example
+Example:
 // Init an empty set.
 RandomizedSet randomSet = new RandomizedSet();
 
