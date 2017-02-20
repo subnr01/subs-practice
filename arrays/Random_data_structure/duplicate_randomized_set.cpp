44775509d810021d2d5ce62132c7c195da21d7e5
Subbu <subnr01@gmail.com>
2017-02-20 07:26:50 -0500

    Update duplicate_randomized_set.cpp

--- arrays/Random_data_structure/duplicate_randomized_set.cpp
+++ arrays/Random_data_structure/duplicate_randomized_set.cpp
@@ -5,7 +5,8 @@ Design a data structure that supports all following operations in average O(1) t
 Note: Duplicate elements are allowed.
 insert(val): Inserts an item val to the collection.
 remove(val): Removes an item val from the collection if present.
-getRandom: Returns a random element from current collection of elements. The probability of each element being returned is linearly related to the number of same value the collection contains.
+getRandom: Returns a random element from current collection of elements. The probability of each element
+being returned is linearly related to the number of same value the collection contains.
 
 */
 
