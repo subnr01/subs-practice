24993f5116a2b8836fa90c83d9af3e233f5332e2
Subbu <subnr01@gmail.com>
2017-02-20 18:36:54 -0500

    Update combination_sum_three.cpp

--- arrays/combination_sum/combination_sum_three.cpp
+++ arrays/combination_sum/combination_sum_three.cpp
@@ -1,6 +1,7 @@
 /*
 
-Find all possible combinations of k numbers that add up to a number n, given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.
+Find all possible combinations of k numbers that add up to a number n, given that only numbers from 1 to 9 
+can be used and each combination should be a unique set of numbers.
 
 
 Example 1:
