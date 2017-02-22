a6058227b3922a75fce9b9d5701c1f2bf2ff7971
Subbu <subnr01@gmail.com>
2017-02-22 06:44:49 -0500

    Update check_interval.cpp

--- arrays/interval/check_interval.cpp
+++ arrays/interval/check_interval.cpp
@@ -3,7 +3,8 @@ Given a set of intervals, for each of the interval i, check if there exists an i
 than or equal to the end point of the interval i, which can be called that j is on the "right" of i.
 
 For any interval i, you need to store the minimum interval j's index, which means that the interval j has the minimum 
-start point to build the "right" relationship for interval i. If the interval j doesn't exist, store -1 for the interval i. Finally, you need output the stored value of each interval as an array.
+start point to build the "right" relationship for interval i. If the interval j doesn't exist, store -1 for the interval i. 
+Finally, you need output the stored value of each interval as an array.
 
 Note:
 You may assume the interval's end point is always bigger than its start point.
