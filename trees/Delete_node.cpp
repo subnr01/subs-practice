dba8b3fd8e59f524afc7b3e50c6bb3405b6a4e5f
Subbu <subnr01@gmail.com>
2017-02-23 20:02:19 -0500

    Delete Delete_node.cpp

--- trees/Delete_node.cpp
+++
@@ -1,8 +0,0 @@
-/*
-
-delete a node in the BST
-
-
-Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
-*/
-
