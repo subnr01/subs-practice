795d46774ede3c155032901c18a3dbec8ecd648a
Subbu <subnr01@gmail.com>
2017-02-24 07:32:09 -0500

    Update maxPathSum.cpp

--- trees/maxPathSum.cpp
+++ trees/maxPathSum.cpp
@@ -15,3 +15,18 @@ Given the below binary tree,
 Return 6.
 
 */
+
+
+int maxPathSum(TreeNode *root) {
+    int maxPath = INT_MIN;
+    dfsMaxPath(root, maxPath);
+    return maxPath;
+}
+
+int dfsMaxPath(TreeNode *root, int &maxPath) {
+    if (!root) return 0;
+    int l = max(0, dfsMaxPath(root->left, maxPath));
+    int r = max(0, dfsMaxPath(root->right, maxPath));
+    maxPath = max(maxPath, l + r + root->val);
+    return root->val + max(l, r);
+}
