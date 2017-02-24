eb98c464279a7af35277788dd1a1a2df457742bd
Subbu <subnr01@gmail.com>
2017-02-24 07:12:57 -0500

    Update postorderTraversal.cpp

--- trees/postorderTraversal.cpp
+++ trees/postorderTraversal.cpp
@@ -15,3 +15,48 @@ Note: Recursive solution is trivial, could you do it iteratively?
 
 
 */
+
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode *root) {
+        stack<TreeNode*> nodeStack;
+        vector<int> result;
+        //base case
+        if(root==NULL)
+        return result;
+        nodeStack.push(root);
+    while(!nodeStack.empty())
+    {
+        TreeNode* node= nodeStack.top();  
+        result.push_back(node->val);
+        nodeStack.pop();
+        if(node->left)
+        nodeStack.push(node->left);
+        if(node->right)
+        nodeStack.push(node->right);
+    }
+     reverse(result.begin(),result.end());
+     return result;
+    }  
+};
+
+
+vector<int> postorderTraversal_2(TreeNode *root) {
+    vector<int> v;
+    if (!root) return v;
+    
+    stack<TreeNode *> s;
+    s.push(root);
+    
+    TreeNode *p = NULL;
+    while(!s.empty()) {
+        p = s.top();
+        s.pop();
+        v.insert(v.begin(), p->val);
+        if (p->left) s.push(p->left);
+        if (p->right) s.push(p->right);
+    }
+    
+    return v;
+}
