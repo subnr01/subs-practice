dd7cb17716f69255ea86af9328803e3d86dc9741
Subbu <subnr01@gmail.com>
2017-02-25 09:52:27 -0500

    Create design_phone_directory.cpp

---
+++ Design/design_phone_directory.cpp
@@ -0,0 +1,41 @@
+/*
+Design a Phone Directory which supports the following operations:
+
+get: Provide a number which is not assigned to anyone.
+check: Check if a number is available or not.
+release: Recycle or release a number.
+*/
+
+http://www.programcreek.com/2014/08/leetcode-design-phone-directory-java/
+
+ Set<Integer> used = new HashSet<Integer>();
+    Queue<Integer> available = new LinkedList<Integer>();
+    int max;
+    public PhoneDirectory(int maxNumbers) {
+        max = maxNumbers;
+        for (int i = 0; i < maxNumbers; i++) {
+            available.offer(i);
+        }
+    }
+    
+    public int get() {
+        Integer ret = available.poll();
+        if (ret == null) {
+            return -1;
+        }
+        used.add(ret);
+        return ret;
+    }
+    
+    public boolean check(int number) {
+        if (number >= max || number < 0) {
+            return false;
+        }
+        return !used.contains(number);
+    }
+    
+    public void release(int number) {
+        if (used.remove(number)) {
+            available.offer(number);
+        }
+    }
