88b6c8221ce669b572e091351ea47e33af92a1f5
Subbu <subnr01@gmail.com>
2017-02-25 07:37:04 -0500

    Create InOrderSuccessor.cpp

---
+++ trees/InOrderSuccessor.cpp
@@ -0,0 +1,10 @@
+
+
+Given a binary search tree and a node in it, find the in-order successor of that node in the BST.
+
+TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+    TreeNode* candidate = NULL;
+    while (root)
+        root = (root->val > p->val) ? (candidate = root)->left : root->right;
+    return candidate;
+}
