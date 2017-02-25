a3dbbddb9c027f99853a36dbfd199c287944ed44
Subbu <subnr01@gmail.com>
2017-02-25 10:01:28 -0500

    Create movingAvg.cpp

---
+++ Design/movingAvg.cpp
@@ -0,0 +1,34 @@
+/*
+Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
+For example,
+MovingAverage m = new MovingAverage(3);
+m.next(1) = 1
+m.next(10) = (1 + 10) / 2
+m.next(3) = (1 + 10 + 3) / 3
+m.next(5) = (10 + 3 + 5) / 3
+
+*/
+
+
+public class MovingAverage {
+    private int [] window;
+    private int n, insert;
+    private long sum;
+    
+    /** Initialize your data structure here. */
+    public MovingAverage(int size) {
+        window = new int[size];
+        insert = 0;
+        sum = 0;
+    }
+    
+    public double next(int val) {
+        if (n < window.length)  n++;
+        sum -= window[insert];
+        sum += val;
+        window[insert] = val;
+        insert = (insert + 1) % window.length;
+        
+        return (double)sum / n;
+    }
+}
