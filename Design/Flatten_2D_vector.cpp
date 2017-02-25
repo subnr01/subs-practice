d4b06b7de8ec2373902e8fa3c160d3bee417bdf3
Subbu <subnr01@gmail.com>
2017-02-25 09:55:23 -0500

    Create Flatten_2D_vector.cpp

---
+++ Design/Flatten_2D_vector.cpp
@@ -0,0 +1,49 @@
+/*
+
+Implement an iterator to flatten a 2d vector.
+For example,
+Given 2d vector =
+[
+  [1,2],
+  [3],
+  [4,5,6]
+]
+By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,2,3,4,5,6].
+Hint:
+How many variables do you need to keep track?
+Two variables is all you need. Try with x and y.
+Beware of empty rows. It could be the first few rows.
+To write correct code, think about the invariant to maintain. What is it?
+The invariant is x and y must always point to a valid point in the 2d vector. Should you maintain your invariant ahead of time or right when you need it?
+Not sure? Think about how you would implement hasNext(). Which is more complex?
+Common logic in two different places should be refactored into a common method.
+Understand the problem:
+The question itself is very easy to solve. Just several corner cases need to think of:
+  -- What if the 2d vector contains empty arrays, e.g. [ ], [ ], 1 2 3 ? In this case, the next() should not output anything, but the return type is int. There the hasNext() should be more complicated in which it handles this situation. 
+  -- What if the 2d vector itself is empty? Again, handle it in hasNext() 
+
+*/
+
+class Vector2D {
+    vector<vector<int>>::iterator i, iEnd;
+    int j = 0;
+public:
+    Vector2D(vector<vector<int>>& vec2d) {
+        i = vec2d.begin();
+        iEnd = vec2d.end();
+    }
+
+    int next() {
+        hasNext();
+        return (*i)[j++];
+    }
+
+    bool hasNext() {
+        while (i != iEnd && j == (*i).size())
+            i++, j = 0;
+        return i != iEnd;
+    }
+};
+
+
+
