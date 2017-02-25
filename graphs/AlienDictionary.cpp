03768d3111396e28b824fa5c46bb11b17a3139b0
Subbu <subnr01@gmail.com>
2017-02-25 09:32:40 -0500

    Create AlienDictionary.cpp

---
+++ graphs/AlienDictionary.cpp
@@ -0,0 +1,54 @@
+/*
+
+There is a new alien language which uses the latin alphabet. However, the order among letters are unknown to you. You receive a list of words from the dictionary, where words are sorted lexicographically by the rules of this new language. Derive the order of letters in this language.
+For example, Given the following words in dictionary,
+[
+  "wrt",
+  "wrf",
+  "er",
+  "ett",
+  "rftt"
+]
+The correct order is: "wertf".
+There are few points not clear :
+each word itself has no order, ie abc can not deduce the order of a, b ,c
+if any chars has not found and there is no dependence what then ?
+
+
+*/
+
+//https://discuss.leetcode.com/topic/22476/16-18-lines-python-30-lines-c
+
+
+string alienOrder(vector<string>& words) {
+    map<char, set<char>> suc, pre;
+    set<char> chars;
+    string s;
+    for (string t : words) {
+        chars.insert(t.begin(), t.end());
+        for (int i=0; i<min(s.size(), t.size()); ++i) {
+            char a = s[i], b = t[i];
+            if (a != b) {
+                suc[a].insert(b);
+                pre[b].insert(a);
+                break;
+            }
+        }
+        s = t;
+    }
+    set<char> free = chars;
+    for (auto p : pre)
+        free.erase(p.first);
+    string order;
+    while (free.size()) {
+        char a = *begin(free);
+        free.erase(a);
+        order += a;
+        for (char b : suc[a]) {
+            pre[b].erase(a);
+            if (pre[b].empty())
+                free.insert(b);
+        }
+    }
+    return order.size() == chars.size() ? order : "";
+}
