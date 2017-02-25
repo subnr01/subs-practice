83520c8bb12f24d196b8d25a3bf6b28bd612706d
Subbu <subnr01@gmail.com>
2017-02-25 08:11:33 -0500

    Create treeUpsideDown.cpp

---
+++ strings/treeUpsideDown.cpp
@@ -0,0 +1,33 @@
+/*
+Leetcode: Binary Tree Upside Down
+Given a binary tree where all the right nodes are either leaf nodes with a sibling (a left node that shares the same parent node) or empty, flip it upside down and turn it into a tree where the original right nodes turned into left leaf nodes. Return the new root.
+For example:
+Given a binary tree {1,2,3,4,5},
+    1
+   / \
+  2   3
+ / \
+4   5
+return the root of the binary tree [4,5,2,#,#,3,1].
+   4
+  / \
+ 5   2
+    / \
+   3   1  
+
+*/
+
+
+TreeNode* upsideDownBinaryTree(TreeNode* root) {
+    if (!root || !root->left) return root;
+    TreeNode* cur_left = root->left;
+    TreeNode* cur_right = root->right;
+    TreeNode* new_root = upsideDownBinaryTree(root->left);
+    cur_left->right = root;
+    cur_left->left = cur_right;
+    root->left = nullptr;
+    root->right = nullptr;
+    return new_root;
+}
+
+https://discuss.leetcode.com/topic/44365/c-recursive-non-recursive-solution-summary-with-detailed-comments
