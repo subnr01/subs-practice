a78c336e7396b0387400765bd816bc8209f7eaeb
Subbu <subnr01@gmail.com>
2017-02-26 20:21:39 -0500

    Update reverseString.cpp

--- strings/reverseString.cpp
+++ strings/reverseString.cpp
@@ -5,6 +5,10 @@ Reverse a string
 */
 
 
+Points to remember:
+-- Loops runs from 0 till n/2.
+-- swap i and (n-1)-i meaning first and last.
+
 class Solution {
 public:
     string reverseString(string s) {
