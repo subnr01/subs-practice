18a5d76425cc0697a80619eb480ee1ab7f69f1ac
Subbu <subnr01@gmail.com>
2017-02-26 19:40:04 -0500

    Update length_of_last_Word.cpp

--- strings/word_probs/length_of_last_Word.cpp
+++ strings/word_probs/length_of_last_Word.cpp
@@ -1,6 +1,7 @@
 /*
 
-Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of 
+last word in the string.
 
 If the last word does not exist, return 0.
 
