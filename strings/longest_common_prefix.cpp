ff3a0a9728a6ce5e00e18ccd93608ad85fa25ec6
Subbu <subnr01@gmail.com>
2017-02-27 22:34:11 -0500

    Update longest_common_prefix.cpp

--- strings/longest_common_prefix.cpp
+++ strings/longest_common_prefix.cpp
@@ -7,13 +7,17 @@ https://leetcode.com/articles/longest-common-prefix/
 
 
 class Solution {
-public:
-    string longestCommonPrefix(vector<string>& strs) {
-        string prefix = "";
-        for(int idx=0; strs.size()>0; prefix+=strs[0][idx], idx++)
-            for(int i=0; i<strs.size(); i++)
-                if(idx >= strs[i].size() ||(i > 0 && strs[i][idx] != strs[i-1][idx]))
-                    return prefix;
-        return prefix;
-    }
-};
+    public:
+        string longestCommonPrefix(vector<string>& strs) {
+            string toReturn;
+            if (!strs.size()) return toReturn;
+            for (int i = 0; i < strs[0].size(); ++i){
+                char curr = strs[0][i];
+                for (int j = 1; j < strs.size(); ++j){
+                    if (i > strs[j].size()-1 || curr != strs[j][i]) return toReturn;
+                }
+                toReturn.push_back(curr);
+            }
+            return toReturn;
+        }
+    };
