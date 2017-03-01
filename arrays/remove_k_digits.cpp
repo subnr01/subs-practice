1774eabdfc14c3db23367f6dee87aa71eb221b18
Subbu <subnr01@gmail.com>
2017-03-01 05:15:16 -0500

    Update remove_k_digits.cpp

--- arrays/remove_k_digits.cpp
+++ arrays/remove_k_digits.cpp
@@ -1,6 +1,7 @@
 /*
 
-Given a non-negative integer num represented as a string, remove k digits from the number so that the new number is the smallest possible.
+Given a non-negative integer num represented as a string, remove k digits from the number so that the new number 
+is the smallest possible.
 
 Note:
 The length of num is less than 10002 and will be ≥ k.
