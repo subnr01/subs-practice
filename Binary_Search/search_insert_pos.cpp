f683d2d353eae5f7a404389b04a97d292a155be2
Subbu <subnr01@gmail.com>
2017-02-28 19:57:22 -0500

    Create search_insert_pos.cpp

---
+++ Binary_Search/search_insert_pos.cpp
@@ -0,0 +1,42 @@
+/*
+Given a sorted array and a target value, return the index if the target is found. If not, return the
+index where it would be if it were inserted in order.
+You may assume no duplicates in the array.
+Here are few examples.
+[1,3,5,6], 5 → 2
+[1,3,5,6], 2 → 1
+[1,3,5,6], 7 → 4
+[1,3,5,6], 0 → 0
+end
+*/
+
+Points to remember:
+1. This follows the regular binary search, however we have to return low instead of mid if we do not find 
+   the element.
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        
+        int high = nums.size() - 1;
+        
+        int low = 0;
+        
+        while (low <= high) {
+            int mid = (high-low)/2 + low;
+            
+            if (nums[mid] > target) {
+                high = mid - 1;
+            }
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            }
+            else {
+                return mid;
+            }
+        }
+        
+        return low;
+        
+    }
+};
