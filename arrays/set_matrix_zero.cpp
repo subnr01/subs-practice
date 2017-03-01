a0764b22ace1d7b264ee82cb6631ba56158db869
Subbu <subnr01@gmail.com>
2017-02-28 19:36:14 -0500

    Update set_matrix_zero.cpp

--- arrays/set_matrix_zero.cpp
+++ arrays/set_matrix_zero.cpp
@@ -16,6 +16,8 @@ public:
                 }
             }
         }
+        
+        /* This will set the corresponding row or column (other than the non first row/column to 0).*/
         for(int i = 1; i < matrix.size(); i++){
             for(int j = 1; j < matrix[0].size(); j++) {
               
@@ -24,11 +26,14 @@ public:
                 }
             }
         }
+        
+        /* the following code sets the first column to 0*/
         if(col){
             for(int i = 0; i < matrix.size(); i++) {
                 matrix[i][0] = 0;
             }
         }
+        /* the following code sets the first row to 0*/
         if(row){
             for(int j = 0; j < matrix[0].size(); j++) {
                 matrix[0][j] = 0;
