05e2d7162753f3303d4129cfea1badd0f7c70928
Subbu <subnr01@gmail.com>
2017-03-07 20:03:06 -0500

    Update getMinDifference.cpp

--- trees/ClosestBSTValue/getMinDifference.cpp
+++ trees/ClosestBSTValue/getMinDifference.cpp
@@ -59,3 +59,32 @@ int getMinimumDifference(TreeNode* root) {
     return min_dif;
 }
 
+
+What if it is not a BST? (Follow up of the problem) The idea is to put values in a TreeSet and then every time we can use O(lgN) time to lookup for the nearest values.
+
+Solution 2 - Pre-Order traverse, time complexity O(NlgN), space complexity O(N).
+
+public class Solution {
+    TreeSet<Integer> set = new TreeSet<>();
+    int min = Integer.MAX_VALUE;
+    
+    public int getMinimumDifference(TreeNode root) {
+        if (root == null) return min;
+        
+        if (!set.isEmpty()) {
+            if (set.floor(root.val) != null) {
+                min = Math.min(min, Math.abs(root.val - set.floor(root.val)));
+            }
+            if (set.ceiling(root.val) != null) {
+                min = Math.min(min, Math.abs(root.val - set.ceiling(root.val)));
+            }
+        }
+        
+        set.add(root.val);
+        
+        getMinimumDifference(root.left);
+        getMinimumDifference(root.right);
+        
+        return min;
+    }
+}
