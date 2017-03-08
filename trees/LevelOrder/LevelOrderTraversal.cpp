1ffd0a7febb057fa61b1590c4539e7f37bd2e44d
Subbu <subnr01@gmail.com>
2017-03-08 06:56:26 -0500

    Update LevelOrderTraversal.cpp

--- trees/LevelOrder/LevelOrderTraversal.cpp
+++ trees/LevelOrder/LevelOrderTraversal.cpp
@@ -108,3 +108,23 @@ public:
         levelOrderRe(node->right, level + 1, res);
     }
 };
+
+
+//Another solution
+vector<vector<int>> ret;
+
+void buildVector(TreeNode *root, int depth)
+{
+    if(root == NULL) return;
+    if(ret.size() == depth)
+        ret.push_back(vector<int>());
+    
+    ret[depth].push_back(root->val);
+    buildVector(root->left, depth + 1);
+    buildVector(root->right, depth + 1);
+}
+
+vector<vector<int> > levelOrder(TreeNode *root) {
+    buildVector(root, 0);
+    return ret;
+}
