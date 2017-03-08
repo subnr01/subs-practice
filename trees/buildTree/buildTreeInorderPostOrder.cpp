50fcfc1afd373eca12ba3cf5ff083bf788d7e69e
Subbu <subnr01@gmail.com>
2017-03-08 11:20:07 -0500

    Update buildTreeInorderPostOrder.cpp

--- trees/buildTree/buildTreeInorderPostOrder.cpp
+++ trees/buildTree/buildTreeInorderPostOrder.cpp
@@ -7,27 +7,29 @@ Given inorder and postorder traversal of a tree, construct the binary tree.
 
 class Solution {
 public:
-    TreeNode *ct(vector<int> &inorder, vector<int> &postorder, int ist, int ied, int ped) {    
-            if (ist>ied){return NULL;}
-            TreeNode *res=new TreeNode(postorder[ped]);
-            int mid;
-            for (int i=ist;i<=ied;i++){
-                if (inorder[i]==res->val){mid = i;break;}
-            }
-            res->right = ct(inorder,postorder,mid+1,ied,ped-1);
-            res->left = ct(inorder,postorder,ist,mid-1, ped - ied + mid - 1);
-            return res;
+   TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
+    int post_order = postorder.size() - 1;
+    return create(inorder, postorder, 0, inorder.size() - 1, &post_order);
+}
+
+TreeNode* create(vector<int> &inorder, vector<int> &postorder, int is, int ie, int *pe){
+    
+    if(is > ie) {
+        return nullptr;
     }
- 
- 
-    TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
-        // Start typing your C/C++ solution below
-        // DO NOT write int main() function
-        if (postorder.size()==0){
-            return NULL;
-        }else{
-            return ct(inorder,postorder,0,inorder.size()-1,postorder.size()-1);
+    TreeNode* node = new TreeNode(postorder[*pe]);
+    (*pe)--;
+    
+    int pos;
+    for(int i = is; i <= ie; i++){
+        if(inorder[i] == node->val){
+            pos = i;
+            break;
         }
-             
-    }    
+    }
+    node->right = create(inorder, postorder, pos + 1, ie, pe);
+    node->left = create(inorder, postorder, is, pos - 1, pe);
+    
+    return node;
+}
 };
