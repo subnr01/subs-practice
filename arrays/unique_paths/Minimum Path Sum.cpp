17732585c29cad93a7b61c6bee7c3485b935ce65
Subbu <subnr01@gmail.com>
2017-03-09 09:52:53 -0500

    Update Minimum Path Sum.cpp

--- arrays/unique_paths/Minimum Path Sum.cpp
+++ arrays/unique_paths/Minimum Path Sum.cpp
@@ -8,3 +8,22 @@ Note: You can only move either down or right at any point in time.
 
 
 https://discuss.leetcode.com/topic/15269/10-lines-28ms-o-n-space-dp-solution-in-c-with-explanations/2
+
+
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<int> cur(m, grid[0][0]);
+        for (int i = 1; i < m; i++)
+            cur[i] = cur[i - 1] + grid[i][0]; 
+        for (int j = 1; j < n; j++) {
+            cur[0] += grid[0][j]; 
+            for (int i = 1; i < m; i++)
+                cur[i] = min(cur[i - 1], cur[i]) + grid[i][j];
+        }
+        return cur[m - 1];
+    }
+};
