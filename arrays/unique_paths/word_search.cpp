152b4a8e5e9ed42fc9a0e14ae400b1d78519ff13
Subbu <subnr01@gmail.com>
2017-03-09 09:50:20 -0500

    Update word_search.cpp

--- arrays/unique_paths/word_search.cpp
+++ arrays/unique_paths/word_search.cpp
@@ -17,3 +17,28 @@ word = "SEE", -> returns true,
 word = "ABCB", -> returns false.
 
 */
+
+
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size(), n = board[0].size();
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (search(board, i, j, word.c_str()))
+                    return true;
+        return false;
+    }
+private: 
+    bool search(vector<vector<char>>& board, int r, int c, const char* word) {
+        if (!*word) return true;
+        int m = board.size(), n = board[0].size();
+        if (r < 0 || r >= m || c < 0 || c >= n || board[r][c] != word[0]) return false;
+        board[r][c] = '$';
+        if (search(board, r - 1 ,c, word + 1) || search(board, r + 1, c, word + 1) ||
+            search(board, r, c - 1, word + 1) || search(board, r, c + 1, word + 1))
+            return true;
+        board[r][c] = word[0];
+        return false;
+    }
+};
