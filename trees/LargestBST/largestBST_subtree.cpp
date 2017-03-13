4786986c48f94997315eaa1c8f5f144d4011e27b
Subbu <subnr01@gmail.com>
2017-03-13 07:26:02 -0400

    Update largestBST_subtree.cpp

--- trees/LargestBST/largestBST_subtree.cpp
+++ trees/LargestBST/largestBST_subtree.cpp
@@ -1,5 +1,6 @@
 /*
-Given a binary tree, find the largest Binary Search Tree (BST), where largest means BST with largest number of nodes in it. The largest BST may or may not include all of its descendants.
+Given a binary tree, find the largest Binary Search Tree (BST), where largest means BST with largest number of nodes 
+in it. The largest BST may or may not include all of its descendants.
 
 */
 
