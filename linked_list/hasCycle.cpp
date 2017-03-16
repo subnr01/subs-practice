441915bbefadf82dea5903d36597a879cc3ef4cd
Subbu <subnr01@gmail.com>
2017-03-16 04:17:35 -0400

    Create hasCycle.cpp

---
+++ linked_list/hasCycle.cpp
@@ -0,0 +1,29 @@
+/*
+
+Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?
+
+*/
+
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == NULL || head -> next == NULL)    
+            return false;
+ 
+        ListNode *fast = head;
+        ListNode *slow = head;
+        
+        while(fast -> next && fast -> next -> next){
+            fast = fast -> next -> next;
+            slow = slow -> next;
+            if(fast == slow)
+                return true;
+        }
+ 
+        return false;
+    }
+};
