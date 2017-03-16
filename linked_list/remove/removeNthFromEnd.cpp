4bbb7153862e36d3e71b57472094d3228b6f7d7b
Subbu <subnr01@gmail.com>
2017-03-16 05:45:13 -0400

    Create removeNthFromEnd.cpp

---
+++ linked_list/remove/removeNthFromEnd.cpp
@@ -0,0 +1,34 @@
+/*
+
+Given a linked list, remove the nth node from the end of list and return its head.
+
+For example,
+
+   Given linked list: 1->2->3->4->5, and n = 2.
+
+   After removing the second node from the end, the linked list becomes 1->2->3->5.
+Note:
+Given n will always be valid.
+Try to do this in one pass.
+*/
+
+
+class Solution
+{
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n)
+    {
+        ListNode** t1 = &head, *t2 = head;
+        for(int i = 1; i < n; ++i)
+        {
+            t2 = t2->next;
+        }
+        while(t2->next != NULL)
+        {
+            t1 = &((*t1)->next);
+            t2 = t2->next;
+        }
+        *t1 = (*t1)->next;
+        return head;
+    }
+};
