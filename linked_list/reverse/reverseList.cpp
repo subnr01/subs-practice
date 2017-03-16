f1cde8d770b0666887e0a7b6694100b141b701a4
Subbu <subnr01@gmail.com>
2017-03-16 04:50:40 -0400

    Create reverseList.cpp

---
+++ linked_list/reverse/reverseList.cpp
@@ -0,0 +1,31 @@
+/*
+Reverse a singly linked list.
+
+*/
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* pre = NULL;
+        while (head) {
+            ListNode* next = head -> next;
+            head -> next = pre;
+            pre = head;
+            head = next;
+        } 
+        return pre;
+    }
+};
+
+
+class Solution {
+public:   
+    ListNode* reverseList(ListNode* head) {
+        if (!head || !(head -> next)) return head;
+        ListNode* node = reverseList(head -> next);
+        head -> next -> next = head;
+        head -> next = NULL;
+        return node; 
+    }
+}; 
+
+
