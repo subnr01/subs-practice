c96b209269f6f00c0bb77ea718a9d39a69568cce
Subbu <subnr01@gmail.com>
2017-03-16 05:06:05 -0400

    Create deleteDuplicates_2.cpp

---
+++ linked_list/remove_duplicates/deleteDuplicates_2.cpp
@@ -0,0 +1,62 @@
+/*
+Given a sorted linked list, delete all nodes that have duplicate numbers,
+leaving only distinct numbers from the original list.
+
+For example,
+Given 1->2->3->3->4->4->5, return 1->2->5.
+Given 1->1->1->2->3, return 2->3.
+
+*/
+
+
+/*
+recursive
+
+*/
+
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (!head) return 0;
+        if (!head->next) return head;
+        
+        int val = head->val;
+        ListNode* p = head->next;
+        
+        if (p->val != val) {
+            head->next = deleteDuplicates(p);
+            return head;
+        } else {
+            while (p && p->val == val) p = p->next;
+            return deleteDuplicates(p);
+        }
+    }
+};
+
+
+class Solution {
+public:
+    ListNode *deleteDuplicates(ListNode *head) {
+        ListNode **runner = &head;
+        
+        if(!head || !head->next)return head;
+        
+        while(*runner)
+        {
+            if((*runner)->next && (*runner)->next->val == (*runner)->val)
+            {
+                ListNode *temp = *runner;
+                while(temp && (*runner)->val == temp->val)
+                    temp = temp->next;
+                
+                *runner = temp;
+            }
+            else
+                runner = &((*runner)->next);
+        }
+        
+        return head;
+    }
+};
+
