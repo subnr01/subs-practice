c24c01543ce7bcad4c31ce9a07336e6caabea665
Subbu <subnr01@gmail.com>
2017-03-17 06:46:18 -0400

    Update reverseKGroup.cpp

--- linked_list/reverse/reverseKGroup.cpp
+++ linked_list/reverse/reverseKGroup.cpp
@@ -20,64 +20,33 @@ For k = 3, you should return: 3->2->1->4->5
 
 class Solution {
 public:
-    ListNode *reverseKGroup(ListNode *head, int k) {
-        if(head==NULL||k==1) return head;
-        int num=0;
-        ListNode *preheader = new ListNode(-1);
-        preheader->next = head;
-        ListNode *cur = preheader, *nex, *pre = preheader;
-        while(cur = cur->next) 
-            num++;
-        while(num>=k) {
-            cur = pre->next;
-            nex = cur->next;
-            for(int i=1;i<k;++i) {
-                cur->next=nex->next;
-                nex->next=pre->next;
-                pre->next=nex;
-                nex=cur->next;
-            }
-            pre = cur;
-            num-=k;
-        }
-        return preheader->next;
-    }
-};
 
 
-class Solution 
+ListNode* reverseKGroup( struct node *head, int k)
 {
-public:
+	if ( !head || !k)
+		return;
 
-    ListNode* reverse(ListNode* first, ListNode* last)
-    {
-        ListNode* prev = last;
+	int n = k;
+	ListNode *current = head;
+	ListNode *prev = NULL;
+	ListNode *next;
 
-        while ( first != last )
-        {
-            auto tmp = first->next;
-            first->next = prev;
-            prev = first;
-            first = tmp;
-        }
+	while (current != NULL && n > 0)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+		n--;
+	}
 
-        return prev;
-    }
+	if ( current != NULL)
+	{	
+		head->next = reverseKGroup(current,k)
+	}
 
-    ListNode* reverseKGroup(ListNode* head, int k) 
-    {
-        auto node=head;
-        for (int i=0; i < k; ++i)
-        {
-            if ( ! node  )
-                return head; // nothing to do list too sort
-            node = node->next;
-        }
-
-        auto new_head = reverse( head, node);
-        head->next = reverseKGroup( node, k);
-        return new_head;
-    }
+	return prev;
+}
 };
-
 
