292aa445adeddd4e6a44fdf0728128aaa24b2eb5
Subbu <subnr01@gmail.com>
2017-03-17 05:52:28 -0400

    Update deleteDuplicates_2.cpp

--- linked_list/remove/deleteDuplicates_2.cpp
+++ linked_list/remove/deleteDuplicates_2.cpp
@@ -9,30 +9,7 @@ Given 1->1->1->2->3, return 2->3.
 */
 
 
-/*
-recursive
 
-*/
-
-
-class Solution {
-public:
-    ListNode* deleteDuplicates(ListNode* head) {
-        if (!head) return 0;
-        if (!head->next) return head;
-        
-        int val = head->val;
-        ListNode* p = head->next;
-        
-        if (p->val != val) {
-            head->next = deleteDuplicates(p);
-            return head;
-        } else {
-            while (p && p->val == val) p = p->next;
-            return deleteDuplicates(p);
-        }
-    }
-};
 
 
 class Solution {
@@ -60,3 +37,27 @@ public:
     }
 };
 
+/*
+recursive
+
+*/
+
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (!head) return 0;
+        if (!head->next) return head;
+        
+        int val = head->val;
+        ListNode* p = head->next;
+        
+        if (p->val != val) {
+            head->next = deleteDuplicates(p);
+            return head;
+        } else {
+            while (p && p->val == val) p = p->next;
+            return deleteDuplicates(p);
+        }
+    }
+};
