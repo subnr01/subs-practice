7804d98acd4cd879158bf0b8eb7822713908e3c3
Subbu <subnr01@gmail.com>
2017-03-21 15:16:14 -0400

    Update meeting-room2.cpp

--- arrays/meeting-rooms/meeting-room2.cpp
+++ arrays/meeting-rooms/meeting-room2.cpp
@@ -1,3 +1,9 @@
 /*
-Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] find the minimum number of conference rooms required.
+Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] 
+
+find the minimum number of conference rooms required.
 */
+
+
+
+https://discuss.leetcode.com/topic/20971/c-o-n-log-n-584-ms-3-solutions
