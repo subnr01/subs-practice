e55cafeaa9c6efd22cf5ea892e950238f4ca9f2f
Subbu <subnr01@gmail.com>
2017-03-21 15:38:28 -0400

    Update meeting-room2.cpp

--- arrays/meeting-rooms/meeting-room2.cpp
+++ arrays/meeting-rooms/meeting-room2.cpp
@@ -7,3 +7,24 @@ find the minimum number of conference rooms required.
 
 
 https://discuss.leetcode.com/topic/20971/c-o-n-log-n-584-ms-3-solutions
+
+
+
+int minMeetingRooms(vector<Interval>& intervals) {
+    vector<int> starts, ends;
+    for (auto i : intervals) {
+        starts.push_back(i.start);
+        ends.push_back(i.end);
+    }
+    sort(begin(starts), end(starts));
+    sort(begin(ends), end(ends));
+    int e = 0, rooms = 0, available = 0;
+    for (int start : starts) {
+        while (ends[e] <= start) {
+            ++e;
+            ++available;
+        }
+        available ? --available : ++rooms;
+    }
+    return rooms;
+}
