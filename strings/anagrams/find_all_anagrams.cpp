d43de2f8a6aeabc4b15ba69ffb3be5c759bbe881
Subbu <subnr01@gmail.com>
2017-03-22 05:11:44 -0400

    Update find_all_anagrams.cpp

--- strings/anagrams/find_all_anagrams.cpp
+++ strings/anagrams/find_all_anagrams.cpp
@@ -23,28 +23,52 @@ The substring with start index = 6 is "bac", which is an anagram of "abc".
 https://discuss.leetcode.com/topic/68976/sliding-window-algorithm-template-to-solve-all-the-leetcode-substring-search-problem
 
 
-class Solution {
-public:
-    vector<int> findAnagrams(string s, string p) {
-        vector<int> pv(26,0), sv(26,0), res;
-        if(s.size() < p.size())
-           return res;
-        for(int i = 0; i < p.size(); ++i)
-        {
-            ++pv[p[i]-'a'];
-            ++sv[s[i]-'a'];
-        }
-        if(pv == sv)
-           res.push_back(0);
-        for(int i = p.size(); i < s.size(); ++i)
-        {
-            ++sv[s[i]-'a'];
-            --sv[s[i-p.size()]-'a'];
-            if(pv == sv)
-               res.push_back(i-p.size()+1);
-        }
-        return res;
+public class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> list = new ArrayList<>();
+    if (s == null || s.length() == 0 || p == null || p.length() == 0) return list;
+    
+    int[] hash = new int[256]; //character hash
+    
+    //record each character in p to hash
+    for (char c : p.toCharArray()) {
+        hash[c]++;
     }
-};
+    //two points, initialize count to p's length
+    int left = 0, right = 0, count = p.length();
+    
+    while (right < s.length()) {
+        //move right everytime, if the character exists in p's hash, decrease the count
+        //current hash value >= 1 means the character is existing in p
+        if (hash[s.charAt(right)] >= 1) {
+            count--;
+        }
+        hash[s.charAt(right)]--;
+        right++;
+        
+        //when the count is down to 0, means we found the right anagram
+        //then add window's left to result list
+        if (count == 0) {
+            list.add(left);
+        }
+        //if we find the window's size equals to p, then we have to move left (narrow the window) to find the new match window
+        //++ to reset the hash because we kicked out the left
+        //only increase the count if the character is in p
+        //the count >= 0 indicate it was original in the hash, cuz it won't go below 0
+        if (right - left == p.length() ) {
+           
+            if (hash[s.charAt(left)] >= 0) {
+                count++;
+            }
+            hash[s.charAt(left)]++;
+            left++;
+        
+        }
+
+        
+    }
+        return list;
+    }
+}
 
 
