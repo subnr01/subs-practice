c4af23c56e9b5a5d648f29d73bd5367f3baf3bed
Subbu <subnr01@gmail.com>
2017-03-22 04:43:18 -0400

    Create is_palindromic_rotation.cpp

---
+++ strings/geeksforgeeks/rotation/is_palindromic_rotation.cpp
@@ -0,0 +1,12 @@
+
+A Simple Solution is to take the input string, try every possible rotation of it and return true if a rotation is a palindrome. 
+If no rotation is palindrome, then return false.
+Complexity is O(n2)
+
+Optimised solution:
+An Optimized Solution can work in O(n) time. The idea is similar to this post. Following are steps.
+1) Let the given string be ‘str’ and length of string be ‘n’. Create a temporary string ‘temp’ which is 
+   of size 2n and contains str followed by str again. For example, let str be “aab”, temp would be “aabaab”.
+2) Now the problem reduces to find a palindromic substring of length n in str. If there is palindromic substring 
+   of length n, then return true, else return false.
+We can find whether there is a palindromic substring of size n or not in O(n) time (See Longest palindromic substring)
