0ada0ec4dc485a1fdc70f6e7c92b9d1ecb463af7
Subbu <subnr01@gmail.com>
2017-03-22 06:46:03 -0400

    Update find_all_anagrams.cpp

--- strings/anagrams/find_all_anagrams.cpp
+++ strings/anagrams/find_all_anagrams.cpp
@@ -23,54 +23,53 @@ The substring with start index = 6 is "bac", which is an anagram of "abc".
 https://discuss.leetcode.com/topic/68976/sliding-window-algorithm-template-to-solve-all-the-leetcode-substring-search-problem
 
 
-public class Solution {
-    public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> list = new ArrayList<>();
-    if (s == null || s.length() == 0 || p == null || p.length() == 0) return list;
+   vector<int> findAnagrams(string s, string p) {
+        std::vector<int> v; 
+    if (s.size() == 0 ||  p.size() == 0) return v;
     
-    int[] hash = new int[256]; //character hash
+
+    unordered_map<int,int> hash;
     
     //record each character in p to hash
-    for (char c : p.toCharArray()) {
-        hash[c]++;
+    for (int i = 0;i < p.size(); i++) {
+        hash[p[i]]++;
     }
     //two points, initialize count to p's length
-    int left = 0, right = 0, count = p.length();
+    int left = 0, right = 0, count = p.size();
     
     while (right < s.length()) {
         //move right everytime, if the character exists in p's hash, decrease the count
         //current hash value >= 1 means the character is existing in p
-        if (hash[s.charAt(right)] >= 1) {
+        if (hash[s[right]] >= 1) {
             count--;
         }
-        hash[s.charAt(right)]--;
+        hash[s[right]]--;
         right++;
         
         //when the count is down to 0, means we found the right anagram
         //then add window's left to result list
         if (count == 0) {
-            list.add(left);
+            v.push_back(left);
         }
         //if we find the window's size equals to p, then we have to move left (narrow the window) to find the new match window
         //++ to reset the hash because we kicked out the left
         //only increase the count if the character is in p
         //the count >= 0 indicate it was original in the hash, cuz it won't go below 0
-        if (right - left == p.length() ) {
+        if (right - left == p.size() ) {
            
-            if (hash[s.charAt(left)] >= 0) {
+            if (hash[s[left]] >= 0) {
                 count++;
             }
-            hash[s.charAt(left)]++;
+            hash[s[left]]++;
             left++;
         
         }
 
         
     }
-        return list;
-    }
+        return v;
 }
-
+};
 
 //Simple straightforward solution using two vectors
     /* Sliding window approach to cover all substrings in s of length p.size() *
