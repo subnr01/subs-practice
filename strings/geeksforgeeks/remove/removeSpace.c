8fa6150bded88b51cb5f0da89919ec62e714ab1f
Subbu <subnr01@gmail.com>
2017-03-22 04:44:20 -0400

    Create removeSpace.c

---
+++ strings/geeksforgeeks/remove/removeSpace.c
@@ -0,0 +1,15 @@
+/* 
+Remove spaces from a string
+*/
+
+
+void removeSpace(char *str) {
+  char *p1 = str, *p2 = str;
+  
+  do {
+
+    while (*p2 == ' ')  //if the character space, then increment p2
+      p2++;
+
+  } while (*p1++ = *p2++); //increment p1 for every valid p2 character
+}
