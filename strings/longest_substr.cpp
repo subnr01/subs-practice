c950fe446991368ab6bbec473ce44497f181faa1
Subbu <subnr01@gmail.com>
2017-03-22 04:47:26 -0400

    Create longest_substr.cpp

---
+++ strings/longest_substr.cpp
@@ -0,0 +1,29 @@
+/*
+Given a string, find the length of the longest substring without repeating
+characters. For example, the longest substring without repeating letters for
+“abcabcbb” is “abc”, which the length is 3. For “bbbbb” the longest substring is
+“b”, with the length of 1.
+*/
+
+int lengthOfLongestSubstring(string s) {
+  int n = s.length();
+  int i = 0, j = 0;
+  int maxLen = 0;
+  bool exist[256] = { false };
+  while (j < n) {
+    if (exist[s[j]]) {
+      maxLen = max(maxLen, j-i);
+      while (s[i] != s[j]) {
+        exist[s[i]] = false;
+        i++;
+      }
+      i++;
+      j++;
+    } else {
+      exist[s[j]] = true;
+      j++;
+    }
+  }
+  maxLen = max(maxLen, n-i);
+  return maxLen;
+}
