9ce0120e8a426c7a21388275bfd8a6d460363efc
Subbu <subnr01@gmail.com>
2017-03-24 15:20:10 -0400

    Update largestBST.cpp

--- trees/LargestBST/largestBST.cpp
+++ trees/LargestBST/largestBST.cpp
@@ -12,11 +12,11 @@ public:
         
         int res = 0;
         int mini, maxi;
-        bool b = isBST(root, res, mini, maxi);
+        bool b = isBST(root, res, &mini, &maxi);
 
         return res;
     }
-    bool isBST(TreeNode* node, int& res, int& mini, int& maxi) {
+    bool isBST(TreeNode* node, int& res, int *mini, int *maxi) {
         
         if (!node)
             return true;
@@ -24,16 +24,16 @@ public:
         int left_size=0, right_size=0;
         int left_mini, left_maxi, right_mini, right_maxi;
         
-        bool leftB = isBST(node->left, left_size, left_mini, left_maxi);
-        bool rightB = isBST(node->right, right_size, right_mini, right_maxi);
+        bool leftB = isBST(node->left, left_size, &left_mini, &left_maxi);
+        bool rightB = isBST(node->right, right_size, &right_mini, &right_maxi);
 
         if (leftB && rightB) {
             if ( (!node->left || node->val >= left_maxi) && (!node->right || node->val <= right_mini) ) {
                 /* The tree rooted at this node is a binary search tree */
                 res = left_size+right_size+1;
                 
-                mini = node->left ? left_mini : node->val;
-                maxi = node->right ? right_maxi : node->val;
+                *mini = node->left ? left_mini : node->val;
+                *maxi = node->right ? right_maxi : node->val;
                 return true;
             }
         }
