5427fe8d0bbf9078e3145db265bfb9e0d9e884e1
Subbu <subnr01@gmail.com>
2017-04-01 17:24:42 -0400

    Create insert.cpp

---
+++ trees/insert.cpp
@@ -0,0 +1,24 @@
+/*
+
+Insert into a BST
+
+*/
+
+
+  
+/* A utility function to insert a new node with given key in BST */
+struct node* insert(struct node* node, int key)
+{
+    /* If the tree is empty, return a new node */
+    if (node == NULL) return newNode(key);
+ 
+    /* Otherwise, recur down the tree */
+    if (key < node->key)
+        node->left  = insert(node->left, key);
+    else if (key > node->key)
+        node->right = insert(node->right, key);   
+ 
+    /* return the (unchanged) node pointer */
+    return node;
+}
+
