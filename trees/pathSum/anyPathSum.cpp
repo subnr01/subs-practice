66a35be518f4e7ccce634e5a49a9c2dda7c7fe65
Subbu <subnr01@gmail.com>
2017-04-03 08:29:14 -0400

    Update anyPathSum.cpp

--- trees/pathSum/anyPathSum.cpp
+++ trees/pathSum/anyPathSum.cpp
@@ -33,19 +33,7 @@ Return 3. The paths that sum to 8 are:
 
 
 
-class Solution {
-public:
-    int pathSum(TreeNode* root, int sum) {
-        if(!root) return 0;
-        return sumUp(root, 0, sum) + pathSum(root->left, sum) + pathSum(root->right, sum);
-    }
-private:
-    int sumUp(TreeNode* root, int pre, int& sum){
-        if(!root) return 0;
-        int current = pre + root->val;
-        return (current == sum) + sumUp(root->left, current, sum) + sumUp(root->right, current, sum);
-    }
-};
+
 
 
 /* returns the paths */
