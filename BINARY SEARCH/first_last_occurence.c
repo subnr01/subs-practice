7295ec2d7f44856041106e9c1a843fdba7b5016b
Subbu <subnr01@gmail.com>
2017-04-09 11:09:13 -0400

    Update first_last_occurence.c

---
+++ BINARY SEARCH/first_last_occurence.c
@@ -0,0 +1,19 @@
+
+/*
+Last occurence
+*/
+
+int last(int arr[], int low, int high, int x, int n)
+{
+  if(high >= low)
+  {
+    int mid = (low + high)/2;  /*low + (high - low)/2;*/
+    if( ( mid == n-1 || x < arr[mid+1]) && arr[mid] == x )
+      return mid;
+    else if(x < arr[mid])
+      return last(arr, low, (mid -1), x, n);
+    else
+      return last(arr, (mid + 1), high, x, n);      
+  }
+  return -1;
+}
