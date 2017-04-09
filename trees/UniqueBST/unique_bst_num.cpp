11bcc92d1e0acbbb69e1f882dfa230bba045f0c1
Subbu <subnr01@gmail.com>
2017-04-09 17:06:37 -0400

    Update unique_bst_num.cpp

--- trees/UniqueBST/unique_bst_num.cpp
+++ trees/UniqueBST/unique_bst_num.cpp
@@ -8,6 +8,28 @@ https://discuss.leetcode.com/topic/8398/dp-solution-in-6-lines-with-explanation-
 
 */
 
+
+ Number of binary search trees = 
+                     (Number of ways root can be choosen)*
+                     (Number of Left binary search sub-trees) *                 
+                     (Number of Right binary search sub-trees)                                                  
+Now, since there are "n" nodes in BST and let, the number of BST be represented by C(n) for n elements.
+
+We can find the number of BSTs recursively as :
+choose 1 as root, no element  on the left sub-tree. n-1 elements on the right sub-tree.
+Choose 2 as root, 1 element  on the left sub-tree. n-2 elements on the right sub-tree.
+Choose 3 as root, 2 element on the left sub-tree. n-3 elements on the right sub-tree.
+
+Similarly, for i-th element as the root, i-1 elements on the left and n-i on the right.
+
+These sub-trees are also BSTs so we can write :
+
+C(n) = C(0)C(n-1) + C(1)C(n-2) + .....+ C(i-1)C(n-i)..... + C(n-1)C(0)
+    
+    
+    
+    
+
 class Solution {
 public:
     int numTrees(int n) {
