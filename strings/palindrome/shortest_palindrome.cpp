420d8ea8bfbbada9df05ee77eac3fd508c08a3f7
Subbu <subnr01@gmail.com>
2017-04-09 17:24:18 -0400

    Update shortest_palindrome.cpp

--- strings/palindrome/shortest_palindrome.cpp
+++ strings/palindrome/shortest_palindrome.cpp
@@ -1,5 +1,6 @@
 /*
 
+HARD QUESTION
 
 Given a string S, you are allowed to convert it to a palindrome by adding characters in front of it.
 Find and return the shortest palindrome you can find by performing this transformation.
