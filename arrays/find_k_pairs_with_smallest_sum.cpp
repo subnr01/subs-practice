507424dae1766e045d5ff83e96e1e185f5e59a1e
Subbu <subnr01@gmail.com>
2017-04-12 06:16:39 -0400

    Update find_k_pairs_with_smallest_sum.cpp

--- arrays/find_k_pairs_with_smallest_sum.cpp
+++ arrays/find_k_pairs_with_smallest_sum.cpp
@@ -34,23 +34,21 @@ https://discuss.leetcode.com/topic/50450/slow-1-liner-to-fast-solutions/23
 https://discuss.leetcode.com/topic/50481/clean-16ms-c-o-n-space-o-klogn-time-solution-using-priority-queue
 class Solution {
 public:
-    vector<pair<int, int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        vector<pair<int,int>> result;
-        if (nums1.empty() || nums2.empty() || k <= 0)
-            return result;
-        auto comp = [&nums1, &nums2](pair<int, int> a, pair<int, int> b) {
-            return nums1[a.first] + nums2[a.second] > nums1[b.first] + nums2[b.second];};
-        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comp)> min_heap(comp);
-        min_heap.emplace(0, 0);
-        while(k-- > 0 && min_heap.size())
+    vector<pair<int, int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) 
+    {
+        vector<pair<int, int>> v;
+        if(nums1.empty() || nums2.empty()) return v;
+        auto cmp = [](const pair<int, int>& a, const pair<int, int>& b) {
+            return a.first+a.second > b.first+b.second; };
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)> minHeap(cmp);
+        for(auto n: nums1)
+            for(auto m: nums2)
+                minHeap.emplace(n, m);
+        while(minHeap.size() && k--)
         {
-            auto idx_pair = min_heap.top(); min_heap.pop();
-            result.emplace_back(nums1[idx_pair.first], nums2[idx_pair.second]);
-            if (idx_pair.first + 1 < nums1.size())
-                min_heap.emplace(idx_pair.first + 1, idx_pair.second);
-            if (idx_pair.first == 0 && idx_pair.second + 1 < nums2.size())
-                min_heap.emplace(idx_pair.first, idx_pair.second + 1);
+            v.emplace_back(minHeap.top());
+            minHeap.pop();
         }
-        return result;
+        return v;
     }
-};
+}
