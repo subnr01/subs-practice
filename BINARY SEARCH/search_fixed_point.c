a6fda9659bc4c56bddd292ca10da5cf8fed13df6
Subbu <subnr01@gmail.com>
2017-04-19 10:12:17 -0400

    Update and rename search_element.c to search_fixed_point.c

---
+++ BINARY SEARCH/search_fixed_point.c
@@ -0,0 +1,28 @@
+
+
+/*
+Given an array of n distinct integers sorted in ascending order, write a function that returns a Fixed Point in the array, if there is any Fixed Point present in array, else returns -1. Fixed Point in an array is an index i such that arr[i] is equal to i. Note that integers in array can be negative.
+
+
+
+*/
+
+
+
+
+int binarySearch(int arr[], int low, int high)
+{
+    if(high >= low)
+    {
+        int mid = (low + high)/2;  /*low + (high - low)/2;*/
+        if(mid == arr[mid])
+            return mid;
+        if(mid > arr[mid])
+            return binarySearch(arr, (mid + 1), high);
+        else
+            return binarySearch(arr, low, (mid -1));
+    }
+ 
+    /* Return -1 if there is no Fixed Point */
+    return -1;
+}
