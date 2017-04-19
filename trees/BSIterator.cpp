c9c7b32c5289d2f8927c114dd708d3e646c38d5b
Subbu <subnr01@gmail.com>
2017-04-19 17:34:27 -0400

    Update BSIterator.cpp

--- trees/BSIterator.cpp
+++ trees/BSIterator.cpp
@@ -9,6 +9,34 @@ uses O(h) memory, where h is the height of the tree.
 
 */
 
+class BSTIterator {
+    stack<TreeNode *> myStack;
+public:
+    BSTIterator(TreeNode *root) {
+        pushAll(root);
+    }
+
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return !myStack.empty();
+    }
+
+    /** @return the next smallest number */
+    int next() {
+        TreeNode *tmpNode = myStack.top();
+        myStack.pop();
+        pushAll(tmpNode->right);
+        return tmpNode->val;
+    }
+
+private:
+    void pushAll(TreeNode *node) {
+        for (; node != NULL; myStack.push(node), node = node->left);
+    }
+};
+
+
+
 
 class BSTIterator {
 private:
