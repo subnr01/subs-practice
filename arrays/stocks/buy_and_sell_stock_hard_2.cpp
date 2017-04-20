1fdea401b50074b3e5471ffd873db9cf99cebd12
Subbu <subnr01@gmail.com>
2017-04-20 06:37:24 -0400

    Update buy_and_sell_stock_hard_2.cpp

--- arrays/stocks/buy_and_sell_stock_hard_2.cpp
+++ arrays/stocks/buy_and_sell_stock_hard_2.cpp
@@ -38,3 +38,25 @@ public:
         return rele[k];
     }
 };
+
+
+public int maxProfit(int k, int[] prices) {
+    if(k>=prices.length/2){
+        int maxProfit = 0;
+        for(int i=1; i<prices.length; i++){
+            if(prices[i]>prices[i-1]) maxProfit += prices[i]-prices[i-1];
+        }
+        return maxProfit;
+    }
+
+    int[] maxProfit = new int[k+1];
+    int[] lowPrice = new int[k+1];
+    for(int i=0; i<lowPrice.length; i++) lowPrice[i]=Integer.MAX_VALUE;
+    for(int p : prices){
+        for(int i=k; i>=1; i--){
+            maxProfit[i] = Math.max(maxProfit[i],p-lowPrice[i]);
+            lowPrice[i] = Math.min(lowPrice[i],p-maxProfit[i-1]);
+        }
+    }
+    return maxProfit[k];
+}
