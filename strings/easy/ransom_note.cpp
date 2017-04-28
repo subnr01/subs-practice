3d80a09e2af5341c405dfaa96925434b8abba101
Subbu <subnr01@gmail.com>
2017-04-28 18:02:52 -0400

    Create ransom_note.cpp

---
+++ strings/easy/ransom_note.cpp
@@ -0,0 +1,23 @@
+/*
+
+You may assume that both strings contain only lowercase letters.
+
+canConstruct("a", "b") -> false
+canConstruct("aa", "ab") -> false
+canConstruct("aa", "aab") -> true
+
+*/
+
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> map(26);
+        for (int i = 0; i < magazine.size(); ++i)
+            ++map[magazine[i]];
+        for (int j = 0; j < ransomNote.size(); ++j)
+            if (--map[ransomNote[j]] < 0)
+                return false;
+        return true;
+    }
+};
