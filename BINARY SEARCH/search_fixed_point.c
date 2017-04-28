3256f6e6f94628fd86e4ebed8719dc3ecc947cde
Subbu <subnr01@gmail.com>
2017-04-28 06:15:04 -0400

    Update search_fixed_point.c

--- BINARY SEARCH/search_fixed_point.c
+++ BINARY SEARCH/search_fixed_point.c
@@ -1,7 +1,9 @@
 
 
 /*
-Given an array of n distinct integers sorted in ascending order, write a function that returns a Fixed Point in the array, if there is any Fixed Point present in array, else returns -1. Fixed Point in an array is an index i such that arr[i] is equal to i. Note that integers in array can be negative.
+Given an array of n distinct integers sorted in ascending order, write a function that returns a Fixed Point 
+in the array, if there is any Fixed Point present in array, else returns -1. Fixed Point in an array is an 
+index i such that arr[i] is equal to i. Note that integers in array can be negative.
 
 
 
