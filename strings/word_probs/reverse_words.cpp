5f597aa5383043b364ffdc2c0eea03809b0f16ae
Subbu <subnr01@gmail.com>
2017-04-27 22:49:45 -0400

    Update reverse_words.cpp

--- strings/word_probs/reverse_words.cpp
+++ strings/word_probs/reverse_words.cpp
@@ -44,3 +44,49 @@ public:
         reverseword(s,0,j-1);                  // reverse whole string
     }
 };
+
+
+
+/*
+Geeksforgeeks solution
+*/
+
+/*Function to reverse words*/
+void reverseWords(char *s)
+{
+  char *word_begin = s;
+  char *temp = s; /* temp is for word boundry */
+ 
+  /*STEP 1 of the above algorithm */
+  while( *temp )
+  {
+    temp++;
+    if (*temp == '\0')
+    {
+      reverse(word_begin, temp-1);
+    }
+    else if(*temp == ' ')
+    {
+      reverse(word_begin, temp-1);
+      word_begin = temp+1;
+    }
+  } /* End of while */
+ 
+   /*STEP 2 of the above algorithm */
+  reverse(s, temp-1);
+}
+ 
+/* UTILITY FUNCTIONS */
+/*Function to reverse any sequence starting with pointer
+  begin and ending with pointer end  */
+void reverse(char *begin, char *end)
+{
+  char temp;
+  while (begin < end)
+  {
+    temp = *begin;
+    *begin++ = *end;
+    *end-- = temp;
+  }
+}
+
