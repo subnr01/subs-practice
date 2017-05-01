1b5ad9226f6c007e0fc3bcda2d248e9895505283
Subbu <subnr01@gmail.com>
2017-05-01 11:20:13 -0400

    Update serialize_deser_bst.cpp

--- trees/SerializeD/serialize_deser_bst.cpp
+++ trees/SerializeD/serialize_deser_bst.cpp
@@ -66,3 +66,37 @@ string serialize(TreeNode* root) {
     return s;
 }
 };
+
+
+
+//markker: geeksforgeeks
+// This function stores a tree in a file pointed by fp
+void serialize(Node *root, FILE *fp)
+{
+    // If current node is NULL, store marker
+    if (root == NULL)
+    {
+        fprintf(fp, "%d ", MARKER);
+        return;
+    }
+ 
+    // Else, store current node and recur for its children
+    fprintf(fp, "%d ", root->key);
+    serialize(root->left, fp);
+    serialize(root->right, fp);
+}
+ 
+// This function constructs a tree from a file pointed by 'fp'
+void deSerialize(Node *&root, FILE *fp)
+{
+    // Read next item from file. If theere are no more items or next
+    // item is marker, then return
+    int val;
+    if ( !fscanf(fp, "%d ", &val) || val == MARKER)
+       return;
+ 
+    // Else create node with this item and recur for children
+    root = newNode(val);
+    deSerialize(root->left, fp);
+    deSerialize(root->right, fp);
+}
