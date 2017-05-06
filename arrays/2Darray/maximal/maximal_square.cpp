a2f3647e343a0fa746117e5e3b921e596ff4da7d
Subbu <subnr01@gmail.com>
2017-05-06 17:02:11 -0400

    Create maximal_square.cpp

---
+++ arrays/2Darray/maximal/maximal_square.cpp
@@ -0,0 +1,10 @@
+/*
+Given a 2D binary matrix filled with 0's and 1's, find the largest square containing all 1's and return its area.
+
+For example, given the following matrix:
+
+1101
+1101
+1111
+Return 4.
+*/
