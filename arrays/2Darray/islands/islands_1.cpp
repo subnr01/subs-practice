4da4d78b1759e64db6061cc5fde2a465daacd07e
Subbu <subnr01@gmail.com>
2017-05-06 17:03:21 -0400

    Create islands_1.cpp

---
+++ arrays/2Darray/islands/islands_1.cpp
@@ -0,0 +1,12 @@
+/*
+Given a 2-d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+
+11110
+11010
+11000
+00000
+Answer: 1
+
+*/
