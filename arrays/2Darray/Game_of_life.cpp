4296601c720f6611e4871ae651021850e8b1b1eb
Subbu <subnr01@gmail.com>
2017-05-06 17:20:05 -0400

    Update Game_of_life.cpp

--- arrays/2Darray/Game_of_life.cpp
+++ arrays/2Darray/Game_of_life.cpp
@@ -8,3 +8,22 @@ Any dead cell with exactly three live neighbors becomes a live cell, as if by re
 
 Write a function to compute the next state (after one update) of the board given its current state.
 */
+
+
+void gameOfLife(vector<vector<int>>& board) {
+    int m = board.size(), n = m ? board[0].size() : 0;
+    for (int i=0; i<m; ++i) {
+        for (int j=0; j<n; ++j) {
+            int count = 0;
+            for (int I=max(i-1, 0); I<min(i+2, m); ++I)
+                for (int J=max(j-1, 0); J<min(j+2, n); ++J)
+                    count += board[I][J] & 1;
+            if (count == 3 || count - board[i][j] == 3)
+                board[i][j] |= 2;
+        }
+    }
+    for (int i=0; i<m; ++i)
+        for (int j=0; j<n; ++j)
+            board[i][j] >>= 1;
+}
+
