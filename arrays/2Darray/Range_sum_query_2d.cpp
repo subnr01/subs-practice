bb4af8f94f9a0debd2d8583a4565b3391756ef10
Subbu <subnr01@gmail.com>
2017-05-06 17:27:25 -0400

    Update Range_sum_query_2d.cpp

--- arrays/2Darray/Range_sum_query_2d.cpp
+++ arrays/2Darray/Range_sum_query_2d.cpp
@@ -1,4 +1,24 @@
 /*
-Given a 2D matrix matrix, find the sum of the elements inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+Given a 2D matrix matrix, find the sum of the elements inside the rectangle defined by its upper left 
+corner (row1, col1) and lower right corner (row2, col2).
 
 */
+class NumMatrix {
+public:
+    NumMatrix(vector<vector<int>> &matrix) {
+        accu = matrix;
+        for (int i=0; i<matrix.size(); ++i)
+            for (int j=0; j<matrix[0].size(); ++j)
+                accu[i][j] += a(i-1, j) + a(i, j-1) - a(i-1, j-1);
+    }
+
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return a(row2, col2) - a(row1-1, col2) - a(row2, col1-1) + a(row1-1, col1-1);
+    }
+
+private:
+    vector<vector<int>> accu;
+    int a(int i, int j) {
+        return i >= 0 && j >= 0 ? accu[i][j] : 0;
+    }
+};
