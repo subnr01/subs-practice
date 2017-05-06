a967fd3b68aeef8ce2a99aebe4637d491995342a
Subbu <subnr01@gmail.com>
2017-05-06 17:13:31 -0400

    Update islands_1.cpp

--- arrays/2Darray/islands/islands_1.cpp
+++ arrays/2Darray/islands/islands_1.cpp
@@ -10,3 +10,45 @@ Example 1:
 Answer: 1
 
 */
+
+
+public class Solution {
+    char[][] g;
+    public int numIslands(char[][] grid) {
+        int islands = 0;
+        g = grid;
+        for (int i=0; i<g.length; i++)
+            for (int j=0; j<g[i].length; j++)
+                islands += sink(i, j);
+        return islands;
+    }
+    int sink(int i, int j) {
+        if (i < 0 || i == g.length || j < 0 || j == g[i].length || g[i][j] == '0')
+            return 0;
+        g[i][j] = '0';
+        sink(i+1, j); sink(i-1, j); sink(i, j+1); sink(i, j-1);
+        return 1;
+    }
+}
+
+
+//Solution 2
+public class Solution {
+    public int numIslands(char[][] grid) {
+        int islands = 0;
+        for (int i=0; i<grid.length; i++)
+            for (int j=0; j<grid[i].length; j++)
+                islands += sink(grid, i, j);
+        return islands;
+    }
+    int sink(char[][] grid, int i, int j) {
+        if (i < 0 || i == grid.length || j < 0 || j == grid[i].length || grid[i][j] == '0')
+            return 0;
+        grid[i][j] = '0';
+        for (int k=0; k<4; k++)
+            sink(grid, i+d[k], j+d[k+1]);
+        return 1;
+    }
+    int[] d = {0, 1, 0, -1, 0};
+}
+
