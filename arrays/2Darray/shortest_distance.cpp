1663371ee21b9b825b3c698e7667388f6d9e5e1d
Subbu <subnr01@gmail.com>
2017-05-06 17:23:09 -0400

    Update shortest_distance.cpp

--- arrays/2Darray/shortest_distance.cpp
+++ arrays/2Darray/shortest_distance.cpp
@@ -11,3 +11,40 @@ For example, given three buildings at (0,0), (0,4), (2,2), and an obstacle at (0
 ideal empty land to build a house, as the total travel distance of 3+3+1=7 is minimal. So return 7.
 
 */
+
+
+https://discuss.leetcode.com/topic/31702/36-ms-c-solution
+
+int shortestDistance(vector<vector<int>> grid) {
+    int m = grid.size(), n = grid[0].size();
+    auto total = grid;
+    int walk = 0, mindist, delta[] = {0, 1, 0, -1, 0};
+    for (int i=0; i<m; ++i) {
+        for (int j=0; j<n; ++j) {
+            if (grid[i][j] == 1) {
+                mindist = -1;
+                auto dist = grid;
+                queue<pair<int, int>> q;
+                q.emplace(i, j);
+                while (q.size()) {
+                    auto ij = q.front();
+                    q.pop();
+                    for (int d=0; d<4; ++d) {
+                        int i = ij.first + delta[d];
+                        int j = ij.second + delta[d+1];
+                        if (i >= 0 && i < m && j >= 0 && j < n && grid[i][j] == walk) {
+                            grid[i][j]--;
+                            dist[i][j] = dist[ij.first][ij.second] + 1;
+                            total[i][j] += dist[i][j] - 1;
+                            q.emplace(i, j);
+                            if (mindist < 0 || mindist > total[i][j])
+                                mindist = total[i][j];
+                        }
+                    }
+                }
+                walk--;
+            }
+        }
+    }
+    return mindist;
+}
