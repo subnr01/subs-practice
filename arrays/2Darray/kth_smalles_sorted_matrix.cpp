eee707abae205f5a29eab93c6006e25be90286f1
Subbu <subnr01@gmail.com>
2017-05-07 16:22:59 -0400

    Update kth_smalles_sorted_matrix.cpp

--- arrays/2Darray/kth_smalles_sorted_matrix.cpp
+++ arrays/2Darray/kth_smalles_sorted_matrix.cpp
@@ -22,31 +22,29 @@ return 13.
 class Solution
 {
 public:
-	int kthSmallest(vector<vector<int>>& matrix, int k)
-	{
-		int n = matrix.size();
-		int le = matrix[0][0], ri = matrix[n - 1][n - 1];
-		int mid = 0;
-		while (le < ri)
-		{
-			mid = le + (ri-le)/2;
-			int num = 0;
-			for (int i = 0; i < n; i++)
-			{
-				int pos = upper_bound(matrix[i].begin(), matrix[i].end(), mid) - matrix[i].begin();
-				num += pos;
-			}
-			if (num < k)
-			{
-				le = mid + 1;
-			}
-			else
-			{
-				ri = mid;
-			}
-		}
-		return le;
-	}
+	 int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n = matrix.size();
+        
+        int left = matrix[0][0];
+        int right = matrix[n-1][n-1];
+        
+        while (left < right) {
+            int midv = (left + right) / 2;
+            
+            int count = 0;  // number of elements no bigger than midv
+            for (int i = 0; i < n; i++) {
+                vector<int>& row = matrix[i];
+                count += std::upper_bound(row.begin(), row.end(), midv) - row.begin();
+            }
+            
+            if (count < k) {
+                left = midv + 1;
+            } else {
+                right = midv;    
+            }
+        }
+        return left;
+    }
 };
 
 
