5eec7e4c74ab6c8dc4810a95d2996c0be1eff7fb
Subbu <subnr01@gmail.com>
2017-05-10 06:34:56 -0400

    Update search2Dsortedarray_1.cpp

--- arrays/2Darray/search2Darray/search2Dsortedarray_1.cpp
+++ arrays/2Darray/search2Darray/search2Dsortedarray_1.cpp
@@ -1,26 +1,16 @@
 
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
 
-#include<stdio.h>
- 
-/* Searches the element x in mat[][]. If the element is found, 
-    then prints its position and returns true, otherwise prints 
-    "not found" and returns false */
-int search(int mat[4][4], int n, int x)
-{
-   int i = 0, j = n-1;  //set indexes for top right element
-   while ( i < n && j >= 0 )
-   {
-      if ( mat[i][j] == x )
-      {
-         printf("\n Found at %d, %d", i, j);
-         return 1;
-      }
-      if ( mat[i][j] > x )
-        j--;
-      else //  if mat[i][j] < x
-        i++;
-   }
- 
-   printf("\n Element not found");
-   return 0;  // if ( i==n || j== -1 )
-}
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+For example,
+
+Consider the following matrix:
+
+[
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+
+
