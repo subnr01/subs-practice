fdf9aff75104829c0af0b19213e72069a35cdc7a
Subbu <subnr01@gmail.com>
2017-05-10 21:41:36 -0400

    Update search2Dsortedarray_2.cpp

--- arrays/2Darray/search2Darray/search2Dsortedarray_2.cpp
+++ arrays/2Darray/search2Darray/search2Dsortedarray_2.cpp
@@ -1,31 +1,45 @@
+/*
+
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+For example,
+
+Consider the following matrix:
+
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+Given target = 5, return true.
+
+Given target = 20, return false.
+
+*/
+
+//Time Complexity: O(m + n)
 
 
-public class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        if(matrix==null || matrix.length==0 || matrix[0].length==0) 
-            return false;
+public boolean searchMatrix(int[][] matrix, int target) {
+    int m=matrix.length-1;
+    int n=matrix[0].length-1;
  
-        int m = matrix.length;
-        int n = matrix[0].length;
+    int i=m; 
+    int j=0;
  
-        int start = 0;
-        int end = m*n-1;
- 
-        while(start<=end){
-            int mid=(start+end)/2;
-            int midX=mid/n;
-            int midY=mid%n;
- 
-            if(matrix[midX][midY]==target) 
-                return true;
- 
-            if(matrix[midX][midY]<target){
-                start=mid+1;
-            }else{
-                end=mid-1;
-            }
+    while(i>=0 && j<=n){
+        if(target < matrix[i][j]){
+            i--;
+        }else if(target > matrix[i][j]){
+            j++;
+        }else{
+            return true;
         }
- 
-        return false;
     }
+ 
+    return false;
 }
