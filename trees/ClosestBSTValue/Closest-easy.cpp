ca9dea87af7ce85d2532bbdd80d16765089c56f2
Subbu <subnr01@gmail.com>
2017-08-06 13:28:25 -0400

    Update Closest-easy.cpp

--- trees/ClosestBSTValue/Closest-easy.cpp
+++ trees/ClosestBSTValue/Closest-easy.cpp
@@ -10,12 +10,12 @@ int closestValue(TreeNode* root, double target, double &val) {
         return;
     }
     int a = root->val;
+    int b;
     auto kid = target < a ? root->left : root->right;
     if (!kid) {
         val = a;
         return;
     } else {
-        int b;
         closestValue(kid, target, b);
     }
     val = abs(a - target) < abs(b - target) ? a : b;
