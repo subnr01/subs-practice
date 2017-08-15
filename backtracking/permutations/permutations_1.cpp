8cbfe52b8ba08d03e5445886a3c8a67776f054ab
Subbu <subnr01@gmail.com>
2017-08-15 07:40:45 -0400

    Update permutations_1.cpp

--- backtracking/permutations/permutations_1.cpp
+++ backtracking/permutations/permutations_1.cpp
@@ -28,7 +28,7 @@ public:
     // permute num[begin..end]
     // invariant: num[0..begin-1] have been fixed/permuted
 	void permuteRecursive(vector<int> &num, int begin, vector<vector<int> > &result)	{
-		if (begin >= num.size()) {
+		if (begin == num.size()) {
 		    // one permutation instance
 		    result.push_back(num);
 		    return;
