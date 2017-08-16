63a567050ed6ac4612a3be2342a6be7f3fa24813
Subbu <subnr01@gmail.com>
2017-08-16 06:22:54 -0400

    Update one_edit_distance.cpp

--- strings/edit_distance/one_edit_distance.cpp
+++ strings/edit_distance/one_edit_distance.cpp
@@ -13,7 +13,27 @@ O(m+n) where m and n are lengths of two strings.
 */
 
 
-//https://discuss.leetcode.com/topic/17194/4ms-11-lines-c-solution-with-explanations
+/* 
+Solution:
+Let the input strings be s1 and s2 and lengths of input 
+strings be m and n respectively.
+
+1) If difference between m an n is more than 1, 
+     return false.
+2) Initialize count of edits as 0.
+3) Start traversing both strings from first character.
+    a) If current characters don't match, then
+       (i)   Increment count of edits
+       (ii)  If count becomes more than 1, return false
+       (iii) If length of one string is more, then only
+             possible  edit is to remove a character.
+             Therefore, move ahead in larger string.
+       (iv)  If length is same, then only possible edit
+             is to  change a character. Therefore, move
+             ahead in both strings. 
+    b) Else, move ahead in both strings. 
+    
+*/
 
 
 
@@ -47,6 +67,9 @@ bool isOneEditDistance(string s, string t) {
 			} else {
 				j++;
 			}
+			/*
+			Do not forget to do this part
+			*/
 			i++;
 			j++;
 		}
