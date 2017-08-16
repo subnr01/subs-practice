4ae87f19550fda5a37414680c805ce42436ef285
Subbu <subnr01@gmail.com>
2017-08-16 06:38:01 -0400

    Update edit_distance_hard.cpp

--- strings/edit_distance/edit_distance_hard.cpp
+++ strings/edit_distance/edit_distance_hard.cpp
@@ -10,3 +10,42 @@ b) Delete a character
 c) Replace a character
 
 */
+
+/* 
+This is a dynamic programming question.
+*/
+
+
+/*
+Solution with the 2D version: relatively simpler
+*/
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size(), n = word2.size();
+        if(m==0 || n==0 )return max(m,n);
+        int dp[m+1][n+1];
+        for(int i=0; i<=m; i++)  dp[i][0] = i;
+        for(int i=0; i<=n; i++)  dp[0][i] = i;
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(word1[i] == word2[j]) dp[i+1][j+1] = dp[i][j];
+                else dp[i+1][j+1] = min( min(dp[i+1][j]+1, dp[i][j+1]+1), dp[i][j]+1);
+            }
+        }
+        return dp[m][n];
+    }
+};
+
+
+/*
+Solution with the 
+1D version
+*/
+
+https://discuss.leetcode.com/topic/17639/20ms-detailed-explained-c-solutions-o-n-space/2
+
+
+
+
