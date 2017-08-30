a9e03ca8bee8d5a00581603db60f0bb98cbdfa63
Subbu <subnr01@gmail.com>
2017-08-30 07:13:07 -0400

    Update closest_palindrome.cpp

--- strings/palindrome/closest_palindrome.cpp
+++ strings/palindrome/closest_palindrome.cpp
@@ -20,17 +20,27 @@ public:
     string nearestPalindromic(string n) {
         int l = n.size();
         set<long> candidates;
+        
         // biggest, one more digit, 10...01
+        //101, 1001, 10001, etc
         candidates.insert(long(pow(10, l)) + 1);
+        
+        //99, 999, 9999
         // smallest, one less digit, 9...9 or 0
         candidates.insert(long(pow(10, l - 1)) - 1);
+        
         // the closest must be in middle digit +1, 0, -1, then flip left to right
+        // Divide the string by half and convert it into a number
         long prefix = stol(n.substr(0, (l + 1) / 2));
+        
+        // Find the other candidates
         for ( long i = -1; i <= 1; ++i ) {
             string p = to_string(prefix + i);
             string pp = p + string(p.rbegin() + (l & 1), p.rend());
             candidates.insert(stol(pp));
         }
+        
+        
         long num = stol(n), minDiff = LONG_MAX, diff, minVal;
         candidates.erase(num);
         for ( long val : candidates ) {
