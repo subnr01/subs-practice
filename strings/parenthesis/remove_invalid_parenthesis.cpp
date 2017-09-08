a8f47d725e04f099a407670f3e07cc56a012e0a5
Subbu <subnr01@gmail.com>
2017-09-08 17:54:08 -0400

    Update remove_invalid_parenthesis.cpp

--- strings/parenthesis/remove_invalid_parenthesis.cpp
+++ strings/parenthesis/remove_invalid_parenthesis.cpp
@@ -11,61 +11,105 @@ Examples:
 
 */
 
-public class Solution {
-    public List<String> removeInvalidParentheses(String s) {
-      List<String> res = new ArrayList<>();
-      
-      // sanity check
-      if (s == null) return res;
-      
-      Set<String> visited = new HashSet<>();
-      Queue<String> queue = new LinkedList<>();
-      
-      // initialize
-      queue.add(s);
-      visited.add(s);
-      
-      boolean found = false;
-      
-      while (!queue.isEmpty()) {
-        s = queue.poll();
-        
-        if (isValid(s)) {
-          // found an answer, add to the result
-          res.add(s);
-          found = true;
+
+// Solution 1
+class Solution {
+public:
+    vector<string> removeInvalidParentheses(string s) {
+        /*** count-the-needed-to-delete-number ***/
+        int left=0, right=0;
+        for(int i=0; i<s.size(); i++){
+            if(s[i]=='(') left++;
+            else if(s[i]==')'){
+                if(left>0) left--;
+                else right++;
+            }
         }
-      
-        if (found) continue;
-      
-        // generate all possible states
-        for (int i = 0; i < s.length(); i++) {
-          // we only try to remove left or right paren
-          if (s.charAt(i) != '(' && s.charAt(i) != ')') continue;
         
-          String t = s.substring(0, i) + s.substring(i + 1);
-        
-          if (!visited.contains(t)) {
-            // for each state, if it's not visited, add it to the queue
-            queue.add(t);
-            visited.add(t);
-          }
-        }
-      }
-      
-      return res;
+        unordered_set<string> result;
+        help(result, "", s, 0, 0, left, right);
+        return vector<string>(result.begin(), result.end());
     }
     
-    // helper function checks if string s contains valid parantheses
-    boolean isValid(String s) {
-      int count = 0;
-    
-      for (int i = 0; i < s.length(); i++) {
-        char c = s.charAt(i);
-        if (c == '(') count++;
-        if (c == ')' && count-- == 0) return false;
-      }
-    
-      return count == 0;
+    void help(unordered_set<string> &result, string cur, string s, int pos, int pair, int left, int right){
+        if(pos==s.size()){
+            if(pair==0 && left==0 && right==0)  result.insert(cur);
+            return;
+        }
+        if(s[pos]=='('){
+            /*** skip ***/
+            if(left>0){
+                help(result, cur, s, pos+1, pair, left-1, right);
+            }
+            /*** keep ***/
+            help(result, cur+'(', s, pos+1, pair+1, left, right);
+        }
+        else if(s[pos]==')'){
+            /*** skip ***/
+            if(right>0){
+                help(result, cur, s, pos+1, pair, left, right-1);
+            }
+            /*** keep ***/
+            /*** bug-places : I forget to check whether pair > 0 ***/
+            if(pair>0){
+                help(result, cur+')', s, pos+1, pair-1, left, right);
+            }
+        }
+        else{
+            help(result, cur+s[pos], s, pos+1, pair, left, right);
+        }
     }
-}
+};
+
+
+
+//Solution 2
+class Solution {
+public:
+    vector<string> removeInvalidParentheses(string s) {
+        vector<string> result;
+        if(s==""){
+            result.push_back(s);
+            return result;
+        } 
+        /*** use the visited to record the visited string ***/
+        unordered_set<string> visited;
+        /*** use the deque to do the BFS ***/
+        deque<string> queue;
+        queue.push_back(s);
+        visited.insert(s);
+        bool found=false;
+        while(!queue.empty()){
+            string temp=queue.front();
+            queue.pop_front();
+            if(check(temp)){
+                result.push_back(temp);
+                found=true;
+            }
+            /*** if found, stop to increase the level depth ***/
+            if(found)  continue;
+            for(int i=0; i<temp.size(); i++){
+                if(temp[i]!='(' && temp[i]!=')') continue;
+                string str=temp.substr(0, i)+temp.substr(i+1);
+                if(visited.find(str)==visited.end()){
+                    queue.push_back(str);
+                    visited.insert(str);
+                }
+            }
+        }
+        return result;
+    }
+    
+    bool check(string s){
+        int count=0;
+        for(int i=0; i<s.size(); i++){
+            char c=s[i];
+            if(c=='(') count++;
+            if(c==')') {
+                if(count==0)    return false; 
+                count--;
+            }
+        }
+        return count==0;
+    }
+};
