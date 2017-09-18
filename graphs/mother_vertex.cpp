51bd3e6e076aa98c496f6562fee765a5cab75a2e
Subbu <subnr01@gmail.com>
2017-09-18 09:39:15 -0400

    Create mother_vertex.cpp

---
+++ graphs/mother_vertex.cpp
@@ -0,0 +1,8 @@
+/*
+Find the mother vertex of a graph.
+Mother vertex is one where all the vertices can be reached 
+from that vertex.
+*/
+
+
+http://www.geeksforgeeks.org/find-a-mother-vertex-in-a-graph/
