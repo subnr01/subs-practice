ea131d60845e075be4ef09af3b0bda816336dae9
Subbu <subnr01@gmail.com>
2017-09-22 21:34:10 -0400

    Update nqueens_2.cpp

--- backtracking/nqueens/nqueens_2.cpp
+++ backtracking/nqueens/nqueens_2.cpp
@@ -1,1 +1,9 @@
 
+/*
+
+The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no 
+two queens attack each other.
+
+Given an integer n, return the total number of distinct solutions.
+
+*/
