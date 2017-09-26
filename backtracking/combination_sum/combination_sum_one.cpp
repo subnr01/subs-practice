10558a80b08c7b0fa625a74e279c03b4ef1bdbe4
Subbu <subnr01@gmail.com>
2017-09-25 22:11:52 -0400

    Update combination_sum_one.cpp

--- backtracking/combination_sum/combination_sum_one.cpp
+++ backtracking/combination_sum/combination_sum_one.cpp
@@ -25,10 +25,11 @@ public:
 private:
     void combinationSum(std::vector<int> &candidates, int target, std::vector<std::vector<int> > &res, 
 			std::vector<int> &combination, int begin) {
-		if  (!target) {
-			res.push_back(combination);
-			return;
-		}
+	if  (!target) {
+	    res.push_back(combination);
+	    return;
+	}
+	    
         for (int i = begin; i != candidates.size() && target >= candidates[i]; ++i) {
             combination.push_back(candidates[i]);
             combinationSum(candidates, target - candidates[i], res, combination, i);
