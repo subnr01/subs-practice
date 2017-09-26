a631c5d3ba227a9c2bccc140455f55c142bb22bb
Subbu <subnr01@gmail.com>
2017-09-25 22:10:14 -0400

    Update combination_sum_three.cpp

--- backtracking/combination_sum/combination_sum_three.cpp
+++ backtracking/combination_sum/combination_sum_three.cpp
@@ -24,28 +24,28 @@ Output:
 
 class Solution {
 public:
-    std::vector<std::vector<int> > combinationSum3(int k, int n) {
-        std::vector<std::vector<int> > res;
+    vector<vector<int>> combinationSum3(int k, int n) { 
+		std::vector<std::vector<int> > res;
         std::vector<int> combination;
-        combinationSum3(n, res, combination, 1, k);
+        int start = 1;
+        combinationSum2(n, res, combination, k, start);
         return res;
     }
 private:
-    void combinationSum3(int target, std::vector<std::vector<int> > &res, std::vector<int> &combination, int begin, int need) {
+    void combinationSum2(int target, std::vector<std::vector<int> > &res, std::vector<int> &combination, int k, int begin) 
+    {
+	if  (combination.size() == k) {
 		if (!target) {
-			res.push_back(combination);
-			return;
-		}
-		else if (!need)
-			return;
-        for (int i = begin; i != 10 && target; ++i) {
-            int max = i * need + need * (need - 1) / 2;
-            if (target< max) {
-                return;
-            }
-			combination.push_back(i);
-			combinationSum3(target - i, res, combination, i + 1, need - 1);
-			combination.pop_back();
+                   res.push_back(combination);
+        	}
+		return;
+	} 
+        
+	for (int i = begin; i < 10 && target >= i; ++i) {
+            combination.push_back(i);
+            combinationSum2(target - i, res, combination, k, i + 1);
+            combination.pop_back();
+            
         }
     }
 };
