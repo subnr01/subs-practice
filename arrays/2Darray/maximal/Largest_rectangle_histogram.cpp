9d7ed47815cbd3057956444eda9df5a9fa77d881
Subbu <subnr01@gmail.com>
2017-10-19 15:18:01 -0400

    Update Largest_rectangle_histogram.cpp

--- arrays/2Darray/maximal/Largest_rectangle_histogram.cpp
+++ arrays/2Darray/maximal/Largest_rectangle_histogram.cpp
@@ -5,3 +5,23 @@ find the area of largest rectangle in the histogram.
 
 */
 
+int largestRectangleArea(vector<int>& height) {
+    height.push_back(0);
+    const int size_h = height.size();
+    stack<int> stk;
+    int i = 0, max_a = 0;
+    while (i < size_h) {
+        if (stk.empty() || height[i] >= height[stk.top()]) {
+          stk.push(i++);
+        } else {
+            int h = stk.top();
+            stk.pop();
+            int H = height[h];
+            int W = (stk.empty() ? i : i - stk.top() - 1);
+            max_a = max(max_a, H * W);
+        }
+    }
+    return max_a;
+}
+
+//Without modifying the vector
