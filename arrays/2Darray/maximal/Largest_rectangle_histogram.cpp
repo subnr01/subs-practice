3c703c0731bc21b5fd9016716446ea563254ec10
Subbu <subnr01@gmail.com>
2017-10-19 15:54:23 -0400

    Update Largest_rectangle_histogram.cpp

--- arrays/2Darray/maximal/Largest_rectangle_histogram.cpp
+++ arrays/2Darray/maximal/Largest_rectangle_histogram.cpp
@@ -5,6 +5,29 @@ find the area of largest rectangle in the histogram.
 
 */
 
+/*
+This problem comes down to basically get the height of the smallest bar and
+width of the entire histogram stretch unless you have another slightly bigger one
+that can marginally give more area.
+
+For example:
+{2,4,2,4} we get 2(smallest height) * width (4 elements) = 8.
+{2,4,2,1} we get 2(smallest height) * widht (3 elements) = 6.
+Note in the second example, even if 1 is the smallest height 1*4 < 2*3
+which emphasises the above point. Another corner case where
+we have really a very tall building that can surpass the 
+width of the entire canvass.
+{2,14,2,4}  = 14
+
+So how do we achieve this?
+
+There is a trick logic.
+
+1. push into the stack until you find a bar of lesser height.
+2. Pop the top, now the top signifies the tallest building and the area of the tallest building
+is height * 1 unit ( this is constant and is calculated by (s.pop(); i - s.top() - 1).
+
+
 int largestRectangleArea(vector<int>& height) {
     height.push_back(0);
     const int size_h = height.size();
