667dff1c23d22dcff2284da85a4b37295e7a1bf2
Subbu <subnr01@gmail.com>
2017-10-23 11:05:02 -0400

    Update rotateImage.cpp

--- arrays/2Darray/rotateImage.cpp
+++ arrays/2Darray/rotateImage.cpp
@@ -1,17 +1,32 @@
 /*
 
-Rotate image
+Rotate image by 90
 
 */
+
+
 
 
 /*
 Rotate image
-*/
 
 1 2 
 3 4
 
+Ans
+3, 1
+4, 2
+
+3*3 example
+1 2 3
+4 5 6
+7 8 9
+
+Ans:
+7 4 1
+8 5 2
+9 6 3
+*/
 
 void rotate(vector<vector<int>>& matrix) {
         const int n = matrix.size();
