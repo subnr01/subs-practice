7a35f65cdca5cfaaab2941d0f5399e7bd38c5977
Subbu <subnr01@gmail.com>
2017-11-04 18:12:04 -0400

    Update subarray_sum.cpp

--- arrays/array_sum/subarray_sum.cpp
+++ arrays/array_sum/subarray_sum.cpp
@@ -12,3 +12,21 @@ The range of numbers in the array is [-1000, 1000] and the range of the integer 
 
 */
 
+
+//https://en.wikipedia.org/wiki/Prefix_sum
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int cum=0; // cumulated sum
+        map<int,int> rec; // prefix sum recorder
+        int cnt = 0; // number of found subarray
+        rec[0]++; // to take into account those subarrays that begin with index 0
+        for(int i=0;i<nums.size();i++){
+            cum += nums[i];
+            cnt += rec[cum-k];
+            rec[cum]++;
+        }
+        return cnt;
+    }
+};
