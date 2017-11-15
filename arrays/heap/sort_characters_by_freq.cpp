92b59426b435f484a50e85e45f29b4d2ea5be56b
Subbu <subnr01@gmail.com>
2017-11-16 02:50:27 +0530

    Update sort_characters_by_freq.cpp

--- arrays/heap/sort_characters_by_freq.cpp
+++ arrays/heap/sort_characters_by_freq.cpp
@@ -42,11 +42,29 @@ Note that 'A' and 'a' are treated as two different characters.
 
 
 
-//Using heap
+//Using heap/priority queue
+class Solution {
+public:
+    string frequencySort(string s)
+    {
+        string ans;
+        int freq[256] = { 0 };
+        priority_queue<pair<int, char>> pq;
+        for (const auto &c : s) ++freq[c];
+        for (int i = 0; i < 256; ++i) {
+            if (freq[i])
+                pq.push(make_pair(freq[i], i));
+        }
+        while (!pq.empty()) {
+            ans.append(pq.top().first, pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+};
 
 
-
-//Using frequency sort
+//Using only map
 class Solution {
 public:
     string frequencySort(string s) {
