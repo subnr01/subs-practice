f1efef07d502d8fb2167fc5a7868cd69b23b046c
Subbu <subnr01@gmail.com>
2017-11-16 03:44:49 +0530

    Create ugly_number_hard.cpp

---
+++ arrays/heap/Ugly_Number/ugly_number_hard.cpp
@@ -0,0 +1,26 @@
+/*
+
+Write a program to find the n-th ugly number.
+
+Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. 
+For example, 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 is the sequence of the first 10 ugly numbers.
+
+Note that 1 is typically treated as an ugly number, and n does not exceed 1690.
+
+
+*/
+
+struct Solution {
+    int nthUglyNumber(int n) {
+        vector <int> results (1,1);
+        int i = 0, j = 0, k = 0;
+        while (results.size() < n)
+        {
+            results.push_back(min(results[i] * 2, min(results[j] * 3, results[k] * 5)));
+            if (results.back() == results[i] * 2) ++i;
+            if (results.back() == results[j] * 3) ++j;
+            if (results.back() == results[k] * 5) ++k;
+        }
+        return results.back();
+    }
+};
