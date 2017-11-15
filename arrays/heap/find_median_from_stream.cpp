72e07548af4420f294e951c460254d0efb35f6aa
Subbu <subnr01@gmail.com>
2017-11-16 02:58:34 +0530

    Create find_median_from_stream.cpp

---
+++ arrays/heap/find_median_from_stream.cpp
@@ -0,0 +1,83 @@
+/*
+Median is the middle value in an ordered integer list. If the size of the list is even,
+there is no middle value. So the median is the mean of the two middle value.
+
+Examples: 
+[2,3,4] , the median is 3
+
+[2,3], the median is (2 + 3) / 2 = 2.5
+
+Design a data structure that supports the following two operations:
+
+void addNum(int num) - Add a integer number from the data stream to the data structure.
+double findMedian() - Return the median of all elements so far.
+For example:
+
+addNum(1)
+addNum(2)
+findMedian() -> 1.5
+addNum(3) 
+findMedian() -> 2
+
+
+
+*/
+
+class MedianFinder {
+    priority_queue<long> small, large;
+public:
+
+    void addNum(int num) {
+        small.push(num);
+        large.push(-small.top());
+        small.pop();
+        if (small.size() < large.size()) {
+            small.push(-large.top());
+            large.pop();
+        }
+    }
+
+    double findMedian() {
+        return small.size() > large.size()
+               ? small.top()
+               : (small.top() - large.top()) / 2.0;
+    }
+};
+
+//Time complexity: O(5 * log(n)) + O(1) \approx O(log(n))O(5∗log(n))+O(1)≈O(log(n)).
+
+At worst, there are three heap insertions and two heap deletions from the top. Each of these takes 
+about O(log(n))O(log(n)) time.
+Finding the mean takes constant O(1)O(1) time since the tops of heaps are directly accessible.
+Space complexity: O(n)O(n) linear space to hold input in containers.
+
+
+
+Leet code solution
+
+class MedianFinder {
+    priority_queue<int> lo;                              // max heap
+    priority_queue<int, vector<int>, greater<int>> hi;   // min heap
+
+public:
+    // Adds a number into the data structure.
+    void addNum(int num)
+    {
+        lo.push(num);                                    // Add to max heap
+
+        hi.push(lo.top());                               // balancing step
+        lo.pop();
+
+        if (lo.size() < hi.size()) {                     // maintain size property
+            lo.push(hi.top());
+            hi.pop();
+        }
+    }
+
+    // Returns the median of current data stream
+    double findMedian()
+    {
+        return lo.size() > hi.size() ? (double) lo.top() : (lo.top() + hi.top()) * 0.5;
+    }
+};
+
