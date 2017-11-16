5bcd06e2f39bf5532c0ecc86053b90eccd547d16
Subbu <subnr01@gmail.com>
2017-11-16 23:09:10 +0530

    Update top_k_frequent_words.cpp

--- arrays/heap/top_k_frequent_words.cpp
+++ arrays/heap/top_k_frequent_words.cpp
@@ -22,36 +22,7 @@ Try to solve it in O(n log k) time and O(n) extra space.
 
 */
 
-class Solution {
-    public List<String> topKFrequent(String[] words, int k) {
-        
-        List<String> result = new LinkedList<>();
-        Map<String, Integer> map = new HashMap<>();
-        for(int i=0; i<words.length; i++)
-        {
-            if(map.containsKey(words[i]))
-                map.put(words[i], map.get(words[i])+1);
-            else
-                map.put(words[i], 1);
-        }
-        
-        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>(
-                 (a,b) -> a.getValue()==b.getValue() ? b.getKey().compareTo(a.getKey()) : a.getValue()-b.getValue()
-        );
-        
-        for(Map.Entry<String, Integer> entry: map.entrySet())
-        {
-            pq.offer(entry);
-            if(pq.size()>k)
-                pq.poll();
-        }
 
-        while(!pq.isEmpty())
-            result.add(0, pq.poll().getKey());
-        
-        return result;
-    }
-}
 
 //C++
 class Solution {
@@ -69,8 +40,10 @@ public:
         my_priority_queue_t  pq(comp);
         
         for(auto w : freq ){
-            pq.emplace(w.first, w.second);
-            if(pq.size()>k) pq.pop();
+            pq.emplace(w.first, w.second); //what does emplace do
+            if(pq.size()>k) {
+                pq.pop(); //check the pop function
+            }
         }
         
         vector<string> output;
