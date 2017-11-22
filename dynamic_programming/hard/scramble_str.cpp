b0cbc2a0da4f7d901f69e3262c63b513d06d1e9c
Subbu <subnr01@gmail.com>
2017-11-22 11:12:52 +0530

    Create scramble_str.cpp

---
+++ dynamic_programming/hard/scramble_str.cpp
@@ -0,0 +1,108 @@
+/*
+
+Given a string s1, we may represent it as a binary tree by partitioning it to two non-empty substrings recursively.
+
+Below is one possible representation of s1 = "great":
+
+    great
+   /    \
+  gr    eat
+ / \    /  \
+g   r  e   at
+           / \
+          a   t
+To scramble the string, we may choose any non-leaf node and swap its two children.
+
+For example, if we choose the node "gr" and swap its two children, it produces a scrambled string "rgeat".
+
+    rgeat
+   /    \
+  rg    eat
+ / \    /  \
+r   g  e   at
+           / \
+          a   t
+We say that "rgeat" is a scrambled string of "great".
+
+Similarly, if we continue to swap the children of nodes "eat" and "at", it produces a scrambled string "rgtae".
+
+    rgtae
+   /    \
+  rg    tae
+ / \    /  \
+r   g  ta  e
+       / \
+      t   a
+We say that "rgtae" is a scrambled string of "great".
+
+Given two strings s1 and s2 of the same length, determine if s2 is a scrambled string of s1.
+
+*/
+
+//https://discuss.leetcode.com/topic/20094/my-c-solutions-recursion-with-cache-dp-recursion-with-cache-and-pruning-with-explanation-4ms
+
+//recursive solution
+ class Solution {
+        bool DP_helper(unordered_map<string, bool> &isScramblePair, string s1, string s2)
+        {
+            int i,len = s1.size();
+            bool res = false;
+            if(0==len) return true;
+            else if(1==len) return s1 == s2;
+            else
+            {
+                if(isScramblePair.count(s1+s2)) return isScramblePair[s1+s2]; // checked before, return intermediate result directly
+                if(s1==s2) res=true;
+                else{
+                    for(i=1; i<len && !res; ++i)
+                    {
+//check s1[0..i-1] with s2[0..i-1] and s1[i..len-1] and s2[i..len-1]
+                        res = res || (DP_helper(isScramblePair, s1.substr(0,i), s2.substr(0,i)) && DP_helper(isScramblePair, s1.substr(i,len-i), s2.substr(i,len-i)));
+ //if no match, then check s1[0..i-1] with s2[len-k.. len-1] and s1[i..len-1] and s2[0..len-i]
+                       res = res || (DP_helper(isScramblePair, s1.substr(0,i), s2.substr(len-i,i)) && DP_helper(isScramblePair, s1.substr(i,len-i), s2.substr(0,len-i)));
+                    }
+                }
+                return isScramblePair[s1+s2]= res; //save the intermediate results
+                
+            }
+        }
+    public:
+        bool isScramble(string s1, string s2) {
+           unordered_map<string, bool> isScramblePair;
+           return DP_helper(isScramblePair, s1, s2);
+        }
+    };
+    
+    
+//DP solution
+
+
+class Solution {
+public:
+    bool isScramble(string s1, string s2) {
+        int sSize = s1.size(), len, i, j, k;
+        if(0==sSize) return true;
+        if(1==sSize) return s1==s2;
+        bool isS[sSize+1][sSize][sSize];
+
+        for(i=0; i<sSize; ++i)
+            for(j=0; j<sSize; ++j)
+                isS[1][i][j] = s1[i] == s2[j];
+                
+        for(len=2; len <=sSize; ++len)
+            for(i=0; i<=sSize-len; ++i)
+                for(j=0; j<=sSize-len; ++j)
+                {
+                    isS[len][i][j] = false;
+                        for(k=1; k<len && !isS[len][i][j]; ++k)
+                        {
+                            isS[len][i][j] = isS[len][i][j] || (isS[k][i][j] && isS[len-k][i+k][j+k]);
+                            isS[len][i][j] = isS[len][i][j] || (isS[k][i+len-k][j] && isS[len-k][i][j+k]);
+                        }
+                }
+        return isS[sSize][0][0];            
+
+    }
+}; 
+
+
