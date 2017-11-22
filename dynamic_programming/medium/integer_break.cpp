67f54396d5189c600117e35836415bedf380728e
Subbu <subnr01@gmail.com>
2017-11-22 12:15:42 +0530

    Update and rename dynamic_programming/integer_break.cpp to dynamic_programming/medium/integer_break.cpp

--- dynamic_programming/medium/integer_break.cpp
+++ dynamic_programming/medium/integer_break.cpp
@@ -20,3 +20,16 @@ public:
         return 3 * integerBreak(n - 3);
     }
 };
+
+
+//dp solution
+public int integerBreak(int n) {
+       int[] dp = new int[n + 1];
+       dp[1] = 1;
+       for(int i = 2; i <= n; i ++) {
+           for(int j = 1; j < i; j ++) {
+               dp[i] = Math.max(dp[i], (Math.max(j,dp[j])) * (Math.max(i - j, dp[i - j])));
+           }
+       }
+       return dp[n];
+    }
