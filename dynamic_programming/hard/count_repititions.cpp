991e386654ea2602db76c8745b64a134b8ef1552
Subbu <subnr01@gmail.com>
2017-11-23 07:10:25 +0530

    Create count_repititions.cpp

---
+++ dynamic_programming/hard/count_repititions.cpp
@@ -0,0 +1,55 @@
+/*
+Define S = [s,n] as the string S which consists of n connected strings s. For example, ["abc", 3] ="abcabcabc".
+
+On the other hand, we define that string s1 can be obtained from string s2 if we can remove some
+characters from s2 such that it becomes s1. For example, “abc” can be obtained from “abdbec” based
+on our definition, but it can not be obtained from “acbbe”.
+
+You are given two non-empty strings s1 and s2 (each at most 100 characters long) and two integers 0 ≤ n1 ≤ 106 
+and 1 ≤ n2 ≤ 106. Now consider the strings S1 and S2, where S1=[s1,n1] and S2=[s2,n2]. Find the maximum integer M such that [S2,M] can be obtained from S1.
+
+Example:
+
+Input:
+s1="acb", n1=4
+s2="ab", n2=2
+
+Return:
+2
+
+
+*/
+
+
+//https://discuss.leetcode.com/topic/72105/c-solution-inspired-by-70664914-with-organized-explanation
+
+class Solution {
+public:
+    int getMaxRepetitions(string s1, int n1, string s2, int n2) {
+        vector<int> repeatCount(s2.size() + 1, 0);
+        vector<int> nextIndex(s2.size() + 1, 0);
+        int j = 0, cnt = 0;
+        for (int k = 1; k <= n1; ++k) {
+            for (int i = 0; i < s1.size(); ++i) {
+                if (s1[i] == s2[j]) {
+                    ++j;
+                    if (j == s2.size()) {
+                        j = 0;
+                        ++cnt;
+                    }
+                }
+            }
+            repeatCount[k] = cnt;
+            nextIndex[k] = j;
+            for (int start = 0; start < k; ++start) {
+                if (nextIndex[start] == j) {
+                    int prefixCount = repeatCount[start];
+                    int patternCount = (repeatCount[k] - repeatCount[start]) * (n1 - start) / (k - start);
+                    int suffixCount = repeatCount[start + (n1 - start) % (k - start)] - repeatCount[start];
+                    return (prefixCount + patternCount + suffixCount) / n2;
+                }
+            }
+        }
+        return repeatCount[n1] / n2;
+    }
+};
