617cf1a8dbf60ff783f8fb59c39f2dbcb6a58d7b
Subbu <subnr01@gmail.com>
2017-12-28 18:53:10 -0500

    Update count_binary_substr.cpp

--- strings/easy/count_binary_substr.cpp
+++ strings/easy/count_binary_substr.cpp
@@ -14,3 +14,32 @@ Notice that some of these substrings repeat and are counted the number of times 
 
 Also, "00110011" is not a valid substring because all the 0's (and 1's) are not grouped together.
 */
+
+
+
+int countBinarySubstrings(string s) {
+    size_t sz = s.size();
+    int curlen = 1;
+    int prevlen = 0;
+    int res = 0;
+    
+    for(int i = 1; i < sz; i++){
+        if(s[i-1] == s[i]) {
+            curlen++;
+        } else {
+            //notice prevlen is updated only
+            //when the prev character does not match.
+            prevlen = curlen;
+            curlen = 1;
+            
+        }
+        // we will always enter the below
+        // if condition in the case
+        // of a non-match. 
+        if(prevlen >= curlen) {
+            res++;
+        }
+        
+    }
+    return res;
+}
