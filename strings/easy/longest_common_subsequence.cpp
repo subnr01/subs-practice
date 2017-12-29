95ed00615a67258229d48dea135a2738e77e371b
Subbu <subnr01@gmail.com>
2017-12-29 10:15:06 -0500

    Update longest_common_subsequence.cpp

--- strings/easy/longest_common_subsequence.cpp
+++ strings/easy/longest_common_subsequence.cpp
@@ -2,11 +2,16 @@
 Longest common subsequence
 
 
-Given a group of two strings, you need to find the longest uncommon subsequence of this group of two strings. The longest uncommon subsequence is defined as the longest subsequence of one of these strings and this subsequence should not be any subsequence of the other strings.
+Given a group of two strings, you need to find the longest uncommon subsequence of this group of two strings. 
+The longest uncommon subsequence is defined as the longest subsequence of one of these strings and this subsequence
+should not be any subsequence of the other strings.
 
-A subsequence is a sequence that can be derived from one sequence by deleting some characters without changing the order of the remaining elements. Trivially, any string is a subsequence of itself and an empty string is a subsequence of any string.
+A subsequence is a sequence that can be derived from one sequence by deleting some 
+characters without changing the order of the remaining elements. Trivially, any string is a 
+subsequence of itself and an empty string is a subsequence of any string.
 
-The input will be two strings, and the output needs to be the length of the longest uncommon subsequence. If the longest uncommon subsequence doesn't exist, return -1.
+The input will be two strings, and the output needs to be the length of the longest uncommon subsequence. 
+If the longest uncommon subsequence doesn't exist, return -1.
 
 Example 1:
 Input: "aba", "cdc"
@@ -23,6 +28,7 @@ Only letters from a ~ z will appear in input strings.
 class Solution {
 public:
     int findLUSlength(string a, string b) {
-        
+        return (a==b)? -1: max(a.size(), b.size());
+    
     }
 };
