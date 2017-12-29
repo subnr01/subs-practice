4b784b79b3187f5d60cc1a3a195ee12c2e4dc44d
Subbu <subnr01@gmail.com>
2017-12-29 08:22:26 -0500

    Create rev_words_str_3.cpp

---
+++ strings/easy/rev_words_str_3.cpp
@@ -0,0 +1,32 @@
+/*
+Given a string, you need to reverse the order of characters in 
+each word within a sentence while still preserving whitespace and initial word order
+
+Example:
+Input: "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+
+In the string, each word is separated by single space and there will not be any extra space in the string.
+*/
+
+
+
+//This logic works because on mainly the words 
+// are seperated by a single space.
+class Solution {
+public:
+    string reverseWords(string s) {
+        size_t front = 0;
+        for(int i = 0; i <= s.length(); ++i){
+            //we need to check for s.length() as 
+            // if we do not do that, then we cannot 
+            // reverse the last word
+            if(i == s.length() || s[i] == ' '){
+                reverse(&s[front], &s[i]);
+                front = i + 1;
+            }
+        }
+        
+        return s;
+    }
+};
