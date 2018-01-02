6149ec6f6e54f2fbee5492ce0021234696429ad9
Subbu <subnr01@gmail.com>
2018-01-02 17:47:21 -0500

    Create pascal_triangle_1.cpp

---
+++ arrays/easy/pascal_triangle_1.cpp
@@ -0,0 +1,27 @@
+/*
+For example, given numRows = 5,
+Return
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+*/
+
+class Solution {
+public:
+   vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res;
+        for(auto i=0;i<numRows;++i)
+        {
+            res.push_back(vector<int>(i+1,1));
+            for(auto j=1; j<i; ++j) 
+            {
+                 res[i][j] = res[i-1][j-1] + res[i-1][j];
+            }
+        }
+        return res;
+    }
+};
