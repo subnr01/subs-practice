361e0292d4445e15443c9994afafae6d7ca3a48e
Subbu <subnr01@gmail.com>
2018-01-02 17:32:23 -0500

    Create first_unique_character.cpp

---
+++ strings/easy/first_unique_character.cpp
@@ -0,0 +1,46 @@
+/*
+Given a string, find the first non-repeating character in it 
+and return it's index. If it doesn't exist, return -1.
+s = "leetcode"
+return 0.
+s = "loveleetcode",
+return 2.
+*/
+
+Points to remember:
+-- Use an unordered map to record the frequency
+-- The first frequency 1 is the character that is the first non repeating character.
+
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char, int> m;
+        for(auto &c : s) {
+            m[c]++;
+        }
+        for(int i = 0; i < s.size(); i++) {
+            if(m[s[i]] == 1) return i;
+        }
+        return -1;
+    }
+};
+
+
+If the string is too long, then we will not traverse it twice.
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char, pair<int, int>> m;
+        int idx = s.size();
+        for(int i = 0; i < s.size(); i++) {
+            m[s[i]].first++;
+            m[s[i]].second = i;
+        }
+        for(auto &p : m) {
+            if(p.second.first == 1) idx = min(idx, p.second.second);
+        }
+        return idx == s.size() ? -1 : idx;
+    }
+};
