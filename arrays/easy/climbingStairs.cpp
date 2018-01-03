ecf203ea141a66907c8bb5ab7e0500dece5521bd
Subbu <subnr01@gmail.com>
2018-01-03 17:56:04 -0500

    Update climbingStairs.cpp

--- arrays/easy/climbingStairs.cpp
+++ arrays/easy/climbingStairs.cpp
@@ -28,14 +28,13 @@ public class Solution {
         if (n == 1) {
             return 1;
         }
-        int first = 1;
-        int second = 2;
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
         for (int i = 3; i <= n; i++) {
-            int third = first + second;
-            first = second;
-            second = third;
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        return second;
+        return dp[n];
     }
 }
 //Time complexity O(n)
