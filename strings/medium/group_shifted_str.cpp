e0417778621edffed41ed2437f5bc605431bd505
Subbu <subnr01@gmail.com>
2018-01-02 23:04:20 -0500

    Create group_shifted_str.cpp

---
+++ strings/medium/group_shifted_str.cpp
@@ -0,0 +1,17 @@
+/*
+Given a string, we can "shift" each of its letter to its successive letter, 
+for example: "abc" -> "bcd". We can keep "shifting" which forms the sequence: "abc" -> "bcd" -> ... -> "xyz".
+
+Given a list of strings which contains only lowercase alphabets, group all strings 
+that belong to the same shifting sequence, return:
+
+ [
+  ["abc","bcd","xyz"],
+  ["az","ba"],
+  ["acef"],
+  ["a","z"]
+]
+
+*/
+
+//https://www.programcreek.com/2014/05/leetcode-group-shifted-strings-java/
