3fb6b6c6a234ac4269a7f5b17f7a3bc6b2626fd9
Subbu <subnr01@gmail.com>
2018-01-02 22:30:24 -0500

    Create shortest_word_distance.cpp

---
+++ arrays/easy/shortest_word_distance.cpp
@@ -0,0 +1,14 @@
+/*
+
+Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
+
+For example,
+Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+
+Given word1 = “coding”, word2 = “practice”, return 3.
+Given word1 = "makes", word2 = "coding", return 1.
+
+
+*/
+
+//https://www.programcreek.com/2014/08/leetcode-shortest-word-distance-java/
