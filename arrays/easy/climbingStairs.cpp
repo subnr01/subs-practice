4d40dbfa1961f63117b61bdf30b55efe6d61bbd9
Subbu <subnr01@gmail.com>
2018-01-03 17:54:46 -0500

    Update climbingStairs.cpp

--- arrays/easy/climbingStairs.cpp
+++ arrays/easy/climbingStairs.cpp
@@ -5,7 +5,7 @@ Each time you can either climb 1 or 2 steps. In how many distinct ways can you c
 
 */
 
-
+//Recursion with Memoziation
 class Solution {
 public:
     map <int, int> curry = {
@@ -19,3 +19,63 @@ public:
         return num;
     }
 };
+Time complexity : O(n). Size of recursion tree can go upto nn
+Space complexity : O(n). The depth of recursion tree can go upto nn.
+
+//Dynamic programming soln
+public class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int first = 1;
+        int second = 2;
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
+    }
+}
+//Time complexity O(n)
+//Space complexity O(n)
+
+
+//Fibonacci number
+public class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int first = 1;
+        int second = 2;
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
+    }
+}
+//Time complexity is O(n)
+//Space Complexity is O(1)
+
+//Brute force
+public class Solution {
+    public int climbStairs(int n) {
+        climb_Stairs(0, n);
+    }
+    public int climb_Stairs(int i, int n) {
+        if (i > n) {
+            return 0;
+        }
+        if (i == n) {
+            return 1;
+        }
+        return climb_Stairs(i + 1, n) + climb_Stairs(i + 2, n);
+    }
+}
+
+//Complexity O(2^n), Size of recursion tree will be 2^n
+
