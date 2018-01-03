58c4e9477e9077f97f20284ff77a535f17bbc0df
Subbu <subnr01@gmail.com>
2018-01-02 22:56:22 -0500

    Create min_time_difference.cpp

---
+++ strings/medium/min_time_difference.cpp
@@ -0,0 +1,10 @@
+/*
+
+Given a list of 24-hour clock time points in "Hour:Minutes" format,
+find the minimum minutes difference between any two time points in the list.
+Example 1:
+Input: ["23:59","00:00"]
+Output: 1
+
+
+*/
