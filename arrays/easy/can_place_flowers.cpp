5b97e9ca84878f787ac93b6db26a770814fc9129
Subbu <subnr01@gmail.com>
2018-01-03 17:41:46 -0500

    Update can_place_flowers.cpp

--- arrays/easy/can_place_flowers.cpp
+++ arrays/easy/can_place_flowers.cpp
@@ -21,6 +21,7 @@ public:
         int count = 0;
         
         while(i < n) {
+            //watch the (i == n-1; very important)
             if(flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && 
                (i == n - 1 || flowerbed[i+1] == 0))
                {
