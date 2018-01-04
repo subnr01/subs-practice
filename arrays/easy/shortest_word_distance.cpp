e153507f4024fe80378ac871d09c5a04d17f649f
Subbu <subnr01@gmail.com>
2018-01-03 19:44:40 -0500

    Update shortest_word_distance.cpp

--- arrays/easy/shortest_word_distance.cpp
+++ arrays/easy/shortest_word_distance.cpp
@@ -12,3 +12,23 @@ Given word1 = "makes", word2 = "coding", return 1.
 */
 
 //https://www.programcreek.com/2014/08/leetcode-shortest-word-distance-java/
+
+
+int shortestDistance(vector<string>& words, string word1, string word2) {
+    int id1 = -1, id2 = -1;
+    int minDist = INT_MAX;
+    for (int i = 0; i < words.size(); i++) {
+        if (words[i] == word1) {
+            id1 = i;
+            if (id2 != -1) {
+                minDist = min(minDist, id1 - id2);
+            }
+        } else if (words[i] == word2) {
+            id2 = i;
+            if (id1 != -1) {
+                minDist = min(minDist, id2 - id1);
+            }
+        }
+    }
+    return minDist;
+}
