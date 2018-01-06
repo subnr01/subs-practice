57de37e431c50a8df7553423fb70cfecf175e1c9
Subbu <subnr01@gmail.com>
2018-01-06 16:22:55 -0500

    Update rotated_array_search_med.cpp

--- arrays/rotated_array_search/rotated_array_search_med.cpp
+++ arrays/rotated_array_search/rotated_array_search_med.cpp
@@ -9,40 +9,79 @@ Write a function to determine if a given target is in the array.
 The array may contain duplicates.
 */
 
+// There are two solns.
+// One soln is to find the index of the minimum element, so that
+// array can be broken into two halves and the right half is used
+// for search.
+
+// The other solution is normal binary search on the entire array,
+// which moves moving to the right segment where the target may 
+// exist
+
+//Second soln
 class Solution {
 public:
-    bool search(vector<int>& a, int target) {
-        
-        int sz = a.size();
-        
-        int high = sz - 1;
-        int low  = 0;
-        
-        while (low < high) {
-            int mid = (high - low)/2 + low;
-            
-            if (a[mid] == target) return true;
-            
-            if (a[mid] > a[high]) {
-                if (a[mid] >target && target >= a[low]) {
-                    high = mid;
-                } else {
-                    low = mid + 1;
-                }
-                
-            } else if (a[mid] < a[high]) {
-                if (a[mid] < target && target <= a[high]) {
-                    low = mid + 1;
-                } else {
-                    high = mid;
-                }
-            } else {
-                //to account for duplicates
-                high--;
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size()-1;
+        while (l <= r) {
+            int mid = (l+r) / 2;
+            if (target == nums[mid])
+                return mid;
+            // there exists rotation; the middle element is in the left part of the array
+            if (nums[mid] > nums[r]) {
+                if (target < nums[mid] && target >= nums[l])
+                    r = mid - 1;
+                else
+                    l = mid + 1;
             }
-            
+            // there exists rotation; the middle element is in the right part of the array
+            else if (nums[mid] < nums[l]) {
+                if (target > nums[mid] && target <= nums[r])
+                    l = mid + 1;
+                else
+                    r = mid - 1;
+            }
+            // there is no rotation; just like normal binary search
+            else {
+                if (target < nums[mid])
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            }
         }
-        return a[low] == target?true:false;
+        return -1;
     }
 };
 
+
+//first soln
+class Solution {
+public:
+    int search(vector<int>& A, int target) {
+        int n = A.size();
+        if (n == 0) {
+            return -1;
+        }
+        int lo=0,hi=n-1;
+        // find the index of the smallest value using binary search.
+        // Loop will terminate since mid < hi, and lo or hi will shrink by at least 1.
+        // Proof by contradiction that mid < hi: if mid==hi, then lo==hi and loop would have been terminated.
+        while(lo<hi){
+            int mid=(lo+hi)/2;
+            if(A[mid]>A[hi]) lo=mid+1;
+            else hi=mid;
+        }
+        // lo==hi is the index of the smallest value and also the number of places rotated.
+        int rot=lo;
+        lo=0;hi=n-1;
+        // The usual binary search and accounting for rotation.
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            int realmid=(mid+rot)%n;
+            if(A[realmid]==target)return realmid;
+            if(A[realmid]<target)lo=mid+1;
+            else hi=mid-1;
+        }
+        return -1;
+    }
+};
