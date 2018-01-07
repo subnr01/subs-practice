dcb88ef83ae97a9add502e4884f46983f1fbefc8
Subbu <subnr01@gmail.com>
2018-01-07 11:20:54 -0500

    Update calendar_1.cpp

--- arrays/medium/Calendar/calendar_1.cpp
+++ arrays/medium/Calendar/calendar_1.cpp
@@ -24,6 +24,18 @@ The third event can be booked, as the first event takes every time less than 20,
 
 */
 
+//Easier soln
+class MyCalendar {
+    vector<pair<int, int>> books;
+public:
+    bool book(int start, int end) {
+        for (pair<int, int> p : books)
+            if (max(p.first, start) < min(end, p.second)) return false;
+        books.push_back({start, end});
+        return true;
+    }
+};
+
 
 class MyCalendar {
     
