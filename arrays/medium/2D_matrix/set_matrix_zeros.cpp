4b74f664da80ba0510a5fc6878df569177b3a269
Subbu <subnr01@gmail.com>
2018-01-07 14:52:21 -0500

    Update set_matrix_zeros.cpp

--- arrays/medium/2D_matrix/set_matrix_zeros.cpp
+++ arrays/medium/2D_matrix/set_matrix_zeros.cpp
@@ -3,6 +3,28 @@ Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do
 */
 
 
+/*
+Logic:
+
+If the element of the matrix is 0, then set the first element in the same row to 0
+and set the first element in the same column to 0.
+If it is actually the first element in the corresponding row or column, then 
+mark them as visited.
+
+Traverse the matrix again in a normal way and if you find that the first element
+in the row or column is set to 0, then set all the elments to 0.
+
+Now there is the special case of the visited, where the first element in the row
+or column is zero. If you follow the above logic to this case, only the row
+or column is set to 0 and not both. So this is need to be handled as a 
+seperate case.
+
+If visited was the first element of a col, then set all the elements of that col to 0
+and vice versa.
+
+*/
+
+
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
@@ -29,12 +51,14 @@ public:
         
         /* the following code sets the first column to 0*/
         if(col){
+            //Notice i is compared against number of rows.
             for(int i = 0; i < matrix.size(); i++) {
                 matrix[i][0] = 0;
             }
         }
         /* the following code sets the first row to 0*/
         if(row){
+            //Notice j is compared against the column size.
             for(int j = 0; j < matrix[0].size(); j++) {
                 matrix[0][j] = 0;
             }
