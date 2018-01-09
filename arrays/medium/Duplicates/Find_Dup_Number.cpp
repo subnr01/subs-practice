25d51bc820525485152fc1cb21eac85d9f385253
Subbu <subnr01@gmail.com>
2018-01-08 20:10:13 -0500

    Update Find_Dup_Number.cpp

--- arrays/medium/Duplicates/Find_Dup_Number.cpp
+++ arrays/medium/Duplicates/Find_Dup_Number.cpp
@@ -39,3 +39,34 @@ public:
 	return -1;
 }
 };
+
+
+
+//Using Set
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<Integer>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return num;
+            }
+            seen.add(num);
+        }
+
+        return -1;
+    }
+}
+
+//Using Sorting
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i-1]) {
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+}
