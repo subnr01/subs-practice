cf4a27242e6966cd153c1c799a300c7a91543592
Subbu <subnr01@gmail.com>
2018-01-11 10:59:07 -0500

    Create k_smallest_pair_dist.cpp

---
+++ arrays/hard/k_smallest_pair_dist.cpp
@@ -0,0 +1,40 @@
+Given an integer array, return the k-th smallest distance among all the pairs. The distance of a pair (A, B)
+is defined as the absolute difference between A and B.
+
+Example 1:
+Input:
+nums = [1,3,1]
+k = 1
+Output: 0 
+Explanation:
+Here are all the pairs:
+(1,3) -> 2
+(1,1) -> 0
+(3,1) -> 2
+Then the 1st smallest distance pair is (1,1), and its distance is 0.
+
+Note:
+2 <= len(nums) <= 10000.
+0 <= nums[i] < 1000000.
+1 <= k <= len(nums) * (len(nums) - 1) / 2.
+
+
+class Solution {
+public:
+    int smallestDistancePair(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size(), low = 0, high = 1000000;
+        while (low < high) {
+            int mid = (low + high)/2, cnt = 0;
+            for (int i = 0, j = 0; i < n; i++) {
+                while (j < n && nums[j]-nums[i] <= mid) j++;
+                cnt += j-i-1;
+            }
+            if (cnt < k) 
+                low = mid+1;
+            else
+                high = mid;
+        }
+        return low;
+    }
+};
