79c28e9490eb43f7187394d812de4b642cac1f03
Subbu <subnr01@gmail.com>
2018-01-11 10:57:00 -0500

    Create min_rotated_sorted_hard.cpp

---
+++ arrays/hard/min_rotated_sorted_hard.cpp
@@ -0,0 +1,45 @@
+/*
+Follow up for "Find Minimum in Rotated Sorted Array":
+What if duplicates are allowed?
+
+Would this affect the run-time complexity? How and why?
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+Find the minimum element.
+
+
+
+*/
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        
+    int low = 0;
+    int high = nums.size()-1;
+    
+    while (low < high && nums[low] == nums[high]) {
+            high -= 1;  // or left += 1; if index is not asked
+    }
+    
+    while (low < high)
+    {
+        if (nums[low] < nums[high]) {
+            return nums[low];
+        }
+        
+        int mid = (low + high)/2;
+        
+        if (nums[low] > nums[mid])
+        {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+    
+    return nums[low];    
+    }
+};
