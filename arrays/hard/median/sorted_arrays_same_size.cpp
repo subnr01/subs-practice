efbe9a307a8ff4987436f95f39ec728ce0f81248
Subbu <subnr01@gmail.com>
2018-01-11 11:02:33 -0500

    Create sorted_arrays_same_size.cpp

---
+++ arrays/hard/median/sorted_arrays_same_size.cpp
@@ -0,0 +1,67 @@
+/*
+
+Median of two sorted arrays
+There are two sorted arrays nums1 and nums2 of size m and n respectively.
+
+Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
+Example 1:
+nums1 = [1, 3]
+nums2 = [2]
+
+The median is 2.0
+Example 2:
+nums1 = [1, 2]
+nums2 = [3, 4]
+
+The median is (2 + 3)/2 = 2.5
+
+
+*/
+https://discuss.leetcode.com/topic/4996/share-my-o-log-min-m-n-solution-with-explanation/6
+
+/* This function returns median of ar1[] and ar2[].
+   Assumptions in this function:
+   Both ar1[] and ar2[] are sorted arrays
+   Both have n elements */
+int getMedian(int ar1[], int ar2[], int n)
+{
+    /* return -1  for invalid input */
+    if (n <= 0)
+        return -1;
+    if (n == 1)
+        return (ar1[0] + ar2[0])/2;
+    if (n == 2)
+        return (max(ar1[0], ar2[0]) + min(ar1[1], ar2[1])) / 2;
+ 
+    int m1 = median(ar1, n); /* get the median of the first array */
+    int m2 = median(ar2, n); /* get the median of the second array */
+ 
+    /* If medians are equal then return either m1 or m2 */
+    if (m1 == m2)
+        return m1;
+ 
+    /* if m1 < m2 then median must exist in ar1[m1....] and
+        ar2[....m2] */
+    if (m1 < m2)
+    {
+        if (n % 2 == 0)
+            return getMedian(ar1 + n/2 - 1, ar2, n - n/2 +1);
+        return getMedian(ar1 + n/2, ar2, n - n/2);
+    }
+ 
+    /* if m1 > m2 then median must exist in ar1[....m1] and
+        ar2[m2...] */
+    if (n % 2 == 0)
+        return getMedian(ar2 + n/2 - 1, ar1, n - n/2 + 1);
+    return getMedian(ar2 + n/2, ar1, n - n/2);
+}
+ 
+/* Function to get median of a sorted array */
+int median(int arr[], int n)
+{
+    if (n%2 == 0)
+        return (arr[n/2] + arr[n/2-1])/2;
+    else
+        return arr[n/2];
+}
