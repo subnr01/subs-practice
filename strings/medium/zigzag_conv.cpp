53a4d0b015011a476786c28c9724724a3ec4ebeb
Subbu <subnr01@gmail.com>
2018-01-13 07:23:34 -0500

    Update zigzag_conv.cpp

--- strings/medium/zigzag_conv.cpp
+++ strings/medium/zigzag_conv.cpp
@@ -14,4 +14,31 @@ convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
 
 */
 
+//NOT A GOOD QUESTION
 
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if( numRows==1 ) return s;
+        vector<string> res(numRows);
+        int row=0;
+        int increase=-1;
+        for(int i=0; i<s.size(); ++i){
+            
+            // every time at turning point, we change the increase direction
+            if (i%(numRows-1)==0) {
+                increase *= -1;
+            }
+            
+            res[row].push_back(s[i]);
+            row += increase;
+        }
+        
+        string ret;
+        for(const auto& str:res){
+            ret += str;
+        }
+        
+        return ret;
+    }
+};
