037635736b2b09b195e7567410ade96e1b949eb3
Subbu <subnr01@gmail.com>
2018-01-15 17:05:25 -0500

    Create group_anagrams.cpp

---
+++ strings/medium/group_anagrams.cpp
@@ -0,0 +1,77 @@
+/*
+Given an array of strings, group anagrams together.
+
+For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"], 
+Return:
+
+[
+  ["ate", "eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+
+
+*/
+
+Points to remember:
+1. Use a hashmap with the sorted version of the string as the key.
+2. Traverse all the strings one by one and put them in the appropriate bucket with the sorted version as the key.
+3. Traverse the hashmap and push elements of each bucket into a vector of strings.
+https://discuss.leetcode.com/topic/21038/10-lines-76ms-easy-c-solution-updated-function-signature/2
+
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, multiset<string>> mp;
+        for (string s : strs) {
+            string t = s; 
+            sort(t.begin(), t.end());
+            mp[t].insert(s);
+        }
+        vector<vector<string>> anagrams;
+        for (auto m : mp) { 
+            vector<string> anagram(m.second.begin(), m.second.end());
+            anagrams.push_back(anagram);
+        }
+        return anagrams;
+    }
+};
+
+
+Update: as suggested by yswu1234 in the answer, general sort takes O(nlogn) time. 
+  In this problem, since the string only contains lower-case alphabets, we can write a sorting 
+  function using counting sort (O(n) time) to speed up the sorting process. I write a string sorting
+  function strSort below and using it to sort the string achieves the overall running time 72ms for
+    this problem.
+
+
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, multiset<string>> mp;
+        for (string s : strs) {
+            string t = strSort(s);
+            mp[t].insert(s);
+        }
+        vector<vector<string>> anagrams;
+        for (auto m : mp) { 
+            vector<string> anagram(m.second.begin(), m.second.end());
+            anagrams.push_back(anagram);
+        }
+        return anagrams;
+    }
+private:
+    string strSort(string& s) {
+        int count[26] = {0}, n = s.length();
+        for (int i = 0; i < n; i++)
+            count[s[i] - 'a']++;
+        int p = 0;
+        string t(n, 'a');
+        for (int j = 0; j < 26; j++)
+            for (int i = 0; i < count[j]; i++)
+                t[p++] += j;
+        return t;
+    } 
+};
