b9dc464fe843d0960440939cf2324184b6599616
Subbu <subnr01@gmail.com>
2018-01-15 11:52:04 -0500

    Update complex_num_multi.cpp

--- strings/medium/complex_num_multi.cpp
+++ strings/medium/complex_num_multi.cpp
@@ -19,3 +19,30 @@ The input strings will be given in the form of a+bi, where the integer a and b w
 to the range of [-100, 100]. And the output should be also in this form.
 
 */
+
+
+class Solution {
+public:
+    string complexNumberMultiply(string a, string b) {
+        pair<int, int> av = parse(a);
+        pair<int, int> bv = parse(b);
+        //Note there is subtraction here
+        int ra = av.first * bv.first - av.second * bv.second;
+        //Note there is addition here
+        int rb = av.first * bv.second + av.second * bv.first;
+        return to_string(ra) + "+" + to_string(rb) + "i";
+    }
+
+    pair<int, int> parse(const string& a) {
+        //begin to plus
+        int plus = find(a.begin(), a.end(), '+') - a.begin();
+        //begin to i        
+        int i = find(a.begin(), a.end(), 'i') - a.begin();
+      
+        // 0 to plus position
+        int ra = stoi(a.substr(0, plus));
+        // plus + 1 to i -plus
+        int rb = stoi(a.substr(plus + 1, i - plus));
+        return {ra, rb};
+    }
+};
