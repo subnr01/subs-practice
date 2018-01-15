35fd3bbc6d82c5213fbd60486004f1c97e7e71e3
Subbu <subnr01@gmail.com>
2018-01-15 11:08:55 -0500

    Update min_time_difference.cpp

--- strings/medium/min_time_difference.cpp
+++ strings/medium/min_time_difference.cpp
@@ -8,3 +8,59 @@ Output: 1
 
 
 */
+
+
+//n log n solution
+class Solution {
+public:
+    int findMinDifference(vector<string>& timePoints) {
+        int n = timePoints.size();
+        if (n > 1440) return 0;
+        vector<int> times;
+        for (int i = 0; i < n; i++) 
+            times.push_back(to_min(timePoints[i]));
+        sort(times.begin(), times.end());
+        int ans = times[0]+60*24-times.back();
+        for (int i = 1; i < n; ++i) {
+            ans = min(ans, times[i]-times[i-1]);
+        }
+        return ans;
+    }
+private:
+    int to_min(string& a) {
+        int h = (a[0]-'0')*10+(a[1]-'0'), m = (a[3]-'0')*10+(a[4]-'0');
+        return 60*h+m;
+    }
+};
+
+//array solution without using sorting
+public class Solution {
+    public int findMinDifference(List<String> timePoints) {
+        boolean[] mark = new boolean[24 * 60];
+        for (String time : timePoints) {
+            String[] t = time.split(":");
+            int h = Integer.parseInt(t[0]);
+            int m = Integer.parseInt(t[1]);
+            if (mark[h * 60 + m]) return 0;
+            mark[h * 60 + m] = true;
+        }
+        
+        int prev = 0, min = Integer.MAX_VALUE;
+        int first = Integer.MAX_VALUE, last = Integer.MIN_VALUE;
+        for (int i = 0; i < 24 * 60; i++) {
+            if (mark[i]) {
+                if (first != Integer.MAX_VALUE) {
+                    min = Math.min(min, i - prev);
+                }
+                first = Math.min(first, i);
+                last = Math.max(last, i);
+                prev = i;
+            }
+        }
+        
+        min = Math.min(min, (24 * 60 - last + first));
+        
+        return min;
+    }
+}
+
