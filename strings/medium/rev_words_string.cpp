1063905166fca5f4fa1dc67c7ecb065cc6d6190f
Subbu <subnr01@gmail.com>
2018-01-15 12:38:22 -0500

    Update rev_words_string.cpp

--- strings/medium/rev_words_string.cpp
+++ strings/medium/rev_words_string.cpp
@@ -11,19 +11,38 @@ return "blue is sky the".
 
 class Solution {
 public:
-
-   void reverseWords(string &s) {
-    reverse(s.begin(), s.end());
-    int storeIndex = 0;
-    for (int i = 0; i < s.size(); i++) {
-        if (s[i] != ' ') {
-            if (storeIndex != 0) s[storeIndex++] = ' ';
-            int j = i;
-            while (j < s.size() && s[j] != ' ') { s[storeIndex++] = s[j++]; }
-            reverse(s.begin() + storeIndex - (j - i), s.begin() + storeIndex);
-            i = j;
+    void reverseWords(string &s) {
+       
+        //reverse the entire string
+        reverse (s.begin(), s.end());
+        
+        int n = s.size();
+        int storeindex = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] != ' ')
+            {
+               //word boundary
+                if (storeindex != 0) {
+                    s[storeindex++] = ' ';
+                }
+                int j = i;
+                while ( j < n && s[j] != ' ') 
+                {
+                    s[storeindex++] = s[j++];
+                }
+                //begin to end of the word, begin to the beginning of the word
+                reverse (s.begin() + storeindex - (j-i), s.begin() + storeindex);
+                
+                i = j;
+            }
+            
+            //Delete space from storeindex to end of the string
+            // so as to compress the string at the end and get rid of
+            //spaces
+            s.erase (s.begin() + storeindex, s.end());
+            
         }
+        
     }
-    s.erase(s.begin() + storeIndex, s.end());
-}
 };
