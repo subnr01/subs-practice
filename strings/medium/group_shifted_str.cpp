1f75e7573db7f8cc1dc0999723750c5ba4aafd41
Subbu <subnr01@gmail.com>
2018-01-15 23:00:40 -0500

    Update group_shifted_str.cpp

--- strings/medium/group_shifted_str.cpp
+++ strings/medium/group_shifted_str.cpp
@@ -15,3 +15,60 @@ that belong to the same shifting sequence, return:
 */
 
 //https://www.programcreek.com/2014/05/leetcode-group-shifted-strings-java/
+
+//https://discuss.leetcode.com/topic/20823/4ms-easy-c-solution-with-explanations
+
+
+class Solution {
+public:
+    vector<vector<string>> groupStrings(vector<string>& strings) {
+        unordered_map<string, vector<string> > mp;
+        for (string  s : strings)
+            mp[shift(s)].push_back(s);
+        vector<vector<string> > groups;
+        for (auto m : mp) {
+            vector<string> group = m.second;
+            sort(group.begin(), group.end());
+            groups.push_back(group);
+        }
+        return groups;
+    }
+private:
+    string shift(string& s) {
+        string t;
+        int n = s.length();
+        for (int i = 1; i < n; i++) {
+            int diff = s[i] - s[i - 1];
+            if (diff < 0) diff += 26;
+            t += 'a' + diff + ',';
+        }
+        return t;
+    }
+};
+
+
+//Another soln
+vector<vector<string>> groupStrings(vector<string>& strings) {
+	unordered_map<string, vector<string>> hmap;
+
+	for (int i = 0; i<strings.size(); i++) {
+		string t = strings[i];
+		int diff = t[0] - 'a';
+		for (int j = 0; j<strings[i].size(); j++) {
+			if (t[j] - diff - 'a' >= 0)
+				t[j] = t[j] - diff;
+			else 
+				t[j] = (t[j] - diff) + 26;
+		}
+
+		hmap[t].push_back(strings[i]);
+	}
+
+	vector<vector<string>> result;
+	for (auto iter = hmap.begin(); iter != hmap.end(); ++iter) {
+		result.push_back(iter->second);
+		sort(result.back().begin(), result.back().end());
+	}
+
+	return result;
+}
