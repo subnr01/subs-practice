c025d8343c91f97d2024c88998f0424ce5c905c4
Subbu <subnr01@gmail.com>
2018-01-15 21:25:53 -0500

    Update encode_decode_strings.cpp

--- strings/medium/encode_decode_strings.cpp
+++ strings/medium/encode_decode_strings.cpp
@@ -19,3 +19,34 @@ strs2 in Machine 2 should be the same as strs in Machine 1.
 Implement the encode and decode methods.
 
 */
+
+
+class Codec {
+public:
+
+    // Encodes a list of strings to a single string.
+    string encode(vector<string>& strs) {
+        string encoded = "";
+        for (string &str: strs) {
+            int len = str.size();
+            encoded += to_string(len) + "@" + str;
+        }
+        
+        return encoded;
+    }
+
+    // Decodes a single string to a list of strings.
+    vector<string> decode(string s) {
+        vector<string> r;
+        int head = 0;
+        while (head < s.size())    {
+            int at_pos = s.find_first_of('@', head);
+            int len = stoi(s.substr(head, at_pos - head));
+            head = at_pos + 1;
+            r.push_back(s.substr(head, len));
+            head += len;
+        }
+        
+        return r;
+    }
+};
