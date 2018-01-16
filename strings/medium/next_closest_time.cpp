57aaef044b3c88cfe4077d8a70b017ce62dbd92c
Subbu <subnr01@gmail.com>
2018-01-15 22:46:03 -0500

    Update next_closest_time.cpp

--- strings/medium/next_closest_time.cpp
+++ strings/medium/next_closest_time.cpp
@@ -19,3 +19,23 @@ Explanation: The next closest time choosing from digits 2, 3, 5, 9, is 22:22.
 It may be assumed that the returned time is next day's time since it is smaller than the input time numerically.
 
 */
+
+//Alexander
+
+class Solution {
+    int mins[4] = { 600, 60, 10, 1 };
+public:
+    string nextClosestTime(string time) {
+        size_t colon = time.find(':');
+        int cur = stoi(time.substr(0, colon)) * 60 + stoi(time.substr(colon + 1));
+        string next = "0000";
+        for (int i = 1, d = 0; i <= 1440 && d < 4; i++) {
+            int m = (cur + i) % 1440;
+            for (d = 0; d < 4; d++) {
+                next[d] = '0' + m / mins[d]; m %= mins[d];
+                if (time.find(next[d]) == string::npos) break;
+            }
+        }
+        return next.substr(0, 2) + ':' + next.substr(2, 2);
+    }
+};
