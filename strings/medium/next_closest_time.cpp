c0db2b3409a8218200899f0eda3d2645cce3ad72
Subbu <subnr01@gmail.com>
2018-01-16 19:55:25 -0500

    Update next_closest_time.cpp

--- strings/medium/next_closest_time.cpp
+++ strings/medium/next_closest_time.cpp
@@ -21,7 +21,8 @@ It may be assumed that the returned time is next day's time since it is smaller 
 */
 
 //Alexander
-
+//WEll this runs 1440 * x times, but is a brilliant solution. 
+// There is a optimised soln that can run only 256 times.
 class Solution {
     int mins[4] = { 600, 60, 10, 1 };
 public:
@@ -39,3 +40,59 @@ public:
         return next.substr(0, 2) + ':' + next.substr(2, 2);
     }
 };
+
+
+//Java soln
+  int diff = 0x3f3f3f3f;
+    String result = "";
+    int h;
+    int m;
+    public String nextClosestTime(String time) {
+        int[] digit = new int[4];
+        int tot = 0;
+        String[] val = time.split(":");
+        int hour = Integer.parseInt(val[0]);
+        int minu = Integer.parseInt(val[1]);
+        digit[tot++] = hour / 10;
+        digit[tot++] = hour % 10;
+        digit[tot++] = minu / 10;
+        digit[tot++] = minu % 10;
+
+        h = hour;
+        m = minu;
+
+        dfs(digit, 0, new int[4]);
+
+        return result;
+    }
+
+    void dfs(int[] digit, int i, int[] ans) {
+        if (i == 4) {
+            int hour = 10 * ans[0] + ans[1];
+            int minu = 10 * ans[2] + ans[3];
+            if (hour >= 0 && hour <= 23 && minu >= 0 && minu <= 59) {
+                int df = diff(hour, minu);
+                if (df < diff) {
+                    diff = df;
+                    result = valid(hour) + ":" + valid(minu);
+                }
+            }
+        }
+        else {
+            for (int j = 0; j < 4; ++j) {
+                ans[i] = digit[j];
+                dfs(digit, i + 1, ans);
+            }
+        }
+    }
+
+    int diff(int hour, int minu) {
+        int c2o = 60 * 60 - h * 60 - m;
+        int n2o = 60 * 60 - hour * 60 - minu;
+        return n2o < c2o ? c2o - n2o : c2o - n2o + 3600;
+    }
+
+    public String valid(int time) {
+        if (time >= 0 && time <= 9) return "0" + time;
+        else return time + "";
+    }
