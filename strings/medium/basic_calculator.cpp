90c176aca577de38aeaec4da1f8910d55735cae2
Subbu <subnr01@gmail.com>
2018-01-18 00:21:43 -0500

    Update basic_calculator.cpp

--- strings/medium/basic_calculator.cpp
+++ strings/medium/basic_calculator.cpp
@@ -15,6 +15,50 @@ Note: Do not use the eval built-in library function.
 */
 
 
+int calculate(string s) {
+    stack<int> myStack;
+    char sign = '+';
+    int res = 0, tmp = 0;
+    for (unsigned int i = 0; i < s.size(); i++) {
+        if (isdigit(s[i]))
+            tmp = 10*tmp + s[i]-'0';
+        //if it is not a digit
+        if (!isdigit(s[i]) && !isspace(s[i]) || i == s.size()-1) {
+            if (sign == '-')
+                myStack.push(-tmp);
+            else if (sign == '+')
+                myStack.push(tmp);
+            else {
+                //if sign is not '+' or '-'
+                //multiply with top and pop, and
+                //put it back in
+                
+                int num;
+                if (sign == '*' )
+                    num = myStack.top()*tmp;
+                else
+                    num = myStack.top()/tmp;
+                //Replacing the top with the
+                //result
+                myStack.pop();
+                myStack.push(num);
+            }
+            //temp and sign reset
+            sign = s[i];
+            tmp = 0;
+        }
+    }
+    
+    //Here is where addition happens
+    while (!myStack.empty()) {
+        res += myStack.top();
+        myStack.pop();
+    }
+    return res;
+}
+
+
+//stephen
 class Solution {
 public:
     int calculate(string s) {
