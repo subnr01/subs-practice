ef8a3ad11bd5b628b64fb73d9c547666a95e3805
Subbu <subnr01@gmail.com>
2018-01-18 05:25:53 -0500

    Update and rename arrays/Pascal_triange/valid triangle.cpp to arrays/medium/valid triangle.cpp

---
+++ arrays/medium/valid triangle.cpp
@@ -0,0 +1,77 @@
+/*
+
+Given an array consists of non-negative integers, 
+your task is to count the number of triplets chosen from the array that can make triangles 
+if we take them as side lengths of a triangle.
+
+
+Example 1:
+Input: [2,2,3,4]
+Output: 3
+Explanation:
+Valid combinations are: 
+2,3,4 (using the first 2)
+2,3,4 (using the second 2)
+2,2,3
+Note:
+The length of the given array won't exceed 1000.
+The integers in the given array are in the range of [0, 1000].
+
+
+*/
+
+//2 Pointers O(n^2logn)
+// space complexity: O(logn) for sorting
+class Solution {
+public:
+    int triangleNumber(vector<int>& a) {
+        int n = a.size();
+        sort(a.begin(), a.end());
+        int res = 0;
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                int sum = a[i] + a[j];   // the sum of 2 shortest sides;
+                int firstGE = firstGreatOrEqual(a, j + 1, sum);
+                res += firstGE - 1 - j;
+            }
+        }
+        return res;
+    }
+
+private:
+    int firstGreatOrEqual(vector<int>& a, int i0, int target) {
+        int lo = i0, hi = a.size();
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            if (a[mid] < target) {
+                lo = mid + 1;
+            }
+            else {
+                hi = mid;
+            }
+        }
+        return hi;
+    }
+};
+
+
+//2 Pointers O(n^2)
+// space complexity: O(logn)  for sorting
+
+class Solution {
+public:
+    int triangleNumber(vector<int>& a) {
+        int res = 0;
+        sort(a.begin(), a.end());
+        reverse(a.begin(), a.end());    // a is decreasing
+        for (int i = 0; i + 2 < a.size(); i++) {
+            for (int j = i + 1, k = a.size() - 1; j < k; j++) {
+                while (j < k && a[j] + a[k] <= a[i]) {
+                    k--;
+                }
+                res += k - j;
+            }
+        }
+        return res;
+    }
+};
