552055f64c9702fb285c96e2e6fd6bb4c7abb7ab
Subbu <subnr01@gmail.com>
2018-01-19 10:36:56 -0500

    Update wild_card_matching.cpp

--- strings/hard/matching/wild_card_matching.cpp
+++ strings/hard/matching/wild_card_matching.cpp
@@ -21,3 +21,95 @@ isMatch("aab", "c*a*b") → false
 
 
 */
+
+
+https://discuss.leetcode.com/topic/21577/my-three-c-solutions-iterative-16ms-dp-180ms-modified-recursion-88ms
+
+
+
+//DP
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int pLen = p.size(), sLen = s.size(), i, j, k, cur, prev;
+        if(!pLen) return sLen == 0;
+        bool matched[2][sLen+1];
+        fill_n(&matched[0][0], 2*(sLen+1), false);
+        
+        matched[0][0] = true;
+        for(i=1; i<=pLen; ++i)
+        {
+            cur = i%2, prev= 1-cur;
+            matched[cur][0]= matched[prev][0] && p[i-1]=='*';
+            if(p[i-1]=='*') for(j=1; j<=sLen; ++j) matched[cur][j] = matched[cur][j-1] || matched[prev][j];
+            else for(j=1; j<=sLen; ++j)            matched[cur][j] =  matched[prev][j-1] && (p[i-1]=='?' || p[i-1]==s[j-1]) ;
+        }
+            return matched[cur][sLen];
+    }
+};
+
+
+//Another soln
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int  slen = s.size(), plen = p.size(), i, j, iStar=-1, jStar=-1;
+
+        for(i=0,j=0 ; i<slen; ++i, ++j)
+        {
+            if(p[j]=='*')
+            { //meet a new '*', update traceback i/j info
+                iStar = i;
+                jStar = j;
+                --i;
+            }
+            else
+            { 
+                if(p[j]!=s[i] && p[j]!='?')
+                {  // mismatch happens
+                    if(iStar >=0)
+                    { // met a '*' before, then do traceback
+                        i = iStar++;
+                        j = jStar;
+                    }
+                    else return false; // otherwise fail
+                }
+            }
+        }
+        while(p[j]=='*') ++j;
+        return j==plen;
+    }
+};
+
+
+
+//recursive
+class Solution {
+private:
+    bool helper(const string &s, const string &p, int si, int pi, int &recLevel)
+    {
+        int sSize = s.size(), pSize = p.size(), i, curLevel = recLevel;
+        bool first=true;
+        while(si<sSize && (p[pi]==s[si] || p[pi]=='?')) {++pi; ++si;} //match as many as possible
+        if(pi == pSize) return si == sSize; // if p reaches the end, return
+        if(p[pi]=='*')
+        { // if a star is met
+            while(p[++pi]=='*'); //skip all the following stars
+            if(pi>=pSize) return true; // if the rest of p are all star, return true
+            for(i=si; i<sSize; ++i)
+            {   // then do recursion
+                if(p[pi]!= '?' && p[pi]!=s[i]) continue;
+                if(first) {++recLevel; first = false;}
+                if(helper(s, p, i, pi, recLevel)) return true;
+                if(recLevel>curLevel+1) return false; // if the currently processed star is not the last one, return
+            }
+        }
+        return false;
+    }
+public:
+    bool isMatch(string s, string p) {
+        int recLevel = 0;
+        return helper(s, p, 0, 0, recLevel);
+    }
+};
+
