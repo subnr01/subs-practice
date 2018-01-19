4792addb07b95ca15ebcb7f5e926f78f4a2c380f
Subbu <subnr01@gmail.com>
2018-01-19 04:35:01 -0500

    Update missing_ranges.cpp

--- arrays/medium/range/missing_ranges.cpp
+++ arrays/medium/range/missing_ranges.cpp
@@ -6,3 +6,30 @@ return its missing ranges.
 For example, given [0, 1, 3, 50, 75], lower = 0 and upper = 99, return ["2", "4->49", "51->74", "76->99"].
 */
 
+
+class Solution {
+    public:
+        vector<string> findMissingRanges(vector<int>& nums, int lower, int upper) {
+            vector<string> ranges;
+            int next = lower;
+            for (int i = 0; i < nums.size(); i++ ) {
+                //if (nums[i] < next)  continue;
+                if (nums[i] == next) {
+                    next ++;
+                    continue;
+                }
+                ranges.push_back(getRange(next, nums[i] - 1));
+                next = nums[i] + 1;
+            }
+            if(next <= upper)  ranges.push_back(getRange(next, upper));
+            return ranges;
+        }
+        
+        string getRange(const int lower, const int upper) {
+            if (lower == upper) {
+                return to_string(lower);
+            } else {
+                return to_string(lower) + "->" + to_string(upper);
+            }
+        }
+    };
