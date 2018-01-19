5d98941c2513f9e186afcbf477e1168d5b9ed08e
Subbu <subnr01@gmail.com>
2018-01-19 10:56:04 -0500

    Update text_justification.cpp

--- strings/hard/text_justification.cpp
+++ strings/hard/text_justification.cpp
@@ -20,3 +20,23 @@ Return the formatted lines as:
 Note: Each word is guaranteed not to exceed L in length.
 
 */
+class Solution {
+public:
+    vector<string> fullJustify(vector<string> &words, int L) {
+    vector<string> res;
+    for(int i = 0, k, l; i < words.size(); i += k) {
+        for(k = l = 0; i + k < words.size() and l + words[i+k].size() <= L - k; k++) {
+            l += words[i+k].size();
+        }
+        string tmp = words[i];
+        for(int j = 0; j < k - 1; j++) {
+            if(i + k >= words.size()) tmp += " ";
+            else tmp += string((L - l) / (k - 1) + (j < (L - l) % (k - 1)), ' ');
+            tmp += words[i+j+1];
+        }
+        tmp += string(L - tmp.size(), ' ');
+        res.push_back(tmp);
+    }
+    return res;
+}
+};
