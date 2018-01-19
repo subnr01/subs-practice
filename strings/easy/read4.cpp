6a4300a09261192914d112ebe9a4c53d729016cf
Subbu <subnr01@gmail.com>
2018-01-19 06:13:00 -0500

    Create read4.cpp

---
+++ strings/easy/read4.cpp
@@ -0,0 +1,36 @@
+/*
+The API: int read4(char *buf) reads 4 characters at a time from a file.
+
+The return value is the actual number of characters read. For example, it returns 3 if there 
+is only 3 characters left in the file.
+
+By using the read4 API, implement the function int read(char *buf, int n) that reads n characters from the file.
+
+*/
+
+//Check for out of bounds in this soln
+
+
+// Forward declaration of the read4 API.
+int read4(char *buf);
+
+class Solution {
+public:
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    int read(char *buf, int n) {
+    int res = 0;
+    while (n > 0) {
+        int tmp = min(read4(buf), n);
+        res += tmp;
+        buf += tmp;
+        if (tmp < 4)
+            break;
+        n -= 4;
+    }
+    return res;
+}
+};
