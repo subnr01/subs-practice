dd623b5548ede3712fb8fecdce2367d5e00d4006
Subbu <subnr01@gmail.com>
2018-01-19 17:02:04 -0500

    Update AlienDictionary.cpp

--- graphs/hard/AlienDictionary.cpp
+++ graphs/hard/AlienDictionary.cpp
@@ -56,3 +56,48 @@ string alienOrder(vector<string>& words) {
     }
     return order.size() == chars.size() ? order : "";
 }
+
+
+
+//very fast
+class Solution {
+public:
+    string alienOrder(vector<string>& words) {
+        vector<unordered_map<char, int>> pre(26, unordered_map<char, int>());
+        vector<bool> used(26, false);
+        vector<int> value(26, 0);
+        int n = words.size(), len = 0;
+        for (int i = 0; i < n; i++) {
+            for (const auto& iter : words[i]) {
+                used[iter - 'a'] = true;
+            }
+            if (i == 0) { continue; }
+            int pos = 0;
+            while (pos < words[i - 1].length() && words[i - 1][pos] == words[i][pos]) { pos++; }
+            if (pos >= words[i - 1].length()) { continue; }
+            pre[words[i - 1][pos] - 'a'][words[i][pos]]++;
+            value[words[i][pos] - 'a']++;
+        }
+        queue<int> wait_queue;
+        for (int i = 0; i < 26; i++) {
+            len += (used[i] ? 1 : 0);
+            if (used[i] && value[i] == 0) {
+                wait_queue.push(i);
+            }
+        }
+        string ans;
+        while (!wait_queue.empty()) {
+            int ch = wait_queue.front();
+            wait_queue.pop();
+            ans += char(ch + 'a');
+            for (const auto& iter : pre[ch]) {
+                value[iter.first - 'a'] -= iter.second;
+                if (value[iter.first - 'a'] == 0) {
+                    wait_queue.push(iter.first - 'a');
+                }
+            }
+        }
+        if (ans.length() != len) { return ""; }
+        return ans;
+    }
+};
