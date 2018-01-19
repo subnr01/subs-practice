38e8b13d48c53ea80f057b6b75d641b405504df5
Subbu <subnr01@gmail.com>
2018-01-19 05:37:39 -0500

    Update longest_consec_sequence.cpp

--- arrays/hard/longest_consec_sequence.cpp
+++ arrays/hard/longest_consec_sequence.cpp
@@ -31,3 +31,19 @@ public:
         return res;
     }
 };
+
+
+//Another similar soln (if you do not like erase)
+int longestConsecutive(const vector<int> &num) {
+    unordered_set<int> s(num.begin(), num.end()), searched;
+    int longest = 0;
+    for (int i: num) {
+        if (searched.find(i) != searched.end()) continue;
+        searched.insert(i);
+        int j = i - 1, k = i + 1;
+        while (s.find(j) != s.end()) searched.insert(j--);
+        while (s.find(k) != s.end()) searched.insert(k++);
+        longest = max(longest, k - 1 - j);
+    }
+    return longest;
+}
