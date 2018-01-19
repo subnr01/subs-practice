417276d6e905a71f99df0e7471225d0d1b723869
Subbu <subnr01@gmail.com>
2018-01-19 04:32:42 -0500

    Update wriggle_sort.cpp

--- arrays/medium/wriggle_sort.cpp
+++ arrays/medium/wriggle_sort.cpp
@@ -5,3 +5,26 @@ Given an unsorted array nums, reorder it in-place such that nums[0] <= nums[1] >
 For example, given nums = [3, 5, 2, 1, 6, 4], one possible answer is [1, 6, 2, 5, 3, 4].
 
 */
+
+/*
+fighter:
+
+The final sorted nums needs to satisfy two conditions:
+
+If i is odd, then nums[i] >= nums[i - 1];
+If i is even, then nums[i] <= nums[i - 1].
+The code is just to fix the orderings of nums that do not satisfy 1 and 2.
+
+
+*/
+
+
+class Solution {
+public: 
+    void wiggleSort(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 1; i < n; i++)
+            if (((i & 1) && nums[i] < nums[i - 1]) || (!(i & 1) && nums[i] > nums[i - 1]))
+                swap(nums[i], nums[i - 1]);
+    } 
+};
