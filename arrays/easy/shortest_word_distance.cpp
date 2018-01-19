dc6646cac26e75854ff51cdc92821018f3388ee4
Subbu <subnr01@gmail.com>
2018-01-19 05:24:18 -0500

    Update shortest_word_distance.cpp

--- arrays/easy/shortest_word_distance.cpp
+++ arrays/easy/shortest_word_distance.cpp
@@ -14,21 +14,40 @@ Given word1 = "makes", word2 = "coding", return 1.
 //https://www.programcreek.com/2014/08/leetcode-shortest-word-distance-java/
 
 
-int shortestDistance(vector<string>& words, string word1, string word2) {
+
+/*
+
+Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
+
+For example,
+Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+
+Given word1 = “coding”, word2 = “practice”, return 3.
+Given word1 = "makes", word2 = "coding", return 1.
+
+Note:
+You may assume that word1 does not equal to word2, and word1 and word2 are both in the list.
+
+*/
+
+class Solution {
+public:
+   int shortestDistance(vector<string>& words, string word1, string word2) {
     int id1 = -1, id2 = -1;
     int minDist = INT_MAX;
     for (int i = 0; i < words.size(); i++) {
         if (words[i] == word1) {
             id1 = i;
-            if (id2 != -1) {
-                minDist = min(minDist, id1 - id2);
-            }
+            
         } else if (words[i] == word2) {
             id2 = i;
-            if (id1 != -1) {
-                minDist = min(minDist, id2 - id1);
-            }
+           
+        }
+        
+        if ((id2 != -1) && (id1 != -1)) {
+            minDist = min(minDist, abs(id2 - id1));
         }
     }
     return minDist;
 }
+};
