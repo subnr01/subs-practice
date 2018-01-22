d8892a5fc57b2aa6db36e36ad0d032dc74f043a6
Subbu <subnr01@gmail.com>
2018-01-21 22:20:24 -0500

    Create gen_parenthesis.cpp

---
+++ strings/medium/gen_parenthesis.cpp
@@ -0,0 +1,61 @@
+/*
+
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+For example, given n = 3, a solution set is:
+
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+
+*/
+
+Related topics: String, backtracking
+
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        
+        vector<string> result;
+        if (n < 1) {
+            return result;
+        }
+        
+        string s;
+        func(result, s, n, n); 
+        
+        return result;
+        
+    }
+    
+    
+    void func(vector<string> &result, string s, int left, int right)
+    {
+        if ( left > right) {
+                return;
+        }
+        
+        if (left + right == 0) {
+            result.push_back(s);
+            return;
+        }
+        
+        if (left > 0) {
+            s.push_back('(');
+            func(result, s, left-1, right);
+            s.pop_back();
+        }
+        
+        if (right > 0) {
+            s.push_back(')');
+            func(result, s, left, right - 1);
+            s.pop_back();
+        }
+        
+        return;
+    }
+};
