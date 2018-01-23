fc2fad11a4b45312e57eebf718be32ec39abf3c2
Subbu <subnr01@gmail.com>
2018-01-23 13:30:32 -0500

    Update max_swap.cpp

--- arrays/medium/max_swap.cpp
+++ arrays/medium/max_swap.cpp
@@ -16,6 +16,8 @@ Explanation: No swap.
 
 */
 
+//Related topics
+// array, math
 
 class Solution {
 public:
@@ -40,4 +42,32 @@ private:
             }
         }
     }
+};
+
+
+//DP
+class Solution {
+public:
+    int maximumSwap(int num) {
+    string numString = to_string(num);
+    int n = numString.length();
+    vector<int> dpPosition(n, -1);
+        
+    int curMaxPos = n - 1;
+    for (int i = n - 1; i >= 0; i--) {
+        if (numString[i] > numString[curMaxPos]) {
+            curMaxPos = i;
+        }
+        dpPosition[i] = curMaxPos;
+    }
+        
+    for (int i = 0; i < n; i++) {
+        if(numString[dpPosition[i]] != numString[i]) {
+            swap(numString[i], numString[dpPosition[i]]);
+            break;
+        }
+    }
+        
+    return stoi(numString);
+}
 };
