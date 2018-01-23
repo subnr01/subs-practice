ea33336a3749f9aacead8ff3f4985a5693dd11f0
Subbu <subnr01@gmail.com>
2018-01-23 16:33:15 -0500

    Update Find_all_Dup.cpp

--- arrays/medium/Duplicates/Find_all_Dup.cpp
+++ arrays/medium/Duplicates/Find_all_Dup.cpp
@@ -16,6 +16,8 @@ public:
     vector<int> findDuplicates(vector<int>& nums) {
         vector<int> res;
         int i = 0;
+        
+        //nums is {4,3,2,7,8,2,3,1}
         while (i < nums.size()) {
             /* what is the secret sauce in this algorithm */
             if (nums[i] != nums[nums[i]-1]) {
@@ -25,6 +27,8 @@ public:
                 i++;
             }
         }
+        
+        //at this stage, nums is {1,2,3,4,3,2,7,8}
         for (i = 0; i < nums.size(); i++) {
             if (nums[i] != i + 1) {
                 res.push_back(nums[i]);
