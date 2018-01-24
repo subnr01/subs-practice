d2c2545db6cb49a1707a48bd5473927387eae6f2
Subbu <subnr01@gmail.com>
2018-01-24 11:40:30 -0500

    Update unique_paths_2.cpp

--- arrays/medium/unique_paths/unique_paths_2.cpp
+++ arrays/medium/unique_paths/unique_paths_2.cpp
@@ -13,7 +13,51 @@ There is one obstacle in the middle of a 3x3 grid as illustrated below.
 The total number of unique paths is 2
 */
 
+//unoptimised version
+class Solution {
+    public:
+        int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+            int m = grid.size();
+            int n = m == 0 ? 0 : grid[0].size();
+            if( !m || !n || grid[m-1][n-1] == 1 ) {
+                return 0;
+            }
+            
+            vector<vector<int>> dp( m, vector<int>(n, 0));
+            
+            for( int i = 0; i < m; i++) 
+            {
+                if( grid[i][0] != 1 ) {
+                    dp[i][0] = 1;
+                }
+                else {
+                    break;
+                }
+            }
+    
+            for (int i = 0; i < n; i++ ) 
+            {
+                if( grid[0][i] != 1 ) { 
+                    dp[0][i] = 1; 
+                }
+                else { 
+                    break;
+                }
+            }
+            
+            for( int i = 1; i < m; i++ ) { 
+                for( int j = 1; j < n; j++ ) {
+                    int a = grid[i-1][j] == 1? 0: dp[i-1][j];
+                    int b = grid[i][j-1] == 1? 0: dp[i][j-1];
+                    dp[i][j] = a + b;
+                }
+            }
+            
+            return dp[m-1][n-1];
+        }
+};
 
+//optimised version
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
