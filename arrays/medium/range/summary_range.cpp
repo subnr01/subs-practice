e86881009676a18b0140c9658c5a5fd4e9ba2d40
Subbu <subnr01@gmail.com>
2018-01-24 13:05:27 -0500

    Update summary_range.cpp

--- arrays/medium/range/summary_range.cpp
+++ arrays/medium/range/summary_range.cpp
@@ -3,33 +3,32 @@
 For example, given [0,1,2,4,5,7], return ["0->2","4->5","7"].
 */
 
-
+//my soln
 class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
-    int n = nums.size();
-    vector<string> res;
-    int i = 0;
-    bool consecutive = false;
-    while(i<n){
-        res.push_back(to_string(nums[i]));
-        int j = i+1;
-        while(j<n && nums[j]==nums[j-1]+1) {
-            ++j;
-            consecutive = true;
+        int start = -1;
+        int end = -1;
+        string s;
+        vector<string> res;
+        int n = nums.size();
+        int i = 0;
+        while (i < n)
+        {
+            start = i;
+            while(i != n - 1  && nums[i + 1] == nums[i] + 1) {
+                i++;
+            }
+            string s = to_string(nums[start]);
+            if (start != i) {    
+                end = i;
+                s += "->" + to_string(nums[end]);
+            } 
+            i++;
+            
+            res.push_back(s);
         }
-        
-        //more than one consecutive number
-        if(consecutive){
-            // we are using (j-1) here and not j.
-            res.back()+="->"+to_string(nums[j-1]);
-            consecutive = false;
-        } 
-        
-        //i is not incremented, this is a while loop
-        i = j;
+        return res;
         
     }
-    return res;
-}
 };
