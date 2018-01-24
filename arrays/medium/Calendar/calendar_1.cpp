4badcfb145faf95bbec7d581fac4023c204377c3
Subbu <subnr01@gmail.com>
2018-01-24 18:48:21 -0500

    Update calendar_1.cpp

--- arrays/medium/Calendar/calendar_1.cpp
+++ arrays/medium/Calendar/calendar_1.cpp
@@ -29,11 +29,18 @@ class MyCalendar {
     vector<pair<int, int>> books;
 public:
     bool book(int start, int end) {
-        for (pair<int, int> p : books)
-            if (max(p.first, start) < min(end, p.second)) return false;
-        books.push_back({start, end});
+        for (auto elem: bookings)
+        {
+            if (max(elem.first, start) < min(elem.second, end)) 
+            {
+                return false;
+            }
+        }
+        bookings.push_back({start, end});
         return true;
+        
     }
+    
 };
 
 
