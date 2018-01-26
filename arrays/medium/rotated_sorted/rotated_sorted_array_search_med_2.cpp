d3acdf058e21f1187b17ba301d4af37128a0c82a
Subbu <subnr01@gmail.com>
2018-01-25 20:01:17 -0500

    Update rotated_sorted_array_search_med_2.cpp

--- arrays/medium/rotated_sorted/rotated_sorted_array_search_med_2.cpp
+++ arrays/medium/rotated_sorted/rotated_sorted_array_search_med_2.cpp
@@ -13,8 +13,46 @@ Write a function to determine if a given target is in the array.
 The array may contain duplicates.
 
 */
+class Solution {
+public:
+    bool search(vector<int>& a, int target) {
+        
+        int sz = a.size();
+        if (!sz) {
+            return false;
+        }
+        
+        int high = sz - 1;
+        int low  = 0;
+        
+        while (low < high) {
+            int mid = (high - low)/2 + low;
+            
+            if (a[mid] == target) return true;
+            
+            if (a[mid] > a[high]) {
+                if (a[mid] >target && target >= a[low]) {
+                    high = mid;
+                } else {
+                    low = mid + 1;
+                }
+                
+            } else if (a[mid] < a[high]) {
+                if (a[mid] < target && target <= a[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid;
+                }
+            } else {
+                high--;
+            }
+            
+        }
+        return a[low] == target?true:false;
+    }
+};	
 
-
+//Linear search
 class Solution {
 
 	public:
