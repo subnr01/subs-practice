41571fbe66c8aa200b3f35b4c21965d1cba920b7
Subbu <subnr01@gmail.com>
2018-01-27 10:11:35 -0500

    Update k_smallest_matrix.cpp

--- heap/medium/k_smallest_matrix.cpp
+++ heap/medium/k_smallest_matrix.cpp
@@ -20,6 +20,36 @@ You may assume k is always valid, 1 ≤ k ≤ n2.
 */
 
 
+//my soln
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        priority_queue<int, vector<int>, greater<int>> pq;
+        
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++) {
+                pq.push(matrix[i][j]);    
+            }
+            
+        }
+        
+        int i = 0;
+        while(i < k - 1)
+        {
+            pq.pop();
+            i++;
+            
+        }
+        
+        return pq.top();
+        
+    }
+};
+
 //binary search
     int kthSmallest(vector<vector<int>>& matrix, int k) {
         int n = matrix.size();
