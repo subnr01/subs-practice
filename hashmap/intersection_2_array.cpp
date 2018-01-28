aaa209299526a4e74fd86b1d315ce09af11e413c
Subbu <subnr01@gmail.com>
2018-01-28 15:20:39 -0500

    Update intersection_2_array.cpp

--- hashmap/intersection_2_array.cpp
+++ hashmap/intersection_2_array.cpp
@@ -33,24 +33,18 @@ public:
 };
 
 
-/*
-Given two arrays, write a function to compute their intersection.
-
-Example:
-Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2, 2].
-
-Note:
-Each element in the result should appear as many times as it shows in both arrays.
-The result can be in any order.
-Follow up:
-What if the given array is already sorted? How would you optimize your algorithm?
-What if nums1's size is small compared to num2's size? Which algorithm is better?
-What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
-
-*/
-
-
-
-
-
-
+//using set
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        set<int> res, st(nums1.begin(), nums1.end());
+        
+        for(int i = 0; i < nums2.size(); ++i){
+            if(st.count(nums2[i])) {
+                res.insert(nums2[i]);
+            }
+        }
+        
+        return vector<int> (res.begin(), res.end());
+    }
+};
