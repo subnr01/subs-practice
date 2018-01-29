97e97354db54ee72a7e7eb1ff5f21e4d897f44a2
Subbu <subnr01@gmail.com>
2018-01-28 21:25:50 -0500

    Create distribute_candles.cpp

---
+++ hashmap/easy/distribute_candles.cpp
@@ -0,0 +1,35 @@
+/*
+
+Given an integer array with even length, where different numbers in this array represent different kinds of candies. Each number means one candy of the corresponding kind. You need to distribute these candies equally in number to brother and sister. Return the maximum number of kinds of candies the sister could gain.
+
+
+Example 1:
+Input: candies = [1,1,2,2,3,3]
+Output: 3
+Explanation:
+There are three different kinds of candies (1, 2 and 3), and two candies for each kind.
+Optimal distribution: The sister has candies [1,2,3] and the brother has candies [1,2,3], too. 
+The sister has three different kinds of candies. 
+
+
+Example 2:
+Input: candies = [1,1,2,3]
+Output: 2
+Explanation: For example, the sister has candies [2,3] and the brother has candies [1,1]. 
+The sister has two different kinds of candies, the brother has only one kind of candies. 
+
+*/
+
+
+class Solution {
+public:
+    int distributeCandies(vector<int>& candies) {
+        unordered_set<int> kinds;
+        for (int kind : candies) {
+            kinds.insert(kind);
+        }
+        int a = kinds.size();
+        int b = candies.size()/2;
+        return min(a, b);
+    }
+};
