832ca05d9326858b3c66607b86924b2308d223ea
Subbu <subnr01@gmail.com>
2018-01-29 21:32:59 -0500

    Update tree_vertical_order_traversal.cpp

--- hashmap/medium/tree_vertical_order_traversal.cpp
+++ hashmap/medium/tree_vertical_order_traversal.cpp
@@ -61,3 +61,55 @@ return its vertical order traversal as:
 ]
 
 */
+
+//hash table and queue
+class Solution {
+public:
+    vector<vector<int>> verticalOrder(TreeNode* root) {
+        vector<vector<int> > res;
+        if(!root) return res;
+        
+        map<int, vector<int> > m; 
+        queue<pair<int, TreeNode* > > q;
+        q.push({0, root});
+        
+        while(!q.empty()){
+            auto t = q.front(); q.pop();
+            m[t.first].push_back(t.second -> val);
+            if(t.second -> left) q.push({t.first - 1, t.second -> left});
+            if(t.second -> right) q.push({t.first + 1, t.second -> right});
+        }
+        
+        for(auto it : m){
+            res.push_back(it.second);
+        }
+        return res;
+    }
+};
+
+
+//recursion
+class Solution {
+public:
+    vector<vector<int>> verticalOrder2(TreeNode* root) {
+        vector<vector<int>> res;
+        helper(root, 0, res);
+        return res;
+    }
+    
+    void helper(TreeNode* root, int idx, vector<vector<int>>& res) {
+        if (!root) return;
+        if (idx == res.size()) {
+            res.push_back(vector<int>(1, root->val));
+        }
+        else if (idx == -1) {
+            res.insert(res.begin(), vector<int>(1, root->val));
+            idx = 0;
+        }
+        else {
+            res[idx].push_back(root->val);
+        }
+        helper(root->left, idx-1, res);
+        helper(root->right, idx+1, res);
+    }
+};
