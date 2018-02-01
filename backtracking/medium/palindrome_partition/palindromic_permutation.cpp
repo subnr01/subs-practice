7e36ee85772da858a81a1565accb87efbdefa496
Subbu <subnr01@gmail.com>
2018-01-31 22:28:37 -0500

    Update palindromic_permutation.cpp

--- backtracking/medium/palindrome_partition/palindromic_permutation.cpp
+++ backtracking/medium/palindrome_partition/palindromic_permutation.cpp
@@ -18,37 +18,51 @@ Given s = "abc", return [].
 class Solution {
 public:
     vector<string> generatePalindromes(string s) {
-		vector<string> palindromes;
+        vector<string> palindromes;
         unordered_map<char, int> counts;
-        for (char c : s) counts[c]++;
-        int odd = 0; char mid; string half;
+        for (char c : s) {
+            counts[c]++;
+        }
+        
+        int odd = 0;
+        char mid;
+        string half;
+        
         for (auto p : counts) {
             if (p.second & 1) {
-                odd++, mid = p.first;
-				if (odd > 1) return palindromes;
+                odd++;
+                mid = p.first;
+                if (odd > 1) {
+                    return palindromes;
+                }
             }
             half += string(p.second / 2, p.first);
         }
+        
         palindromes = permutations(half);
+        
         for (string& p : palindromes) {
             string t(p);
             reverse(t.begin(), t.end());
-			if (odd) t = mid + t;
+            if (odd) {
+                t = mid + t;
+            }
             p += t;
         }
         return palindromes;
     }
-private: 
+private:
     vector<string> permutations(string& s) {
         vector<string> perms;
         string t(s);
         do {
             perms.push_back(s);
-            next_permutation(s.begin(), s.end()); 
+            next_permutation(s.begin(), s.end());
         } while (s != t);
-        return perms; 
+        return perms;
     }
 };
+
 
 //using backtracking
 class Solution {
