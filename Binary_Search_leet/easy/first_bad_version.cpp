73c8a55aea1a48c0b9492af54a1f46f8cc4541a2
Subbu <subnr01@gmail.com>
2018-02-03 23:36:14 -0500

    Update first_bad_version.cpp

--- Binary_Search_leet/easy/first_bad_version.cpp
+++ Binary_Search_leet/easy/first_bad_version.cpp
@@ -5,14 +5,23 @@ You are given an API bool isBadVersion(version) which will return whether versio
 
 */
 
-public class Solution extends VersionControl {
-    public int firstBadVersion(int n) {
-    int start = 1, end = n;
-    while (start < end) {
-        int mid = start + (end-start) / 2;
-        if (!isBadVersion(mid)) start = mid + 1;
-        else end = mid;            
-    }        
-    return start;
-}
-}
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int low = 1;
+        int high = n;
+        
+        while (low <= high)
+        {
+            int mid = (high - low)/2 + low;
+            
+            if (isBadVersion(mid) == false) {
+                low = mid + 1;
+            } else
+            {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+};
