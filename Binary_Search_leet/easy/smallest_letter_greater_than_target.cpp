301b57ecda99c6a491275d24feb93dd9187641dc
Subbu <subnr01@gmail.com>
2018-02-04 00:43:43 -0500

    Update smallest_letter_greater_than_target.cpp

--- Binary_Search_leet/easy/smallest_letter_greater_than_target.cpp
+++ Binary_Search_leet/easy/smallest_letter_greater_than_target.cpp
@@ -42,13 +42,36 @@ Output: "c"
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        if (letters.back() <= target) return letters.front();
-        int low = 0, high = letters.size() - 1;
-        while (low < high) {
-            auto mid = (low + high) / 2;
-            if (target < letters[mid]) high = mid;
-            else low = mid + 1;
+        int n = letters.size();
+        
+        if (n > 0 && target >= letters.back()) {
+            return letters.front();
         }
+        
+        int low = 0;
+        int high = n - 1;
+        
+        
+        //remember (0 + 1)/2 = 0
+        
+        //Low is not equal to high in this case
+        while(low < high)
+        {
+            int mid = (low + high)/2;
+            
+            if (letters[mid] > target)
+            {
+                high = mid ;
+            } 
+            // we cannot have a seperate block for
+            // "=" as mid + 1 can be the same number.
+            
+            else if (letters[mid] <= target) { 
+                low = mid + 1;
+            } 
+           
+        }
+        
         return letters[low];
     }
 };
