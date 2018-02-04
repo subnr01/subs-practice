1235d1415fcc6676ff847c64ae5512a6371eb441
Subbu <subnr01@gmail.com>
2018-02-03 23:06:29 -0500

    Update two_arrays_intersection_2.cpp

--- Binary_Search_leet/easy/two_arrays_intersection_2.cpp
+++ Binary_Search_leet/easy/two_arrays_intersection_2.cpp
@@ -59,3 +59,31 @@ public:
         return ans;
     }
 };
+
+
+//Soln with hashmap
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+
+        unordered_map<int,int> map;
+        vector<int> result;
+
+        for(int i=0;i<nums1.size();i++)
+        {
+            map[nums1[i]]++;
+        }
+
+        for(int i=0;i<nums2.size();i++)
+        {
+            if(map[nums2[i]] > 0)
+            {
+                result.push_back(nums2[i]);
+                map[nums2[i]] -= 1;
+            }
+        }
+
+        return result;
+
+    }
+};
