e9659c818d92c1a6b5b7e8af5ccc5bdf85422df7
Subbu <subnr01@gmail.com>
2018-02-04 00:11:22 -0500

    Update closest_binary_search_tree_val.cpp

--- Binary_Search_leet/easy/closest_binary_search_tree_val.cpp
+++ Binary_Search_leet/easy/closest_binary_search_tree_val.cpp
@@ -10,33 +10,27 @@ Recursive:
 
 class Solution {
 public:
-    void getResult(TreeNode *node, double target, double &result){
-        if(!node)return;
-        if(abs((double)node->val-target)<abs(target-result))result=(double)node->val;
-        if((double)node->val>target)
-            getResult(node->left,target,result);
-        else if((double)node->val<target)
-            getResult(node->right,target,result);
-    }
     int closestValue(TreeNode* root, double target) {
-        double result=(double)root->val;
-        getResult(root,target,result);
-        return (int)result;
-    }
-};
-Iterative:
-
-class Solution {
-public:
-    int closestValue(TreeNode* root, double target) {
-        int result=root->val;
-        while(root){
-            if((double)root->val==target)return root->val;
-            if(abs((double)root->val-target)<abs(result-target))
-                result=root->val;
-            if(root->val>target)root=root->left;
-            else root=root->right;
+        int result = root->val;
+        
+        while(root)
+        {
+            if ((double)root->val == target)
+            {
+                return root->val;
+            }
+            double a = abs(target - (double)result);
+            double b = abs(target - (double)root->val);
+            result = (a<b)?result:root->val;
+            
+            if (target > (double)root->val)
+            {
+                root = root->right;
+            } else {
+                root = root->left;
+            }
         }
         return result;
+        
     }
 };
