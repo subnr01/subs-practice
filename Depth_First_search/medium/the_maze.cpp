38a61be81dcb4ce4a30745de8f1e7683103ae7a2
Subbu <subnr01@gmail.com>
2018-02-03 20:48:50 -0500

    Create the_maze.cpp

---
+++ Depth_First_search/medium/the_maze.cpp
@@ -0,0 +1,45 @@
+/*
+There is a ball in a maze with empty spaces and walls. The ball can go through empty spaces by 
+rolling up, down, left or right, but it won't stop rolling until hitting a wall. When the ball stops, 
+it could choose the next direction.
+
+Given the ball's start position, the destination and the maze, determine whether the ball could stop 
+t the destination.
+
+The maze is represented by a binary 2D array. 1 means the wall and 0 means the empty space. You may 
+assume that the borders of the maze are all walls. The start and destination coordinates are represented 
+by row and column indexes.
+
+Example 1
+
+Input 1: a maze represented by a 2D array
+
+0 0 1 0 0
+0 0 0 0 0
+0 0 0 1 0
+1 1 0 1 1
+0 0 0 0 0
+
+Input 2: start coordinate (rowStart, colStart) = (0, 4)
+Input 3: destination coordinate (rowDest, colDest) = (4, 4)
+Output:true
+
+
+Example 2
+
+Input 1: a maze represented by a 2D array
+
+0 0 1 0 0
+0 0 0 0 0
+0 0 0 1 0
+1 1 0 1 1
+0 0 0 0 0
+
+Input 2: start coordinate (rowStart, colStart) = (0, 4)
+Input 3: destination coordinate (rowDest, colDest) = (3, 2)
+
+Output: false
+Explanation: There is no way for the ball to stop at the destination.
+
+*/
+
