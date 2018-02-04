5489b19fbc2c9bd89130da6852010b90ce7cbe01
Subbu <subnr01@gmail.com>
2018-02-03 20:20:27 -0500

    Update min_size_subarray_sum.cpp

--- arrays/medium/min_size_subarray_sum.cpp
+++ arrays/medium/min_size_subarray_sum.cpp
@@ -7,7 +7,7 @@ the subarray [4,3] has the minimal length under the problem constraint.
 
 */
 
-//good soln
+//good soln O(n)
 class Solution {
 public:
     int minSubArrayLen(int s, vector<int>& nums) {
@@ -39,3 +39,38 @@ public:
         return minlen == INT_MAX? 0: minlen;
     }
 };
+
+
+
+//Slightly difficult one using binary search with O(nlogn)
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+        vector<int> sums = accumulate(nums);
+        int n = nums.size(), minlen = INT_MAX;
+        for (int i = 1; i <= n; i++) { 
+            if (sums[i] >= s) {
+                int p = upper_bound(sums, 0, i, sums[i] - s);
+                if (p != -1) minlen = min(minlen, i - p + 1);
+            }
+        }
+        return minlen == INT_MAX ? 0 : minlen;
+    }
+private:
+    vector<int> accumulate(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> sums(n + 1, 0);
+        for (int i = 1; i <= n; i++) 
+            sums[i] = nums[i - 1] + sums[i - 1];
+        return sums;
+    }
+    int upper_bound(vector<int>& sums, int left, int right, int target) {
+        int l = left, r = right;
+        while (l < r) {
+            int m = l + ((r - l) >> 1);
+            if (sums[m] <= target) l = m + 1;
+            else r = m;
+        }
+        return sums[r] > target ? r : -1;
+    }
+};  
