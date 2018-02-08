bb1d270a88381c4a8e4ed526e649048426d7987b
Subbu <subnr01@gmail.com>
2018-02-07 23:18:26 -0500

    Update max_len_repeated_subarray.cpp

--- arrays/medium/max_len_repeated_subarray.cpp
+++ arrays/medium/max_len_repeated_subarray.cpp
@@ -27,16 +27,22 @@ The repeated subarray with maximum length is [3, 2, 1].
 
 class Solution {
 public:
-    int findLength(vector<int>& a, vector<int>& b) {
-        int m = a.size(), n = b.size();
-        if (!m || !n) return 0;
-        vector<int> dp(n + 1);
+    int findLength(vector<int>& A, vector<int>& B) {
+        int m = A.size();
+        int n = B.size();
+        
+        vector<int> dp(m + 1);
         int res = 0;
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = 0; j < n; j++) {
-                res = max(res, dp[j] = a[i] == b[j] ? 1 + dp[j + 1] : 0);
+        
+        for (int i = m - 1; i >= 0; i--)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                dp[j] = A[i] == B[j]? 1 + dp[j+1]: 0;
+                res = max(res, dp[j]);        
             }
         }
+        
         return res;
     }
 };
