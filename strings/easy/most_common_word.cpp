c1be5c3d3ccaf673a678d7261e227385b9ef3002
Subbu <subnr01@gmail.com>
2018-04-23 07:33:11 -0400

    Update most_common_word.cpp

--- strings/easy/most_common_word.cpp
+++ strings/easy/most_common_word.cpp
@@ -11,9 +11,13 @@ public:
     string mostCommonWord(string paragraph, vector<string>& banned) {
         unordered_map<string, int>m;
         for(int i = 0; i < paragraph.size();){
-            string s = "";
-            while(i < paragraph.size() && isalpha(paragraph[i])) s.push_back(tolower(paragraph[i++]));
-            while(i < paragraph.size() && !isalpha(paragraph[i])) i++;
+            string s;
+            while(i < paragraph.size() && isalpha(paragraph[i])) {
+                s.push_back(tolower(paragraph[i++]));
+            }
+            while(i < paragraph.size() && !isalpha(paragraph[i])) {
+                i++;
+            }
             m[s]++;
         }
         for(auto x: banned) m[x] = 0;
