20ca036536c787e69e638c496071f223f8056be7
Subbu <subnr01@gmail.com>
2018-04-23 13:26:32 -0400

    Create custom_sort_string.cpp

---
+++ strings/easy/custom_sort_string.cpp
@@ -0,0 +1,43 @@
+/*
+S and T are strings composed of lowercase letters. In S, no letter occurs more than once.
+
+S was sorted in some custom order previously. We want to permute the characters of T so that they 
+match the order that S was sorted. More specifically, if x occurs before y in S, 
+then x should occur before y in the returned string.
+
+Return any permutation of T (as a string) that satisfies this property.
+
+Example :
+Input: 
+S = "cba"
+T = "abcd"
+Output: "cbad"
+Explanation: 
+"a", "b", "c" appear in S, so the order of "a", "b", "c" should be "c", "b", and "a". 
+Since "d" does not appear in S, it can be at any position in T. "dcba", "cdba", "cbda" are also valid outputs.
+
+*/
+
+
+class Solution {
+public:
+    string customSortString(string S, string T) {
+        string res = "";
+        vector<int>v(26);
+        for(auto c: T) {
+            v[c - 'a']++;
+        }
+        for(auto c: S) {
+            while(v[c - 'a']--) {
+                res.push_back(c);
+            }
+        }
+        
+        for(int i = 0; i < 26; i++) {
+            while(v[i]-- > 0) {
+                res.push_back('a' + i);
+            }
+        }
+        return res;
+    }
+};
