5f67a78d92f855358393ac069d6f3dc92eab86d6
Subbu <subnr01@gmail.com>
2018-05-03 19:50:22 -0400

    Update count_num_segments.cpp

--- strings/easy/count_num_segments.cpp
+++ strings/easy/count_num_segments.cpp
@@ -13,6 +13,7 @@ public:
     int countSegments(string s) {
         int res = 0;
         s.push_back(' ');
+        //notice i is 1 here
         for(int i = 1; i < s.size(); ++i)
           if(s[i] == ' ' && s[i-1] != ' ') ++res;
         return res;
