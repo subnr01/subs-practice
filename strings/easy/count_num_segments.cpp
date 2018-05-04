08189057db6beee76aff0645b525a7f3cc848f8f
Subbu <subnr01@gmail.com>
2018-05-03 20:00:24 -0400

    Update count_num_segments.cpp

--- strings/easy/count_num_segments.cpp
+++ strings/easy/count_num_segments.cpp
@@ -11,26 +11,23 @@ Output: 5
 class Solution {
 public:
     int countSegments(string s) {
+        int len = s.size();
         int res = 0;
-        s.push_back(' ');
-        //notice i is 1 here
-        for(int i = 1; i < s.size(); ++i)
-          if(s[i] == ' ' && s[i-1] != ' ') ++res;
-        return res;
-    }
-};
-
-
-/*
-Another solution
-*/
-
-class Solution {
-public:
-    int countSegments(string s) {
-        int res = 0;
-        for (int i = 0; i < s.size(); i++) 
-            res += s[i] != ' ' && (i + 1 == s.size() || s[i + 1] == ' ');
+        
+        for (int i = 0; i < len; i++)
+        {
+            /*
+             the current letter must not be space
+             and the next letter can be space or the
+             next letter is eof
+            */
+            if (s[i] != ' ' &&
+               (i + 1 == len ||
+                s[i + 1] == ' '))
+            {
+                res++;
+            }
+        }
         return res;
     }
 };
