c32b092179d2ce5e6c398763eb7797ac205f990a
Subbu <subnr01@gmail.com>
2018-05-07 21:12:34 -0400

    Update find_k_closest_elements.cpp

--- Binary_Search_leet/medium/find_k_closest_elements.cpp
+++ Binary_Search_leet/medium/find_k_closest_elements.cpp
@@ -13,15 +13,47 @@ Output: [1,2,3,4]
 
 */
 
+// good soln
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int left = 0, right = arr.size() - k;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (x - arr[mid] > arr[mid+k] - x) left = mid + 1; 
-            else right = mid;
-        } //find left, don't care too much about right
+        // find the index of the first element 
+        // that is equal to or greater than x
+        int index = std::lower_bound(arr.begin(), arr.end(), x) - arr.begin();
+        int i = index - 1;
+        int j = index;                                    
+        while(k--) 
+        {
+            if (i<0 || (j<arr.size() && abs(arr[i] - x) > abs(arr[j] - x) )) {
+                j++;
+            } else {
+                i--;
+            }
+        }
+        return vector<int>(arr.begin() + i + 1, arr.begin() + j );
+    }
+};
+
+
+//difficult solution hard to comprehend
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int left = 0;
+        int right = arr.size() - k;
+        
+        while (left < right)
+        {
+            int mid = (left + right)/2;
+            if (x -arr[mid] > arr[mid + k] - x) 
+            {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
         return vector<int>(arr.begin() + left, arr.begin() + left + k);
     }
 };
+
+
