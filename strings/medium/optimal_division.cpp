8f0e2b0cef4d4f50a3ad061b1ef8492e0d5890f6
Subbu <subnr01@gmail.com>
2018-05-18 06:03:39 -0400

    Update optimal_division.cpp

--- strings/medium/optimal_division.cpp
+++ strings/medium/optimal_division.cpp
@@ -22,3 +22,33 @@ Other cases:
 
 
 */
+
+
+class Solution {
+public:
+    string optimalDivision(vector<int>& nums) {
+        int n = nums.size();
+        string expr;
+        for (int i = 0; i < n; i++)
+        {
+            if (i > 0)
+            {
+                expr += "/";
+            }
+            
+            if (i == 1 && n > 2)
+            {
+                expr += "(";
+            }
+            
+            expr += to_string(nums[i]);
+            
+            if (i == n - 1 && n > 2)
+            {
+                expr += ")";
+            }
+            
+        }
+        return expr;
+    }
+};
