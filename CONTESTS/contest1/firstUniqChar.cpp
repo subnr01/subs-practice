334eb4074ea4ead7e3b8baa3ca37b23b14e2306b
Subbu <subnr01@gmail.com>
2018-05-19 12:22:48 -0400

    Update firstUniqChar.cpp

--- CONTESTS/contest1/firstUniqChar.cpp
+++ CONTESTS/contest1/firstUniqChar.cpp
@@ -1,4 +1,5 @@
 /*
+https://leetcode.com/contest/warm-up-contest/problems/first-unique-character-in-a-string/
 
 Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
 
