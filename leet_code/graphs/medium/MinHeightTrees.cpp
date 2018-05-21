627a9c73da088ad7b321ad5b77ae4f32f0407f73
Subbu <subnr01@gmail.com>
2018-05-21 19:19:31 -0400

    Update MinHeightTrees.cpp

--- leet_code/graphs/medium/MinHeightTrees.cpp
+++ leet_code/graphs/medium/MinHeightTrees.cpp
@@ -37,8 +37,97 @@ return [3, 4]
 */
 
 
+/*
+
+https://leetcode.com/problems/minimum-height-trees/discuss/76055/Share-some-thoughts
+
+Our problem want us to find the minimum height trees and return their root labels. First we can think 
+about a simple case -- a path graph.
+
+For a path graph of n nodes, find the minimum height trees is trivial. Just designate the middle point(s) as roots.
+
+Despite its triviality, let design a algorithm to find them.
+
+Suppose we don't know n, nor do we have random access of the nodes. We have to traversal. It is 
+very easy to get the idea of two pointers. One from each end and move at the same speed. 
+When they meet or they are one step away, (depends on the parity of n), we have the roots we want.
+
+This gives us a lot of useful ideas to crack our real problem.
+
+For a tree we can do some thing similar. We start from every end, by end we mean vertex of degree 1 (aka leaves). 
+We let the pointers move the same speed. When two pointers meet, we keep only one of them, until the last two pointers meet or one step away we then find the roots.
+
+It is easy to see that the last two pointers are from the two ends of the longest path in the graph.
+
+The actual implementation is similar to the BFS topological sort. Remove the leaves, update the 
+degrees of inner vertexes. Then remove the new leaves. Doing so level by level until 
+there are 2 or 1 nodes left. What's left is our answer!
+
+The time complexity and space complexity are both O(n).
+
+Note that for a tree we always have V = n, E = n-1.
+*/
+
+//35 ms
+class Solution {
+public:
+    vector<int> findMinHeightTrees(int n, vector<pair<int, int>>& edges) {
+        vector<vector<int>> adj_list(n);
+        vector<int> counter(n, 0);
+        for (auto &p:edges){
+            adj_list[p.first].push_back(p.second);
+            adj_list[p.second].push_back(p.first);
+            counter[p.first]++;
+            counter[p.second]++;
+        }
+        queue<int> res;
+        for (int i = 0; i < n; i++){
+            if (counter[i] <= 1){
+                res.push(i);
+            }
+        }
+        while (n > 2){
+            int leafsize = res.size();
+            n -= leafsize;
+            for (int i = 0; i < leafsize; i++){
+                int node = res.front();
+                res.pop();
+                for (auto adj : adj_list[node]){
+                    if (--counter[adj] == 1) res.push(adj);
+                }
+            }
+        }
+        vector<int> r;
+        while (!res.empty()){
+            r.push_back(res.front());
+            res.pop();
+        }
+        return r;
+    }
+};
+
+
+//66 ms
+/*
 https://discuss.leetcode.com/topic/30535/c-solution-o-n-time-o-n-space
 
+The basic idea is "keep deleting leaves layer-by-layer, until reach the root."
+
+Specifically, first find all the leaves, then remove them. After removing, some nodes will
+become new leaves. So we can continue remove them. Eventually, there is only 1 or 2 nodes left. 
+If there is only one node left, it is the root. If there are 2 nodes, either of them could be a possible root.
+
+Time Complexity: Since each node will be removed at most once, the complexity is O(n).
+
+Thanks for pointing out any mistakes.
+
+Updates:
+More precisely, if the number of nodes is V, and the number of edges is E. The space complexity 
+is O(V+2E), for storing the whole tree. The time complexity is O(E), because we gradually 
+remove all the neighboring information. As some friends pointing out, for a tree, if V=n, then E=n-1. 
+Thus both time complexity and space complexity become O(n).
+
+*/
 class Solution {
     public:
         
