26380d0612e1a32a5afd92c952cb6693a407dc37
Subbu <subnr01@gmail.com>
2018-05-22 09:02:40 -0400

    Update findCircleNum.cpp

--- leet_code/union_find/medium/findCircleNum.cpp
+++ leet_code/union_find/medium/findCircleNum.cpp
@@ -25,3 +25,67 @@ Explanation:The 0th and 1st students are direct friends, the 1st and 2nd student
 so the 0th and 2nd students are indirect friends. All of them are in the same friend circle, so return 1.
 
 */
+
+
+//Related topics: Union Find, DFS
+
+//DFS solution is faster than union find.
+
+
+//Union find
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& M) {
+        if (M.empty()) return 0;
+        int n = M.size();
+
+        vector<int> leads(n, 0);
+        for (int i = 0; i < n; i++) { leads[i] = i; }   // initialize leads for every kid as themselves
+
+        int groups = n;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {   // avoid recalculate M[i][j], M[j][i]
+                if (M[i][j]) {
+                    int lead1 = find(i, leads);
+                    int lead2 = find(j, leads);
+                    if (lead1 != lead2) {       // if 2 group belongs 2 different leads, merge 2 group to 1
+                        leads[lead1] = lead2;
+                        groups--;
+                    }
+                }
+            }
+        }
+        return groups;
+    }
+
+private:
+    int find(int x, vector<int>& parents) {
+        return parents[x] == x ? x : find(parents[x], parents);
+    }
+};
+ 
+//DFS (faster)
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& M) {
+        if (M.empty()) return 0;
+        int n = M.size();
+        vector<bool> visited(n, false);
+        int groups = 0;
+        for (int i = 0; i < visited.size(); i++) {
+            groups += !visited[i] ? dfs(i, M, visited), 1 : 0;
+        }
+        return groups;
+    }
+
+private:
+    void dfs(int i, vector<vector<int>>& M, vector<bool>& visited) {
+        visited[i] = true;
+        for (int j = 0; j < visited.size(); j++) {
+            if (i != j && M[i][j] && !visited[j]) {
+                dfs(j, M, visited);
+            }
+        }
+    }
+};
+
