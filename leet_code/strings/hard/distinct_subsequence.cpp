084b32ae2e1d2afd71d5da862963642361cbd71c
Subbu <subnr01@gmail.com>
2018-05-27 20:03:20 -0700

    Update distinct_subsequence.cpp

--- leet_code/strings/hard/distinct_subsequence.cpp
+++ leet_code/strings/hard/distinct_subsequence.cpp
@@ -1,11 +1,58 @@
 /*
-Given a string S and a string T, count the number of distinct subsequences of T in S.
+Given a string S and a string T, count the number of distinct subsequences of S which equals T.
 
-A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) 
-of the characters without disturbing the relative positions of the remaining characters. (ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
+A subsequence of a string is a new string which is formed from the original string by deleting some 
+(can be none) of the characters without disturbing the relative positions of the remaining characters.
+(ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
 
-Here is an example:
-S = "rabbbit", T = "rabbit"
+Example 1:
 
-Return 3.
+Input: S = "rabbbit", T = "rabbit"
+Output: 3
+Explanation:
+
+As shown below, there are 3 ways you can generate "rabbit" from S.
+(The caret symbol ^ means the chosen letters)
+
+rabbbit
+^^^^ ^^
+rabbbit
+^^ ^^^^
+rabbbit
+^^^ ^^^
+Example 2:
+
+Input: S = "babgbag", T = "bag"
+Output: 5
+Explanation:
+
+As shown below, there are 5 ways you can generate "bag" from S.
+(The caret symbol ^ means the chosen letters)
+
+babgbag
+^^ ^
+babgbag
+^^    ^
+babgbag
+^    ^^
+babgbag
+  ^  ^^
+babgbag
+    ^^^
 */
+
+
+//Related topics: dynamic progrmming
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int m = t.length(), n = s.length();
+        vector<vector<int>> dp(m + 1, vector<int> (n + 1, 0));
+        for (int j = 0; j <= n; j++) dp[0][j] = 1;
+        for (int j = 1; j <= n; j++)
+            for (int i = 1; i <= m; i++)
+                dp[i][j] = dp[i][j - 1] + (t[i - 1] == s[j - 1] ? dp[i - 1][j - 1] : 0);
+        return dp[m][n];
+    }
+};  
