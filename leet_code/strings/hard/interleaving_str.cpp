38f905933e5144be198540f1475b6f8b62eec711
Subbu <subnr01@gmail.com>
2018-05-27 20:24:19 -0700

    Update interleaving_str.cpp

--- leet_code/strings/hard/interleaving_str.cpp
+++ leet_code/strings/hard/interleaving_str.cpp
@@ -1,12 +1,61 @@
 /*
 Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
 
-For example,
-Given:
-s1 = "aabcc",
-s2 = "dbbca",
+Example 1:
 
-When s3 = "aadbbcbcac", return true.
-When s3 = "aadbbbaccc", return false.
+Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+Output: true
+Example 2:
+
+Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+Output: false
 
 */
+
+
+//Related topics: dynamic programming, string
+
+//2d array
+ bool isInterleave(string s1, string s2, string s3) {
+    
+    if(s3.length() != s1.length() + s2.length())
+        return false;
+    
+    bool table[s1.length()+1][s2.length()+1];
+    
+    for(int i=0; i<s1.length()+1; i++)
+        for(int j=0; j< s2.length()+1; j++){
+            if(i==0 && j==0)
+                table[i][j] = true;
+            else if(i == 0)
+                table[i][j] = ( table[i][j-1] && s2[j-1] == s3[i+j-1]);
+            else if(j == 0)
+                table[i][j] = ( table[i-1][j] && s1[i-1] == s3[i+j-1]);
+            else
+                table[i][j] = (table[i-1][j] && s1[i-1] == s3[i+j-1] ) || (table[i][j-1] && s2[j-1] == s3[i+j-1] );
+        }
+        
+    return table[s1.length()][s2.length()];
+}
+
+
+//1d array
+class Solution { 
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        int m = s1.length(), n = s2.length(), l = s3.length();
+        if (m > n) return isInterleave(s2, s1, s3);
+        if (l != m + n) return false;
+        bool *dp = new bool[m + 1]();
+        dp[0] = true;
+        for (int i = 1; i <= m; i++)
+            dp[i] = dp[i - 1] && s3[i - 1] == s1[i - 1];
+        for (int j = 1; j <= n; j++) {
+            dp[0] = dp[0] && s3[j - 1] == s2[j - 1];
+            for (int i = 1; i <= m; i++)
+                dp[i] = (dp[i - 1] && s3[i + j - 1] == s1[i - 1]) || (dp[i] && s3[i + j - 1] == s2[j - 1]);
+        }
+        return dp[m];
+    }
+};;
+
