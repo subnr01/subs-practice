58bea6eedab10aceb66cddeb445508a373c6e3d9
Subbu <subnr01@gmail.com>
2018-05-27 18:47:33 -0700

    Update basic_calc_3.cpp

--- leet_code/strings/hard/calculator/basic_calc_3.cpp
+++ leet_code/strings/hard/calculator/basic_calc_3.cpp
@@ -21,5 +21,102 @@ Some examples:
 */
 
 
+class Solution {
+public:
+    int calculate(string s) {
+        int i = 0;
+        return parseExpr(s, i);
+    }
+    
+    int parseExpr(string& s, int& i) {
+        vector<int> nums;
+        char op = '+';
+        for (; i < s.length() && op != ')'; i++) {
+            if (s[i] == ' ') continue;
+            int n = s[i] == '(' ? parseExpr(s, ++i) : parseNum(s, i);
+            switch(op) {
+                case '+' : nums.push_back(n); break;
+                case '-' : nums.push_back(-n); break;
+                case '*' : nums.back() *= n; break;
+                case '/' : nums.back() /= n; break;
+            }            
+            op = s[i];
+        }
+        int res = 0;
+        for (int n : nums) res += n;
+        return res;
+    }
+    
+    int parseNum(string& s, int& i) {
+        int n = 0;
+        while(i < s.length() && isdigit(s[i]))
+            n = s[i++] - '0' + 10 * n;
+        return n;
+    }
+};
+
+
+
+
+//using stack
+class Solution {
+public:
+	int calculate(string s) {
+		stack<int> nums;
+		stack<char> ops;
+		int num = 0;
+		for (int i = 0; i < s.length(); i++) {
+			char c = s[i];
+			if (c == ' ') continue;
+			if (isdigit(c)) {
+				num = c - '0';
+				while (i < s.length() - 1 && isdigit(s[i + 1])) {
+					num = num * 10 + s[i + 1] - '0';
+					i++;
+				}
+				nums.push(num);
+				num = 0;
+			}
+			else {
+				if (c == '(') ops.push(c);
+				if (c == ')') {
+					while (ops.top() != '(') nums.push(eval(ops, nums));
+					ops.pop();
+				}
+				if (c == '+' || c == '-' || c == '*' || c == '/') {
+					while (!ops.empty() && lowPriority(c, ops.top())) nums.push(eval(ops, nums));
+					ops.push(c);
+				}
+			}
+		}
+		while (!ops.empty()) {
+			nums.push(eval(ops, nums));
+		}
+		return nums.top();
+	}
+
+private:
+	int eval(stack<char>& ops, stack<int>& nums) {
+		char op = ops.top();
+		ops.pop();
+		int num2 = nums.top();
+		nums.pop();
+		int num1 = nums.top();
+		nums.pop();
+		if (op == '+') return num1 + num2;
+		if (op == '-') return num1 - num2;
+		if (op == '*') return num1 * num2;
+		if (op == '/' && num2 != 0) return num1 / num2;
+		return 0;
+	}
+
+	bool lowPriority(char cur, char pre) {
+		if (pre == '(' || pre == ')') return false;
+		if ((cur == '*' || cur == '/') && (pre == '+' || pre == '-')) return false;
+		return true;
+	}
+};
+
+
 
 
