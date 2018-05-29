4ad15056e5326f3dd9e2916c8e900f49edd698af
Subbu <subnr01@gmail.com>
2018-05-28 20:54:46 -0700

    Update word_abbrev.cpp

--- leet_code/strings/hard/word_abbrev.cpp
+++ leet_code/strings/hard/word_abbrev.cpp
@@ -22,3 +22,75 @@ The return answers should be in the same order as the original array.
 
 */
 
+//Related topics: String, sort
+
+
+
+
+
+class Solution {
+public:
+  vector<string> wordsAbbreviation(vector<string>& dict) {
+    vector<string> result(dict.size());
+    unordered_map<string, vector<int>> map;
+    
+    for(int i=0; i<dict.size(); i++) {
+      if(dict[i].size() <= 3)
+        result[i] = dict[i];
+      else {
+        string key = encode(dict[i], 1);
+        map[key].push_back(i);
+      }
+    }
+    
+    for(auto p : map) {
+      if(p.second.size() == 1)
+        result[p.second.front()] = p.first;
+      else
+        helper(dict, p.second, 1, result);
+    }
+    return result;
+  }
+  
+  void helper(vector<string> &dict, vector<int> & lt, int index, vector<string> &result) {
+    if(lt.size() == 1) {
+      result[lt.front()] = encode(dict[lt.front()], index);
+      return;
+    }
+    
+    int maxNumSame = 0, numDistinct = 0;
+    vector<int> stat(26, 0);
+    for(int i=0; i<lt.size(); i++) {
+      int idx = dict[lt[i]][index] - 'a';
+      if(stat[idx] == 0) numDistinct++;
+      maxNumSame = max(maxNumSame, ++stat[idx]);
+    }
+    if(maxNumSame == lt.size()) { // all the same
+      helper(dict, lt, index+1, result);
+    }
+    else if(numDistinct == lt.size()) { // all different
+      for(auto n : lt)
+        result[n] = encode(dict[n], index+1);
+    }
+    else {
+      vector<vector<int>> vv(26, vector<int>());
+      for(auto n : lt)
+        vv[dict[n][index]-'a'].push_back(n);
+      
+      for(auto v : vv)
+        if(!v.empty())
+          helper(dict, v, index+1, result);
+    }
+  }
+  
+  string encode(string s, int start) {
+    int len = s.length() - start - 1;
+    if(len > 1)
+      return s.substr(0, start) + to_string(len) + s.back();
+    else
+      return s; 
+  }
+};
+
+
+
