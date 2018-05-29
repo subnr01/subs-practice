d2aa775549d34c17f1252139cb30ae50639bc98f
Subbu <subnr01@gmail.com>
2018-05-28 18:55:56 -0700

    Update regular_expr_matching.cpp

--- leet_code/strings/hard/matching/regular_expr_matching.cpp
+++ leet_code/strings/hard/matching/regular_expr_matching.cpp
@@ -75,3 +75,24 @@ public:
         return f[m][n];
     }
 };
+
+//92%
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int sl = s.size();
+        int pl = p.size();
+        vector<vector<bool>> dp(sl+1, vector<bool>(pl+1, false));
+        dp[sl][pl] = true;
+        for (int i = sl; i >= 0; --i) {
+            for (int j = pl-1; j >= 0; --j) {
+                bool first_match = (i < sl) && (s[i] == p[j] || p[j] == '.');
+                if (j < pl-1 && p[j+1] == '*')
+                    dp[i][j] = dp[i][j+2] || first_match && dp[i+1][j];
+                else
+                    dp[i][j] = first_match && dp[i+1][j+1];
+            }
+        }
+        return dp[0][0];
+    }
+};
