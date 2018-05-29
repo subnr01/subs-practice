251d24b5b1b000c4108ef6a660d2ead2046d0624
Subbu <subnr01@gmail.com>
2018-05-28 21:29:14 -0700

    Update k_smallest_pair_dist.cpp

--- leet_code/arrays/hard/k_smallest_pair_dist.cpp
+++ leet_code/arrays/hard/k_smallest_pair_dist.cpp
@@ -1,3 +1,4 @@
+/*
 Given an integer array, return the k-th smallest distance among all the pairs. The distance of a pair (A, B)
 is defined as the absolute difference between A and B.
 
@@ -17,7 +18,9 @@ Note:
 2 <= len(nums) <= 10000.
 0 <= nums[i] < 1000000.
 1 <= k <= len(nums) * (len(nums) - 1) / 2.
+*/
 
+//Related topics: Binary search, heap
 
 class Solution {
 public:
