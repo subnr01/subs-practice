ef42f39b7ecaa27a0a09358877b3e173e02d3d2b
Subbu <subnr01@gmail.com>
2018-05-30 16:58:08 -0400

    Update distinct_subsequences.cpp

--- leet_code/dynamic_programming/hard/distinct_subsequences.cpp
+++ leet_code/dynamic_programming/hard/distinct_subsequences.cpp
@@ -1,39 +1,86 @@
 /*
-
 Given a string S and a string T, count the number of distinct subsequences of S which equals T.
 
-A subsequence of a string is a new string which is formed from the original string by deleting 
-some (can be none) of the characters without disturbing the relative positions of the remaining
-characters. (ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
+A subsequence of a string is a new string which is formed from the original string by deleting some 
+(can be none) of the characters without disturbing the relative positions of the remaining characters.
+(ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
 
-Here is an example:
-S = "rabbbit", T = "rabbit"
+Example 1:
 
-Return 3.
+Input: S = "rabbbit", T = "rabbit"
+Output: 3
+Explanation:
+
+As shown below, there are 3 ways you can generate "rabbit" from S.
+(The caret symbol ^ means the chosen letters)
+
+rabbbit
+^^^^ ^^
+rabbbit
+^^ ^^^^
+rabbbit
+^^^ ^^^
+Example 2:
+
+Input: S = "babgbag", T = "bag"
+Output: 5
+Explanation:
+
+As shown below, there are 5 ways you can generate "bag" from S.
+(The caret symbol ^ means the chosen letters)
+
+babgbag
+^^ ^
+babgbag
+^^    ^
+babgbag
+^    ^^
+babgbag
+  ^  ^^
+babgbag
+    ^^^
+*/
+
+
+//Related topics: dynamic progrmming
+
+
+/*
+Well, a dynamic programming problem. Let's first define its state dp[i][j] to be the number of 
+distinct subsequences of t[0..i - 1] in s[0..j - 1]. Then we have the following state equations:
+
+General case 1: dp[i][j] = dp[i][j - 1] if t[i - 1] != s[j - 1];
+General case 2: dp[i][j] = dp[i][j - 1] + dp[i - 1][j - 1] if t[i - 1] == s[j - 1];
+Boundary case 1: dp[0][j] = 1 for all j;
+Boundary case 2: dp[i][0] = 0 for all positive i.
+Now let's give brief explanations to the four equations above.
+
+If t[i - 1] != s[j - 1], the distinct subsequences will not include s[j - 1] and thus all the number 
+of distinct subsequences will simply be those in s[0..j - 2], which corresponds to dp[i][j - 1];
+If t[i - 1] == s[j - 1], the number of distinct subsequences include two parts: those with s[j - 1] 
+and those without;
+An empty string will have exactly one subsequence in any string :-)
+Non-empty string will have no subsequences in an empty string.
+Putting these together, we will have the following simple codes (just like translation :-)):
 
 */
 
 
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int m = t.length(), n = s.length();
+        vector<vector<int>> dp(m + 1, vector<int> (n + 1, 0));
+        for (int j = 0; j <= n; j++) dp[0][j] = 1;
+        for (int j = 1; j <= n; j++)
+            for (int i = 1; i <= m; i++)
+                dp[i][j] = dp[i][j - 1] + (t[i - 1] == s[j - 1] ? dp[i - 1][j - 1] : 0);
+        return dp[m][n];
+    }
+};  
 
 
-//three solutions
-
-
-
-
-
-
-//C++ DP
-int numDistinct(string s, string t) {
-    int tLen = t.size();
-    vector<int> prefixVec(tLen,0);
-    for (auto c: s)
-        for (int i = tLen-1;i >= 0;--i)
-            if (t[i] == c)
-                prefixVec[i] = (i > 0 ? prefixVec[i-1] : 1) + prefixVec[i];
-    return prefixVec.back();
-}
-
+//1-D array
 class Solution {
 public:
     int numDistinct(string s, string t) {
@@ -52,27 +99,3 @@ public:
     }
 };
 
-//Java
-public int numDistinct(String S, String T) {
-    // array creation
-    int[][] mem = new int[T.length()+1][S.length()+1];
-
-    // filling the first row: with 1s
-    for(int j=0; j<=S.length(); j++) {
-        mem[0][j] = 1;
-    }
-    
-    // the first column is 0 by default in every other rows but the first, which we need.
-    
-    for(int i=0; i<T.length(); i++) {
-        for(int j=0; j<S.length(); j++) {
-            if(T.charAt(i) == S.charAt(j)) {
-                mem[i+1][j+1] = mem[i][j] + mem[i+1][j];
-            } else {
-                mem[i+1][j+1] = mem[i+1][j];
-            }
-        }
-    }
-    
-    return mem[T.length()][S.length()];
-}
