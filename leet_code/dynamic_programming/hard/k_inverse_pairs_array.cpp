01e66d9d4f7d5a18db56b4f0be370e7648c366ea
Subbu <subnr01@gmail.com>
2018-05-30 17:19:56 -0400

    Update k_inverse_pairs_array.cpp

--- leet_code/dynamic_programming/hard/k_inverse_pairs_array.cpp
+++ leet_code/dynamic_programming/hard/k_inverse_pairs_array.cpp
@@ -27,20 +27,24 @@ The integer n is in the range [1, 1000] and k is in the range [0, 1000].
 
 //https://discuss.leetcode.com/topic/93815/java-dp-o-nk-solution/2
 
-  public static int kInversePairs(int n, int k) {
-        int mod = 1000000007;
-        if (k > n*(n-1)/2 || k < 0) return 0;
-        if (k == 0 || k == n*(n-1)/2) return 1;
-        long[][] dp = new long[n+1][k+1];
-        dp[2][0] = 1;
-        dp[2][1] = 1;
-        for (int i = 3; i <= n; i++) {
+//there seeems to be faster solns, but this one is shorter than the rest. (70 %)
+ class Solution {
+public:
+    int kInversePairs(int n, int k) {
+        vector<vector<int>> dp(n+1, vector<int>(k+1));
+        dp[0][0] = 1;
+        for(int i = 1; i <= n; ++i){
             dp[i][0] = 1;
-            for (int j = 1; j <= Math.min(k, i*(i-1)/2); j++) {
-                dp[i][j] = dp[i][j-1] + dp[i-1][j];
-                if (j >= i) dp[i][j] -= dp[i-1][j-i];
-                dp[i][j] = (dp[i][j]+mod) % mod;
+            for(int j = 1; j <= k; ++j){
+                dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % mod;
+                if(j - i >= 0){
+                    dp[i][j] = (dp[i][j] - dp[i-1][j-i] + mod) % mod; 
+                    //It must + mod, If you don't know why, you can check the case 1000, 1000
+                }
             }
         }
-        return (int) dp[n][k];
+        return dp[n][k];
     }
+private:
+    const int mod = pow(10, 9) + 7;
+};
