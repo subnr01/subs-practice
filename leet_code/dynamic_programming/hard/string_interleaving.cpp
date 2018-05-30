0712476b17ae0fcfb87048ec5fda94c1c9295c68
Subbu <subnr01@gmail.com>
2018-05-30 17:17:22 -0400

    Update string_interleaving.cpp

--- leet_code/dynamic_programming/hard/string_interleaving.cpp
+++ leet_code/dynamic_programming/hard/string_interleaving.cpp
@@ -1,22 +1,45 @@
 /*
-
-String Intervleaving
-
 Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
 
-For example,
-Given:
-s1 = "aabcc",
-s2 = "dbbca",
+Example 1:
 
-When s3 = "aadbbcbcac", return true.
-When s3 = "aadbbbaccc", return false.
+Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+Output: true
+Example 2:
+
+Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+Output: false
 
 */
 
 
-//Check out BFS and DFS solutions if possible.
+//Related topics: dynamic programming, string
 
+//2d array
+ bool isInterleave(string s1, string s2, string s3) {
+    
+    if(s3.length() != s1.length() + s2.length())
+        return false;
+    
+    bool table[s1.length()+1][s2.length()+1];
+    
+    for(int i=0; i<s1.length()+1; i++)
+        for(int j=0; j< s2.length()+1; j++){
+            if(i==0 && j==0)
+                table[i][j] = true;
+            else if(i == 0)
+                table[i][j] = ( table[i][j-1] && s2[j-1] == s3[i+j-1]);
+            else if(j == 0)
+                table[i][j] = ( table[i-1][j] && s1[i-1] == s3[i+j-1]);
+            else
+                table[i][j] = (table[i-1][j] && s1[i-1] == s3[i+j-1] ) || (table[i][j-1] && s2[j-1] == s3[i+j-1] );
+        }
+        
+    return table[s1.length()][s2.length()];
+}
+
+
+//1d array
 class Solution { 
 public:
     bool isInterleave(string s1, string s2, string s3) {
@@ -34,5 +57,5 @@ public:
         }
         return dp[m];
     }
-};
+};;
 
