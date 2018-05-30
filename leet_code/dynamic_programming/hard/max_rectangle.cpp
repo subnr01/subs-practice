b580dcb2948c1d46c585774b7bce34097891a214
Subbu <subnr01@gmail.com>
2018-05-30 17:29:15 -0400

    Update max_rectangle.cpp

--- leet_code/dynamic_programming/hard/max_rectangle.cpp
+++ leet_code/dynamic_programming/hard/max_rectangle.cpp
@@ -1,49 +1,64 @@
 /*
-
-Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
-
+Given a 2D binary matrix filled with 0's and 1's, find the largest 
+rectangle containing only 1's and return its area.
 For example, given the following matrix:
-
 1 0 1 0 0
 1 0 1 1 1
 1 1 1 1 1
 1 0 0 1 0
 Return 6.
-
 */
 
+//Related topics: array, hash, DP
 
-//https://discuss.leetcode.com/topic/6650/share-my-dp-solution/2
-
-
+//34%
 class Solution {
 public:
-int maximalRectangle(vector<vector<char> > &matrix) {
-    if(matrix.empty()) return 0;
-    const int m = matrix.size();
-    const int n = matrix[0].size();
-    int left[n], right[n], height[n];
-    fill_n(left,n,0); fill_n(right,n,n); fill_n(height,n,0);
-    int maxA = 0;
-    for(int i=0; i<m; i++) {
-        int cur_left=0, cur_right=n; 
-        for(int j=0; j<n; j++) { // compute height (can do this from either side)
-            if(matrix[i][j]=='1') height[j]++; 
-            else height[j]=0;
+    int maximalRectangle(vector<vector<char> > &matrix) {
+         if(matrix.empty()){
+            return 0;
+         }
+        int max_area = 0;
+        vector<int> temp(matrix[0].size(), 0);
+        size_t sz = matrix[0].size();
+        
+        for(int i = 0; i < matrix.size(); ++i) {
+            for(int j = 0; j < sz; ++j) {
+                if(matrix[i][j] == '0') {
+                    temp[j] = 0;
+                } else {
+                    temp[j]++;
+                }
+            }
+            max_area = max(max_area,largestRectangleArea(temp));
         }
-        for(int j=0; j<n; j++) { // compute left (from left to right)
-            if(matrix[i][j]=='1') left[j]=max(left[j],cur_left);
-            else {left[j]=0; cur_left=j+1;}
-        }
-        // compute right (from right to left)
-        for(int j=n-1; j>=0; j--) {
-            if(matrix[i][j]=='1') right[j]=min(right[j],cur_right);
-            else {right[j]=n; cur_right=j;}    
-        }
-        // compute the area of rectangle (can do this from either side)
-        for(int j=0; j<n; j++)
-            maxA = max(maxA,(right[j]-left[j])*height[j]);
+        return max_area;
     }
-    return maxA;
-}
-}:
+    
+    
+    //brute force, O(n^2)
+    int largestRectangleArea(vector<int> &height) {
+        height.push_back(0);
+        size_t n = height.size();
+        
+        int max_area = 0;
+        int i = 0;
+        stack<int> mystack;
+        
+        while(i < n) {
+            if(mystack.empty() || height[i] >= height[mystack.top()]) {
+                mystack.push(i);
+                i++;
+            } else {
+                int h = height[mystack.top()];
+                mystack.pop();
+                int w = mystack.empty()? i: i - mystack.top() - 1;
+                max_area = max(max_area, h * w);
+            }
+            
+            
+        }
+        return max_area;
+    }
+    
+};
