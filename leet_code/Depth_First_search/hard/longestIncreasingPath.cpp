52ca7bcb66f8994d21deae0be4163589174f4470
Subbu <subnr01@gmail.com>
2018-05-30 11:34:00 -0400

    Update longestIncreasingPath.cpp

--- leet_code/Depth_First_search/hard/longestIncreasingPath.cpp
+++ leet_code/Depth_First_search/hard/longestIncreasingPath.cpp
@@ -25,3 +25,104 @@ Output: 4
 Explanation: The longest increasing path is [3, 4, 5, 6]. Moving diagonally is not allowed.
 
 */
+
+
+class Solution {
+private:
+    vector<vector<int>> cache;
+    vector<pair<int, int>> direction;
+    int m, n;
+    int DFS(int i, int j, vector<vector<int>>& matrix) {
+        if(cache[i][j] != 0) return cache[i][j];
+        int longest = 1;
+        for(auto dir : direction) {
+            int x = i + dir.first, y = j + dir.second;
+            if(x < 0 || x >= m || y < 0 || y >= n || matrix[x][y] <= matrix[i][j]) {
+                continue;
+            } else {
+                longest = max(longest, DFS(x, y, matrix) + 1);
+            }
+        }
+        cache[i][j] = longest;
+        return longest;
+    }
+public:
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        if(matrix.empty()) return 0;
+        m = matrix.size(), n = matrix[0].size();
+        direction.push_back(pair<int, int>(0, 1));
+        direction.push_back(pair<int, int>(0, -1));
+        direction.push_back(pair<int, int>(1, 0));
+        direction.push_back(pair<int, int>(-1, 0));
+        cache = vector<vector<int>>(m, vector<int>(n, 0));
+        int longest = 1;
+        for (int i = 0; i < m; i++) {
+           for(int j = 0; j < n; j++) {
+               longest = max(longest, DFS(i, j, matrix));
+           } 
+        }
+        return longest;
+    }
+};
+
+
+//another soln
+class Solution {
+public:
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int res = 0;
+        if(matrix.size()==0||matrix[0].size()==0)
+            return res;
+        int row = matrix.size(), column = matrix[0].size();
+        //如何初始化vector的二维数组
+        vector<vector<int>> dp(row, vector<int>(column, -1));
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<column;j++)
+            {
+                int localMax = dfs(matrix,dp,i,j);
+                res = res>localMax?res:localMax;
+            }
+        }
+        return res;
+    }
+
+private:
+    int dfs(vector<vector<int>>& matrix,vector<vector<int>>& dp,int row,int column)
+    {
+        int cur = matrix[row][column];
+        //访问当前节点
+        int down=1,up=1,left=1,right=1;
+        //分别向上下左右四个方向遍历
+        if((row+1)<matrix.size()&&matrix[row+1][column]>cur)
+        {
+            if(dp[row+1][column]!=-1)
+                down += dp[row+1][column];
+            else
+                down += dfs(matrix,dp,row+1,column);
+        }
+        if((row-1)>=0&&matrix[row-1][column]>cur)
+        {
+            if(dp[row-1][column]!=-1)
+                up += dp[row-1][column];
+            else
+                up += dfs(matrix,dp,row-1,column);
+        }
+        if((column+1)<matrix[0].size()&&matrix[row][column+1]>cur)
+        {
+            if(dp[row][column+1]!=-1)
+                right += dp[row][column+1];
+            else
+                right += dfs(matrix,dp,row,column+1);
+        }
+        if((column-1)>=0&&matrix[row][column-1]>cur)
+        {
+            if(dp[row][column-1]!=-1)
+                left += dp[row][column-1];
+            else
+                left += dfs(matrix,dp,row,column-1);
+        }
+        dp[row][column] = max(max(down,up),max(right,left));
+        return dp[row][column];
+    }
+};
