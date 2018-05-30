71755c3c99aa946fd2f80d3b54088504b2835e9d
Subbu <subnr01@gmail.com>
2018-05-30 11:42:05 -0400

    Update zuma_game.cpp

--- leet_code/Depth_First_search/hard/zuma_game.cpp
+++ leet_code/Depth_First_search/hard/zuma_game.cpp
@@ -27,3 +27,35 @@ Output: 3
 Explanation: RBYYBBRRB -> RBYY[Y]BBRRB -> RBBBRRB -> RRRB -> B -> B[B] -> BB[B] -> empty 
 
 */
+
+//there is another soln in the discussion, if you do not understand this.
+
+class Solution {
+public:
+    int findMinStep(string board, string hand) {
+        vector<int> c(128);
+		for (char x : hand) {
+			c[x]++;
+		}
+		return  aux(board, c);
+	}
+private:
+	int aux(string s, vector<int>& c) {
+		if (s.empty()) return 0;
+		//worst case, every character needs 2 characters; plus one to make it impossible, ;-)
+		int res = 2 * s.length() + 1;
+		for (int i = 0; i < s.length();) {
+			int j = i++;
+			while (i < s.length() && s[i] == s[j]) i++;
+			int inc = 3 - i + j;
+			if (c[s[j]] >= inc) {
+				int used = inc <= 0 ? 0 : inc;
+				c[s[j]] -= used;
+				int temp = aux(s.substr(0, j) + s.substr(i), c);
+				if (temp >= 0) res = min(res, used + temp);
+				c[s[j]] += used;
+			}
+		}
+		return res == 2 * s.length() + 1 ? -1 : res;
+    }
+};
