99a090618e19779adaaeab083b805b07372e40d7
Subbu <subnr01@gmail.com>
2018-05-31 11:22:06 -0400

    Update maximal_rectangle.cpp

--- leet_code/arrays/hard/maximal/maximal_rectangle.cpp
+++ leet_code/arrays/hard/maximal/maximal_rectangle.cpp
@@ -19,17 +19,25 @@ public:
             return 0;
          }
         int max_area = 0;
+        //We are taking a matrix with the size of columns
+        
         vector<int> temp(matrix[0].size(), 0);
         size_t sz = matrix[0].size();
         
         for(int i = 0; i < matrix.size(); ++i) {
             for(int j = 0; j < sz; ++j) {
+                // the temp [j] will contain the number
+                // of 1s encountered without zeros in each
+                // column.
                 if(matrix[i][j] == '0') {
                     temp[j] = 0;
                 } else {
                     temp[j]++;
                 }
             }
+            // find the area for each row using the values of temp[j].
+            // the final row with have the cumulative number of 1s, 
+            // representing height of a perfect histogram.
             max_area = max(max_area,largestRectangleArea(temp));
         }
         return max_area;
