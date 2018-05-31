f8fc453fbd68cf795fd7c9eaa3449b2288052463
Subbu <subnr01@gmail.com>
2018-05-31 12:13:36 -0400

    Update trapping_rain_water.cpp

--- leet_code/arrays/hard/trapping_rain_water.cpp
+++ leet_code/arrays/hard/trapping_rain_water.cpp
@@ -1,12 +1,18 @@
 /*
 
-Refer leetcode
+Refer leetcode for figure
 
+Given n non-negative integers representing an elevation map where the width of each bar is 1,
+compute how much water it is able to trap after raining.
+
+Input: [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
 */
 
 
 //Related topics: two pointers, stack
 
+//All solutions are O(n)
 
 //standard 2 pointer soln
 class Solution {
@@ -32,3 +38,46 @@ public:
         return res; 
     }
 };
+
+
+//stack
+int trap(vector<int>& height)
+{
+    int ans = 0, current = 0;
+    stack<int> st;
+    while (current < height.size()) {
+        while (!st.empty() && height[current] > height[st.top()]) {
+            int top = st.top();
+            st.pop();
+            if (st.empty())
+                break;
+            int distance = current - st.top() - 1;
+            int bounded_height = min(height[current], height[st.top()]) - height[top];
+            ans += distance * bounded_height;
+        }
+        st.push(current++);
+    }
+    return ans;
+}
+
+//DP
+int trap(vector<int>& height)
+{
+	if(height == null)
+		return 0;
+    int ans = 0;
+    int size = height.size();
+    vector<int> left_max(size), right_max(size);
+    left_max[0] = height[0];
+    for (int i = 1; i < size; i++) {
+        left_max[i] = max(height[i], left_max[i - 1]);
+    }
+    right_max[size - 1] = height[size - 1];
+    for (int i = size - 2; i >= 0; i--) {
+        right_max[i] = max(height[i], right_max[i + 1]);
+    }
+    for (int i = 1; i < size - 1; i++) {
+        ans += min(left_max[i], right_max[i]) - height[i];
+    }
+    return ans;
+}
