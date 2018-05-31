820ff3d7b279d8bd9c4c9917f37a8242e4c76928
Subbu <subnr01@gmail.com>
2018-05-31 07:31:59 -0400

    Update longest_consec_sequence.cpp

--- leet_code/arrays/hard/longest_consec_sequence.cpp
+++ leet_code/arrays/hard/longest_consec_sequence.cpp
@@ -64,3 +64,36 @@ public:
         return ans;
     }
 };
+
+
+//using sort, easier and interesting and intuitive.
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) 
+    {
+        if (nums.size() == 0)
+        {
+            return 0;
+        }
+        int longst = 1;
+        int currst = 1;
+        
+        sort (nums.begin(), nums.end());
+        
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] != nums[i-1]) {
+                if (nums[i] == nums[i-1] + 1)
+                {
+                    currst++;
+                } else {
+                    longst = max(longst, currst);
+                    currst = 1;
+                }
+            }
+            
+        }
+        
+        return max(longst, currst);
+    }
+};
