a1f1d123a1583360d72e8535f0006439394bdaf4
Subbu <subnr01@gmail.com>
2018-05-31 23:18:51 -0400

    Update max_avg_subarray_2.cpp

--- leet_code/arrays/hard/max_avg_subarray_2.cpp
+++ leet_code/arrays/hard/max_avg_subarray_2.cpp
@@ -27,30 +27,34 @@ public:
         while(left + 0.00001 < right){
             mid = left + (right - left)/2;
             if(islarger(nums, mid, k)){
-                right = mid;
-            } else {
                 left = mid;
+            } else {
+                right = mid;
             }
         }
         return left;
     }
     
-    //Return true when mid is larger than or equal to the maximum average value;
-    //Return false when mid is smaller than the maximum average value.
+    
     bool islarger(vector<int>& nums, double mid, int k){
         // sum: the sum from nums[0] to nums[i];
         // prev_sum:  the sum from nums[0] to nums[i-k];
         // min_sum: the minimal sum from nums[0] to nums[j] ( 0=< j  <= i-k );
         double sum = 0, prev_sum = 0, min_sum = 0;
-        for(int i = 0; i < nums.size(); i++){
+        for (int i = 0; i < k; i++) {
             sum += nums[i] - mid;
-            if(i >= k){
-                prev_sum += nums[i-k] - mid;                        
-                min_sum = min(prev_sum, min_sum); 
-            }
-            if(i >= k-1 && sum > min_sum)return false;
         }
-        return true;                                               
+        if (sum >= 0)
+            return true;
+        for(int i = k; i < nums.size(); i++){
+            sum += nums[i] - mid;
+            prev_sum += nums[i-k] - mid;                        
+            min_sum = min(prev_sum, min_sum); 
+            if (sum >= min_sum) {
+                return true;
+            }
+        }
+        return false;                                               
     }
 };
 
