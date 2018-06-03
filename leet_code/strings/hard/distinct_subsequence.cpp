b027305d8190003aefddfc33c631e0e0dfed9dba
Subbu <subnr01@gmail.com>
2018-06-03 07:25:51 -0400

    Update distinct_subsequence.cpp

--- leet_code/strings/hard/distinct_subsequence.cpp
+++ leet_code/strings/hard/distinct_subsequence.cpp
@@ -69,16 +69,26 @@ Putting these together, we will have the following simple codes (just like trans
 class Solution {
 public:
     int numDistinct(string s, string t) {
-        int m = t.length(), n = s.length();
+        int m = t.length();
+        int n = s.length();
         vector<vector<int>> dp(m + 1, vector<int> (n + 1, 0));
-        for (int j = 0; j <= n; j++) dp[0][j] = 1;
-        for (int j = 1; j <= n; j++)
-            for (int i = 1; i <= m; i++)
-                dp[i][j] = dp[i][j - 1] + (t[i - 1] == s[j - 1] ? dp[i - 1][j - 1] : 0);
+        
+        for (int j = 0; j <= n; j++) 
+        {
+            dp[0][j] = 1;
+        }
+        
+        for (int j = 1; j <= n; j++) 
+        {
+            for (int i = 1; i <= m; i++) 
+            {
+                int res = (t[i - 1] == s[j - 1]) ? dp[i - 1][j - 1] : 0;
+                dp[i][j] = dp[i][j - 1] + res;
+            }
+        }
         return dp[m][n];
     }
 };  
-
 
 //1-D array
 class Solution {
