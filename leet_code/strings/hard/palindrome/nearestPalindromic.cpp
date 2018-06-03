a1dc055f9c3de41221cff95d6d6e3d190011d6e0
Subbu <subnr01@gmail.com>
2018-06-03 16:08:50 -0400

    Update nearestPalindromic.cpp

--- leet_code/strings/hard/palindrome/nearestPalindromic.cpp
+++ leet_code/strings/hard/palindrome/nearestPalindromic.cpp
@@ -26,9 +26,25 @@ public:
         
         set<long> candidates;
         
+        /*
+        we need this because of inputs such as 10, 99.
+        for 10, we must get ouput as 9, not 11 and
+        for 99 we must get output as 101 and not 88,
+        */
+        
         candidates.insert(a);
+        /*
+        we need b especially as it goes down a digit, whereas
+        our main logic below divides the gives input into half
+        and hence obtains palindoromes of the same size,but not size - 1
+        */
         candidates.insert(b);
         
+        
+        // find the mid "n" digits: 
+        // for 121, it is 12
+        // for 1234 it is 12
+        // for 12345 it is 123
         int prefix = stol(n.substr(0,(sz+1)/2));
         
         
@@ -43,6 +59,12 @@ public:
         long val = stol(n);
         long mindiff = LONG_MAX;
         long minval = LONG_MAX;
+        /*
+        we need the below statement becuase for
+        inputs like 11, 999 we will add the same
+        number based on our main logic into 
+        the set 
+        */
         candidates.erase(val);
         
         for (auto x:candidates)
