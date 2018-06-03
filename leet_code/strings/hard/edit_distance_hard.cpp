a7805089b6c3dbe7842f338e2e6337e68bd31170
Subbu <subnr01@gmail.com>
2018-06-03 09:46:00 -0400

    Update edit_distance_hard.cpp

--- leet_code/strings/hard/edit_distance_hard.cpp
+++ leet_code/strings/hard/edit_distance_hard.cpp
@@ -48,17 +48,27 @@ Solution with the 2D version: relatively simpler
 class Solution { 
 public:
     int minDistance(string word1, string word2) { 
-        int m = word1.length(), n = word2.length();
+        int m = word1.length(); 
+        int n = word2.length();
+        
         vector<vector<int> > dp(m + 1, vector<int> (n + 1, 0));
-        for (int i = 1; i <= m; i++)
+        
+        for (int i = 1; i <= m; i++) {
             dp[i][0] = i;
-        for (int j = 1; j <= n; j++)
+        }
+        
+        for (int j = 1; j <= n; j++) {
             dp[0][j] = j;  
+        }
+        
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                if (word1[i - 1] == word2[j - 1]) 
+                if (word1[i - 1] == word2[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1];
-                else dp[i][j] = min(dp[i - 1][j - 1] + 1, min(dp[i][j - 1] + 1, dp[i - 1][j] + 1));
+                } else {
+                    int res = min(dp[i][j - 1] + 1, dp[i - 1][j] + 1);
+                    dp[i][j] = min(dp[i - 1][j - 1] + 1, res);
+                }
             }
         }
         return dp[m][n];
