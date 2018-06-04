c9d7b1e8e2aa754af3087ffe16d6fca1997d0007
Subbu <subnr01@gmail.com>
2018-06-04 00:12:21 -0400

    Update min_window_substr.cpp

--- leet_code/strings/hard/sliding_window/min_window_substr.cpp
+++ leet_code/strings/hard/sliding_window/min_window_substr.cpp
@@ -19,73 +19,68 @@ If there is no such window in S that covers all characters in T, return the empt
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if (s.size() == 0 || t.size() == 0) return "";
+        if (s.size() == 0 || t.size() == 0) {
+            return "";
+        }
+        
         vector<int> remaining(128, 0);
         int required = t.size();
-        for (int i = 0; i < required; i++) remaining[t[i]]++;
+        for (int i = 0; i < required; i++) 
+        { 
+            remaining[t[i]]++;
+        }
+        
         // left is the start index of the min-length substring ever found
-        int min = INT_MAX, start = 0, left = 0, i = 0;
-        while(i <= s.size() && start < s.size()) {
-            if(required) {
-                if (i == s.size()) break;
+        int min = INT_MAX; 
+        int start = 0; 
+        int left = 0;
+        int i = 0;
+        
+        /*
+        we have to compare against equal to size 
+        because the difference in distance is calculated
+        at the next index, so min = i - start will give
+        us the right value. If i is at the end of the string, 
+        then i has to move 1 character past the end to 
+        calcualte the min
+        */
+        while(i <= s.size()) 
+        {
+            if (required) 
+            {
+                if (i == s.size()){
+                    break;
+                }
+                
                 remaining[s[i]]--;
-                if (remaining[s[i]] >= 0) required--;
+                if (remaining[s[i]] >= 0) { 
+                    required--;
+                }
                 i++;
             } else {
+                /*
+                Point to be noted here.
+                i is not moving, only start
+                moves in this block
+                */
                 if (i - start < min) {
-                    min = i -start;
+                    min = i - start;
                     left = start;
                 }
                 remaining[s[start]]++;
-                if (remaining[s[start]] > 0) required++;
+                /*
+                Other characters in s, will be at -1, 
+                so required will not get incremented for
+                charcaters not in t
+                */
+                if (remaining[s[start]] > 0) {
+                    required++;
+                }
                 start++;
             }
         }
-        return min == INT_MAX? "" : s.substr(left, min);
-    }
-};
-
-//simpler???
-class Solution {
-public:
-    string minWindow(string s, string t) {
-        int n=s.size();
-        int m=t.size();
-        if(n<1 || m<1 || n<m)
-            return string("");
-        int map[255]={0};
-        for(char c: t)
-            map[c]--;
         
-        int counter=-m;
-        int begin=0;
-        int end=0;
-        int d=n+1;
-        int head=0;
-        while(end<n)
-        {
-            if(map[s[end]]<0)
-            { 
-                counter++;
-            }
-            map[s[end]]++;
-            end++;
-            
-            while(counter==0)
-            {
-                if(d>end-begin)
-                {
-                    d=end-begin;
-                    head=begin;
-                }
-                if(map[s[begin]]==0) counter--;
-                map[s[begin]]--;                    
-                begin++;
-            }
-        }
-        if(d==n+1)
-            return string("");
-        return string(s, head, d);
+        return min == INT_MAX? "" : s.substr(left, min);
     }
 };
 
