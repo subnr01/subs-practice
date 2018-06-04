171b15cafd0c262ce8c1601305352881b2812b46
Subbu <subnr01@gmail.com>
2018-06-04 10:17:31 -0400

    Update special_binary_str.cpp

--- leet_code/strings/hard/special_binary_str.cpp
+++ leet_code/strings/hard/special_binary_str.cpp
@@ -21,8 +21,32 @@ This is the lexicographically largest string possible after some number of swaps
 
 //related topics: string, recursion
 
-//Look at other solns as well
+//Alexander
+class Solution {
+public:
+    string makeLargestSpecial(string s) {
+        int i = 0;
+        return dfs(s, i);
+    }
 
+private:
+    string dfs(string& s, int& i) {
+        string res;
+        vector<string> toks;
+        while (i < s.size() && res.empty()) {
+            if (s[i++] == '1') toks.push_back(dfs(s, i));
+            else res += "1";
+        }
+        bool prefix = res.size();
+        sort(toks.begin(), toks.end());
+        for (auto it = toks.rbegin(); it != toks.rend(); it++) res += *it;
+        if (prefix) res += '0';
+        return res;
+    }
+};
+
+
+//soln difficult to comprehend like the question
 string makeLargestSpecial(string S) {
     int count = 0, i = 0;
     vector<string> res;
