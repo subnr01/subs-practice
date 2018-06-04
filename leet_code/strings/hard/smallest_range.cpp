1760f02919de89d5b71ac537c699396bdb0b1d28
Subbu <subnr01@gmail.com>
2018-06-04 11:38:24 -0400

    Update smallest_range.cpp

--- leet_code/strings/hard/smallest_range.cpp
+++ leet_code/strings/hard/smallest_range.cpp
@@ -17,8 +17,8 @@ List 3: [5, 18, 22, 30], 22 is in range [20,24].
 
 //related topics: hash table, string, 2 pointers
 
-//Check out the brute force solns
-
+//Check out the other solution which is better complexity
+// Solution is O(n * log m), space complexity is O(m)
 class Solution {
 public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
@@ -55,3 +55,34 @@ public:
         return ans;
     }
 };
+
+
+// Solution is O(n * m), space complexity is O(m)
+// Convert this to C++.
+public class Solution {
+    public int[] smallestRange(int[][] nums) {
+        int minx = 0, miny = Integer.MAX_VALUE;
+        int[] next = new int[nums.length];
+        boolean flag = true;
+        for (int i = 0; i < nums.length && flag; i++) {
+            for (int j = 0; j < nums[i].length && flag; j++) {
+                int min_i = 0, max_i = 0;
+                for (int k = 0; k < nums.length; k++) {
+                    if (nums[min_i][next[min_i]] > nums[k][next[k]])
+                        min_i = k;
+                    if (nums[max_i][next[max_i]] < nums[k][next[k]])
+                        max_i = k;
+                }
+                if (miny - minx > nums[max_i][next[max_i]] - nums[min_i][next[min_i]]) {
+                    miny = nums[max_i][next[max_i]];
+                    minx = nums[min_i][next[min_i]];
+                }
+                next[min_i]++;
+                if (next[min_i] == nums[min_i].length) {
+                    flag = false;
+                }
+            }
+        }
+        return new int[] {minx, miny};
+    }
+}
