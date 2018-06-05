f751c7e56922e6257cb2f2d6dd5da3d885c07f95
Subbu <subnr01@gmail.com>
2018-06-05 14:24:10 -0400

    Update course_schedule_2.cpp

--- leet_code/graphs/medium/course_schedule/course_schedule_2.cpp
+++ leet_code/graphs/medium/course_schedule/course_schedule_2.cpp
@@ -67,40 +67,32 @@ private:
 //BFS
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites) {
-        vector<unordered_set<int>> graph = make_graph(numCourses, prerequisites);
-        vector<int> degrees = compute_indegree(graph);
-        queue<int> zeros;
-        for (int i = 0; i < numCourses; i++)
-            if (!degrees[i]) zeros.push(i);
-        vector<int> toposort;
-        for (int i = 0; i < numCourses; i++) {
-            if (zeros.empty()) return {};
-            int zero = zeros.front();
-            zeros.pop();
-            toposort.push_back(zero);
-            for (int neigh : graph[zero]) {
-                if (!--degrees[neigh])
-                    zeros.push(neigh);
+    vector<int> findOrder(int N, vector<pair<int, int>>& pre) {
+        vector<vector<int>> graph(N,vector<int>());
+        vector<int> in(N,0),ans;
+        for(auto p:pre){
+            graph[p.second].push_back(p.first);
+            in[p.first]++;
+        }
+        queue<int> q;
+        for(int i=0;i<N;i++)
+            if(in[i]== 0)  q.push(i);
+        while(!q.empty()){
+            int tmp = q.front();
+            ans.push_back(tmp);
+            q.pop();
+            for(auto i:graph[tmp]){
+                in[i]--;
+                if(in[i]==0)    q.push(i);
             }
         }
-        return toposort;
-    }
-private:
-    vector<unordered_set<int>> make_graph(int numCourses, vector<pair<int, int>>& prerequisites) {
-        vector<unordered_set<int>> graph(numCourses);
-        for (auto pre : prerequisites)
-            graph[pre.second].insert(pre.first);
-        return graph; 
-    }
-    vector<int> compute_indegree(vector<unordered_set<int>>& graph) {
-        vector<int> degrees(graph.size(), 0);
-        for (auto neighbors : graph)
-            for (int neigh : neighbors)
-                degrees[neigh]++;
-        return degrees;
+        
+        if(ans.size()!=N) 
+        {
+            return {};
+        } else {
+            return ans;
+        }
     }
 };
-
-
 
