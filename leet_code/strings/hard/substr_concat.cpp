ac83f63565d3b9959d5b8fe0c9a250533f189950
Subbu <subnr01@gmail.com>
2018-06-05 05:58:58 -0400

    Update substr_concat.cpp

--- leet_code/strings/hard/substr_concat.cpp
+++ leet_code/strings/hard/substr_concat.cpp
@@ -17,6 +17,36 @@ You should return the indices: [0,9].
 
 //Related topics: Hash table, 2 pointers, String
 
+
+
+//2 map solution
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        unordered_map<string, int> counts;
+        for (string word : words)
+            counts[word]++;
+        int n = s.length(), num = words.size(), len = words[0].length();
+        vector<int> indexes;
+        for (int i = 0; i < n - num * len + 1; i++) {
+            unordered_map<string, int> seen;
+            int j = 0;
+            for (; j < num; j++) {
+                string word = s.substr(i + j * len, len);
+                if (counts.find(word) != counts.end()) {
+                    seen[word]++;
+                    if (seen[word] > counts[word])
+                        break;
+                } 
+                else break;
+            }
+            if (j == num) indexes.push_back(i);
+        }
+        return indexes;
+    }
+};
+
+
 //Very fast solution
 class Solution {
 public:
@@ -90,32 +120,4 @@ public:
     return sol;
   }
 };
-
-//2 map solution
-class Solution {
-public:
-    vector<int> findSubstring(string s, vector<string>& words) {
-        unordered_map<string, int> counts;
-        for (string word : words)
-            counts[word]++;
-        int n = s.length(), num = words.size(), len = words[0].length();
-        vector<int> indexes;
-        for (int i = 0; i < n - num * len + 1; i++) {
-            unordered_map<string, int> seen;
-            int j = 0;
-            for (; j < num; j++) {
-                string word = s.substr(i + j * len, len);
-                if (counts.find(word) != counts.end()) {
-                    seen[word]++;
-                    if (seen[word] > counts[word])
-                        break;
-                } 
-                else break;
-            }
-            if (j == num) indexes.push_back(i);
-        }
-        return indexes;
-    }
-};
-
 
