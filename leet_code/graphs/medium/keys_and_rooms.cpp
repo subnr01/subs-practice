d457b30fba78ec0308f48ad5d48eaae55be07412
Subbu <subnr01@gmail.com>
2018-06-09 00:29:40 -0400

    Update keys_and_rooms.cpp

--- leet_code/graphs/medium/keys_and_rooms.cpp
+++ leet_code/graphs/medium/keys_and_rooms.cpp
@@ -54,25 +54,31 @@ public:
 //DFS
 
 class Solution {
-    void dfs(vector<vector<int>>& rooms, unordered_set<int> & keys, unordered_set<int> & visited, int curr) {
-        visited.insert(curr);
-        for (int k : rooms[curr]) {
-          keys.insert(k);
-        }
-        for (int k : keys) 
-        {
-          if (visited.find(k) == visited.end()) 
-          {
-            dfs(rooms, keys, visited, k);
-          }
-        }
+public:
+    
+    unordered_set<int> visited;
+    unordered_set<int> keys;
+    
+    bool canVisitAllRooms(vector<vector<int>>& rooms) 
+    {
+       
+        int start = 0;
+        dfs (start, rooms);
+        return rooms.size() == visited.size();
     }
     
-public:
-    bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        unordered_set<int> keys;
-        unordered_set<int> visited;
-        dfs(rooms, keys, visited, 0);
-        return visited.size() == rooms.size();
+    void dfs(int curr, vector<vector<int>>& rooms)
+    {
+        visited.insert(curr);
+        for (int k: rooms[curr])
+        {
+            keys.insert(k);
+        }
+        
+        for (int k : keys)
+        {
+            if (visited.find(k) == visited.end())
+                dfs(k, rooms);
+        }
     }
 };
