abbd363996edacc5bbb089e8c9f2ea6160374c45
Subbu <subnr01@gmail.com>
2018-06-10 18:19:46 -0400

    Update add_search_word.cpp

--- leet_code/Trie/medium/add_search_word.cpp
+++ leet_code/Trie/medium/add_search_word.cpp
@@ -25,54 +25,84 @@ if the tire contains a dot somewhere.
 
 class TrieNode {
 public:
-    bool isKey;
-    TrieNode* children[26];
-    TrieNode(): isKey(false) {
-        memset(children, NULL, sizeof(TrieNode*) * 26);
+    bool m_is_end;
+    vector<TrieNode*> children;
+    
+    TrieNode() {
+        m_is_end = false;
+        children.resize(26, NULL);
     }
+    
+    ~TrieNode()
+    {
+        for (auto child: children)
+        {
+            delete child;
+        }
+    }
+    
+    
 };
 
+
 class WordDictionary {
+private:
+    TrieNode *root;
 public:
-    WordDictionary() {
+    /** Initialize your data structure here. */
+    WordDictionary() 
+    {
         root = new TrieNode();
     }
-
-    // Adds a word into the data structure.
-    void addWord(string word) {
-        TrieNode* run = root;
-        for (char c : word) {
-            if (!(run -> children[c - 'a'])) 
-                run -> children[c - 'a'] = new TrieNode();
-            run = run -> children[c - 'a'];
+    
+    /** Adds a word into the data structure. */
+    void addWord(string word) 
+    {
+        TrieNode *curr = root;
+        TrieNode *newNode = NULL;
+        for (auto ch : word)
+        {
+            newNode = curr->children[ch - 'a'];
+            if (!newNode) 
+            {
+                curr->children[ch - 'a'] = new TrieNode();
+            }
+            curr = curr->children[ch - 'a'];
+            
         }
-        run -> isKey = true;
+        curr->m_is_end = true;
+        
     }
-
-    // Returns if the word is in the data structure. A word could
-    // contain the dot character '.' to represent any one letter.
-    bool search(string word) {
+    
+    /** 
+    Returns if the word is in the data structure. 
+    A word could contain the dot character '.' to represent any one letter. 
+    */
+    bool search(string word) 
+    {
         return query(word.c_str(), root);
     }
-
-private:
-    TrieNode* root;
-
-    bool query(const char* word, TrieNode* node) {
-        TrieNode* run = node;
-        for (int i = 0; word[i]; i++) {
-            if (run && word[i] != '.')
-                run = run -> children[word[i] - 'a'];
-            else if (run && word[i] == '.') { 
-                TrieNode* tmp = run;
-                for (int j = 0; j < 26; j++) {
-                    run = tmp -> children[j];
-                    if (query(word + i + 1, run))
+    
+    bool query(const char *word, TrieNode *node)
+    {
+        TrieNode *curr = node;
+        for (int i = 0; word[i] != '\0'; i++)
+        {
+            char ch = word[i];
+            if (curr && ch != '.') {
+                curr = curr->children[ch - 'a'];
+            } else if (curr && ch == '.')
+            {
+                for (auto child: curr->children)
+                {
+                    //THIS LINE IS VERY IMPORTANT
+                    curr = child;
+                    if (query(word + i + 1, child)) {
                         return true;
+                    }
                 }
             }
-            else break;
         }
-        return run && run -> isKey; 
+        return curr && curr->m_is_end;
     }
 };
