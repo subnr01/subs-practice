42b26e44864431d7a43a23d3ba0d2ae3343582d4
Subbu <subnr01@gmail.com>
2018-06-18 05:34:37 +0530

    Update and rename leet_code/trees/problems/ClosestBSTValue/Closest-easy.cpp to leet_code/trees/easy/Closest-easy.cpp

---
+++ leet_code/trees/easy/Closest-easy.cpp
@@ -0,0 +1,33 @@
+
+/*
+Given a non-empty binary search tree and a target value, find the value in the BST that is closest to the target.
+
+
+*/
+
+class Solution {
+public:
+    int closestValue(TreeNode* root, double target) {
+        int result = root->val;
+        
+        while(root)
+        {
+            if ((double)root->val == target)
+            {
+                return root->val;
+            }
+            double a = abs(target - (double)result);
+            double b = abs(target - (double)root->val);
+            result = (a<b)?result:root->val;
+            
+            if (target > (double)root->val)
+            {
+                root = root->right;
+            } else {
+                root = root->left;
+            }
+        }
+        return result;
+        
+    }
+};
