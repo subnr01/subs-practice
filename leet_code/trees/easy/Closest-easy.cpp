58b07e20b21ebbe82bea9130041327ec983a2364
Subbu <subnr01@gmail.com>
2018-06-18 05:59:58 +0530

    Update Closest-easy.cpp

--- leet_code/trees/easy/Closest-easy.cpp
+++ leet_code/trees/easy/Closest-easy.cpp
@@ -31,3 +31,27 @@ public:
         
     }
 };
+
+
+//RECURSIVE (BUT SLOWER)
+class Solution {
+public:
+    void helper(TreeNode* root, double target, int& val)
+    {
+        if (root == nullptr) return;
+        if (abs(root->val - target) < abs(val - target)) val = root->val;
+        
+        if (root->val > target) helper(root->left, target, val);
+        else helper(root->right, target, val);
+    }
+    
+    
+    int closestValue(TreeNode* root, double target) {
+        
+        int val = root->val;
+        helper(root, target, val);
+        
+        return val;
+    }
+};
+
