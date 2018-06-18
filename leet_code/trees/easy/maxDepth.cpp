9d3f23d21efcfdd8b1cd0974579de189cab82f70
Subbu <subnr01@gmail.com>
2018-06-19 05:10:48 +0530

    Update maxDepth.cpp

--- leet_code/trees/easy/maxDepth.cpp
+++ leet_code/trees/easy/maxDepth.cpp
@@ -21,14 +21,14 @@ return its depth = 3.
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if (!root) {
+        if(!root) {
             return 0;
         }
         
         int lh = maxDepth(root->left);
         int rh = maxDepth(root->right);
         
-        return lh>=rh?lh+1:rh+1;
+        return 1 + max(lh, rh);
         
     }
 };
