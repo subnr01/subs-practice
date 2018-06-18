09028dc399f4a0bc253ae4ad23c8e04bc3cc4195
Subbu <subnr01@gmail.com>
2018-06-18 06:16:50 +0530

    Update Closest-easy.cpp

--- leet_code/trees/easy/Closest-easy.cpp
+++ leet_code/trees/easy/Closest-easy.cpp
@@ -8,26 +8,24 @@ Given a non-empty binary search tree and a target value, find the value in the B
 class Solution {
 public:
     int closestValue(TreeNode* root, double target) {
-        int result = root->val;
+        int closest = root->val;
         
-        while(root)
+        while (root)
         {
-            if ((double)root->val == target)
+            if (root->val == target)
             {
                 return root->val;
             }
-            double a = abs(target - (double)result);
-            double b = abs(target - (double)root->val);
-            result = (a<b)?result:root->val;
             
-            if (target > (double)root->val)
+            if (abs(target - root->val) < abs(target - closest))
             {
-                root = root->right;
-            } else {
-                root = root->left;
+                closest = root->val;
             }
+            
+            root = (target > root->val) ? root->right: root->left;
         }
-        return result;
+        
+        return closest;
         
     }
 };
@@ -35,38 +33,6 @@ public:
 
 //RECURSIVE (BUT SLOWER)
 class Solution {
-public:
-    int closestValue(TreeNode* root, double target) {
-        int closest = root->val;
-        findClosest(root, target, closest);
-        return closest;
-    }
-    
-    void findClosest(TreeNode *root, double target, int &closest)
-    {
-        if (!root) {
-            return;
-        }
-        
-        if (target == root->val) {
-            closest = root->val;
-            return;
-        }
-        
-        
-        if (abs (target - root->val) < abs(target - closest))
-        {
-            closest = root->val;
-        }
-        
-        if (target > root->val)
-        {
-            findClosest(root->right, target, closest);
-        } else {
-            findClosest(root->left, target, closest);
-        }
-    }
-};class Solution {
 public:
     int closestValue(TreeNode* root, double target) {
         int closest = root->val;
@@ -88,8 +54,7 @@ public:
             closest = root->val;
             return;
         }
-        
-        
+       
         if (abs (target - root->val) < abs(target - closest))
         {
             closest = root->val;
