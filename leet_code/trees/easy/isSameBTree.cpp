4283e074a6442dbacb5f842db776b0111b73742c
Subbu <subnr01@gmail.com>
2018-06-19 05:54:14 +0530

    Update isSameBTree.cpp

--- leet_code/trees/easy/isSameBTree.cpp
+++ leet_code/trees/easy/isSameBTree.cpp
@@ -10,27 +10,12 @@ Two binary trees are considered equal if they are structurally identical and the
 
 class Solution {
 public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        if ( !p && !q) {
-            return true;
+    bool isSameTree(TreeNode* p, TreeNode* q) 
+    {
+        if (!p || !q) {
+            return (p == q);
         }
-        
-        if (!p && q ) {
-            return false;
-        }
-        
-        if (p && !q ) {
-            return false;
-        }     
-        
-        if ( p->val != q->val) {
-            return false;
-        }
-        
-        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        return (p->val == q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
         
     }
-    
-    
 };
-
