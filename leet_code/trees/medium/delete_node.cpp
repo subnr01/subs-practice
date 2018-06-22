3bdbead6917237971ac3742bfe545f26462d77b0
Subbu <subnr01@gmail.com>
2018-06-22 09:09:21 +0530

    Update delete_node.cpp

--- leet_code/trees/medium/delete_node.cpp
+++ leet_code/trees/medium/delete_node.cpp
@@ -15,6 +15,47 @@ Note: Time complexity should be O(height of tree).
 
 
 
+
+//iterative, but perfect sln
+class Solution {
+public:
+  TreeNode* deleteNode(TreeNode* root, int key) {
+    if (root == nullptr) return root;
+    if (key > root->val) {
+      root->right = deleteNode(root->right, key);
+    } else if (key < root->val) {
+      root->left = deleteNode(root->left, key);
+    } else {
+      TreeNode* new_root = nullptr;
+      if (root->left == nullptr) {
+        new_root = root->right;
+      } else if (root->right == nullptr) {
+        new_root = root->left;
+      } else {
+        // Find the min node in the right sub tree
+        TreeNode* parent = root;
+        new_root = root->right;
+        while (new_root->left != nullptr) {
+          parent = new_root;
+          new_root = new_root->left;
+        }
+        
+        if (parent != root) {
+          parent->left = new_root->right;
+          new_root->right = root->right;
+        }
+        
+        new_root->left = root->left;      
+      }
+      
+      delete root;
+      return new_root;
+    }
+    
+    return root;
+  }
+}; 
+      
 /* 
 Solution for a binary tree
 */
