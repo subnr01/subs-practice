f1d29d0c509d81ce430a11e102272c22d4599ff9
Subbu <subnr01@gmail.com>
2018-06-22 09:21:24 +0530

    Update delete_node.cpp

--- leet_code/trees/medium/delete_node.cpp
+++ leet_code/trees/medium/delete_node.cpp
@@ -60,7 +60,7 @@ public:
 }; 
       
 /* 
-Solution for a binary tree
+This works for all trees
 */
 class Solution {
 public:
@@ -87,42 +87,3 @@ public:
 
 
 
-/*
-Solution for a binary search tree
-*/
-class Solution {
-public:
-    TreeNode* deleteNode(TreeNode* root, int key) {
-        if (!root) return nullptr;
-        
-        if (root->val > key) {
-            root->left = deleteNode(root->left, key);
-        } 
-        else if (root->val < key) {
-            root->right = deleteNode(root->right, key);
-        }
-        else if (root->val == key) {
-            if (!root->right) {
-                TreeNode* left = root->left;
-                delete root;
-                return left;
-            }
-            else if (!root->left) {
-                TreeNode* right = root->right;
-                delete root;
-                return right;
-            }
-            else {
-                TreeNode* right = root->right;
-                while (right->left)
-                    right = right->left;
-                
-                root->val = right->val;
-                root->right = deleteNode(root->right, right->val);      
-            }
-        }
-        
-        
-        return root;
-    }
-};
