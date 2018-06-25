29066e3a9526db49460b8685b1125c65d9c683d5
Subbu <subnr01@gmail.com>
2018-06-25 06:41:52 +0530

    Update boundaryOfBinaryTree.cpp

--- leet_code/trees/medium/boundaryOfBinaryTree.cpp
+++ leet_code/trees/medium/boundaryOfBinaryTree.cpp
@@ -49,6 +49,80 @@ So order them in anti-clockwise without duplicate nodes we have [1,2,4,7,8,9,10,
 
 */
 
+//DFS Soln
+class Solution {
+public:
+    vector<int> boundaryOfBinaryTree(TreeNode* root) {
+        vector<int>res;
+        if(!root) return res;
+        res.push_back(root->val);
+        vector<int>lefts,leaves,rights;
+        
+        getLeft(root->left,lefts);
+        getLeaves(root->left,leaves);
+        getLeaves(root->right,leaves);
+        getRight(root->right,rights); 
+        reverse(rights.begin(),rights.end());
+        
+        for(auto l:lefts) res.push_back(l);
+        for(auto leave:leaves) res.push_back(leave);
+        for(auto r:rights) res.push_back(r);
+        
+        return res;   
+    }
+private:
+    void getLeft(TreeNode* curr,vector<int>&lefts)
+    {
+        if(!curr|| (curr->left==NULL && curr->right==NULL)) return;
+        lefts.push_back(curr->val);
+        if(curr->left!=NULL) 
+            getLeft(curr->left,lefts);
+        else
+            getLeft(curr->right,lefts);
+    }
+    
+    void getLeaves(TreeNode * curr,vector<int>&leaves)
+    {
+        if(!curr) return;
+        if(curr->left==NULL && curr->right==NULL) leaves.push_back(curr->val);
+        getLeaves(curr->left,leaves);
+        getLeaves(curr->right,leaves);
+    }
+    
+    void getRight(TreeNode*curr,vector<int>&rights)
+    {
+        if(!curr|| (curr->left==NULL && curr->right==NULL)) return;       
+        rights.push_back(curr->val);
+        if(curr->right) getRight(curr->right,rights);
+        else getRight(curr->left,rights);
+    }
+};
+
+//perorder postorder hybrid
+
+class Solution {
+public:
+    vector<int> boundaryOfBinaryTree(TreeNode* root) {
+        vector<int> bounds;
+        if (root) {
+            bounds.push_back(root->val);
+            getBounds(root->left, bounds, true, false);
+            getBounds(root->right, bounds, false, true);
+        }
+        return bounds;
+    }
+
+private:
+    void getBounds(TreeNode* node, vector<int>& res, bool lb, bool rb) {
+        if (!node)  return;
+        if (lb) res.push_back(node->val);
+        if (!lb && !rb && !node->left && !node->right)  res.push_back(node->val);
+        getBounds(node->left, res, lb, rb && !node->right);
+        getBounds(node->right, res, lb && !node->left, rb);
+        if (rb) res.push_back(node->val);
+    }
+};
+
 
 
 
