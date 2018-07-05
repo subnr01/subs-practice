f08ea1b0d5a1ad5518f53cacb6413de546ca3037
Subbu <subnr01@gmail.com>
2018-07-05 08:53:28 +0530

    Update isPalindrome.cpp

--- leet_code/linked_list/easy/isPalindrome.cpp
+++ leet_code/linked_list/easy/isPalindrome.cpp
@@ -10,6 +10,42 @@ Solution
 
 */
 
+//cleaner soln
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if(head==NULL||head->next==NULL)
+            return true;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast->next!=NULL&&fast->next->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        slow->next=reverseList(slow->next);
+        slow=slow->next;
+        while(slow!=NULL){
+            if(head->val!=slow->val)
+                return false;
+            head=head->next;
+            slow=slow->next;
+        }
+        return true;
+    }
+    ListNode* reverseList(ListNode* head) {
+        ListNode* pre=NULL;
+        ListNode* next=NULL;
+        while(head!=NULL){
+            next=head->next;
+            head->next=pre;
+            pre=head;
+            head=next;
+        }
+        return pre;
+    }
+};
+
+
 class Solution {
 private:
 	bool isSameList(ListNode *node0, ListNode *node1) {
