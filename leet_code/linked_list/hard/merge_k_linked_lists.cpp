57347425bfb2dc1a3e7d2f76f4cb008b423bb58b
Subbu <subnr01@gmail.com>
2018-07-05 13:46:55 +0530

    Update merge_k_linked_lists.cpp

--- leet_code/linked_list/hard/merge_k_linked_lists.cpp
+++ leet_code/linked_list/hard/merge_k_linked_lists.cpp
@@ -26,29 +26,50 @@ Implementation: c++
 1.priority_queue: we can only get the top element (from ChiangKaiShrek's solution)
 
 */
-struct compare {
-    bool operator()(const ListNode* l, const ListNode* r) {
-        return l->val > r->val;
+class Solution {
+public:
+   struct compare {
+        bool operator()(const ListNode* l, const ListNode* r) 
+        {
+            return l->val > r->val;
+        }
+    };
+    ListNode *mergeKLists(vector<ListNode *> &lists) 
+    {  
+        priority_queue<ListNode *, vector<ListNode *>, compare> q;
+        for(auto l : lists) 
+        {
+            if(l) 
+            {
+                q.push(l);
+            } 
+        }
+
+        if(q.empty()) 
+        {
+            return NULL;
+        }  
+   
+        ListNode* result = q.top();
+        q.pop();
+        if(result->next) 
+        {
+            q.push(result->next);
+        }
+       
+        ListNode* tail = result;            
+        while(!q.empty()) 
+        {
+            tail->next = q.top();
+            q.pop();
+            tail = tail->next;
+            if(tail->next) {
+                q.push(tail->next);
+            }
+        }
+        return result;
     }
 };
-ListNode *mergeKLists(vector<ListNode *> &lists) { //priority_queue
-    priority_queue<ListNode *, vector<ListNode *>, compare> q;
-    for(auto l : lists) {
-        if(l)  q.push(l);
-    }
-    if(q.empty())  return NULL;
-    ListNode* result = q.top();
-    q.pop();
-    if(result->next) q.push(result->next);
-    ListNode* tail = result;            
-    while(!q.empty()) {
-        tail->next = q.top();
-        q.pop();
-        tail = tail->next;
-        if(tail->next) q.push(tail->next);
-    }
-    return result;
-}
 
 
 
