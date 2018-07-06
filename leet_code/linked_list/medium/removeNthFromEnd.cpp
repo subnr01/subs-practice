5c6b39713dd6cdb1d09684adc9c644b838d87a5b
Subbu <subnr01@gmail.com>
2018-07-06 09:44:59 +0530

    Update removeNthFromEnd.cpp

--- leet_code/linked_list/medium/removeNthFromEnd.cpp
+++ leet_code/linked_list/medium/removeNthFromEnd.cpp
@@ -13,22 +13,25 @@ Try to do this in one pass.
 */
 
 
-class Solution
-{
+class Solution {
 public:
-    ListNode* removeNthFromEnd(ListNode* head, int n)
-    {
-        ListNode** t1 = &head, *t2 = head;
-        for(int i = 1; i < n; ++i)
-        {
-            t2 = t2->next;
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *p = head;
+        ListNode *pre = head;
+        while (n > 0) {
+            if (!p) break;
+            p = p->next;
+            n--;
         }
-        while(t2->next != NULL)
-        {
-            t1 = &((*t1)->next);
-            t2 = t2->next;
+        if (p == NULL) {
+            return head->next;
         }
-        *t1 = (*t1)->next;
+
+        while (p->next != NULL) {
+            p = p->next;
+            pre = pre->next;
+        }
+        pre->next = pre->next->next;
         return head;
     }
 };
