b15976e52313a99c0a583c9f4fb2aaddfbee6a58
Subbu <subnr01@gmail.com>
2018-07-14 14:35:04 -0400

    Update largestBST.cpp

--- leet_code/trees/medium/largestBST.cpp
+++ leet_code/trees/medium/largestBST.cpp
@@ -48,3 +48,26 @@ public:
     }
     
 };
+
+//Another solution
+class Solution {
+public:
+    int largestBSTSubtree(TreeNode* root) {
+        int res = 0, mn = INT_MIN, mx = INT_MAX;
+        isValidBST(root, mn, mx, res);
+        return res;
+    }
+    void isValidBST(TreeNode *root, int &mn, int &mx, int &res) {
+        if (!root) return;
+        int left_n = 0, right_n = 0, left_mn = INT_MIN;
+        int right_mn = INT_MIN, left_mx = INT_MAX, right_mx = INT_MAX;
+        isValidBST(root->left, left_mn, left_mx, left_n);
+        isValidBST(root->right, right_mn, right_mx, right_n);
+        if ((!root->left || root->val > left_mx) && (!root->right || root->val < right_mn)) {
+            res = left_n + right_n + 1;
+            mn = root->left ? left_mn : root->val;
+            mx = root->right ? right_mx : root->val;
+        } else res = max(left_n, right_n);
+    }
+};
+
