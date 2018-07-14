ad1d50e6dcbcd03f2ebc7e5d4e98cac3c3453ded
Subbu <subnr01@gmail.com>
2018-07-14 07:26:21 -0400

    Create equal_tree_partition.cpp

---
+++ leet_code/trees/medium/equal_tree_partition.cpp
@@ -0,0 +1,28 @@
+/*
+
+Given a binary tree with n nodes, your task is to check if it's possible to partition 
+the tree to two trees which have the equal sum of values after removing exactly one edge on the original tree.
+
+Example 1:
+Input:     
+    5
+   / \
+  10 10
+    /  \
+   2   3
+
+Output: True
+Explanation: 
+    5
+   / 
+  10
+      
+Sum: 15
+
+   10
+  /  \
+ 2    3
+
+Sum: 15
+*/
+
