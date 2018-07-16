feda8863621e47d378d0dbc081569b0801d18948
Subbu <subnr01@gmail.com>
2018-07-16 04:34:55 -0400

    Update findReplaceString.cpp

--- leet_code/strings/medium/new/findReplaceString.cpp
+++ leet_code/strings/medium/new/findReplaceString.cpp
@@ -23,3 +23,27 @@ Explanation: "ab" starts at index 0 in S, so it's replaced by "eee".
 
 
 */
+
+class Solution {
+public:
+   string findReplaceString(string S, vector<int>& indexes, vector<string>& sources, vector<string>& targets) {
+        vector<pair<int, int>> sorted;
+       
+       for (int i = 0 ; i < indexes.size(); i++) {
+            sorted.push_back(make_pair(indexes[i], i));
+        }
+       
+        sort(sorted.rbegin(), sorted.rend());
+       
+        for (auto ind : sorted) {
+            int i = ind.first;
+            string s = sources[ind.second];
+            string t = targets[ind.second];
+            
+            if (S.substr(i, s.length()) == s) {
+                S = S.substr(0, i) + t + S.substr(i + s.length());
+            }
+        }
+        return S;
+    }
+};
