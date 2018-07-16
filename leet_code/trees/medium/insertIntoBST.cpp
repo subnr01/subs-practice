6643631e6d6599c3922ed009a76bb8fd4f5b5067
Subbu <subnr01@gmail.com>
2018-07-16 03:50:01 -0400

    Create insertIntoBST.cpp

---
+++ leet_code/trees/medium/insertIntoBST.cpp
@@ -0,0 +1,24 @@
+/*
+
+Given the root node of a binary search tree (BST) and a value to be inserted 
+into the tree, insert the value into the BST. Return the root node of the BST after the insertion
+
+*/
+
+
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if (!root) {
+            root = new TreeNode(val);
+            return root;
+        }
+        
+        if (val < root->val) {
+            root->left = insertIntoBST(root->left, val);
+        } else {
+            root->right = insertIntoBST(root->right, val);
+        }
+        return root;
+    }
+};
