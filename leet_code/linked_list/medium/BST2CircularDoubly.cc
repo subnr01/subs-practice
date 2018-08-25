f1245524a53be0ee76698e95fd8d4e611a036f9c
Subbu <subnr01@gmail.com>
2018-08-25 19:12:03 -0400

    Update BST2CircularDoubly.cc

--- leet_code/linked_list/medium/BST2CircularDoubly.cc
+++ leet_code/linked_list/medium/BST2CircularDoubly.cc
@@ -1,5 +1,6 @@
 /*
-
+Convert a BST to a sorted circular doubly-linked list in-place. 
+Think of the left and right pointers as synonymous to the previous and next pointers in a doubly-linked list.
 */
 
 //Solution 1 using inorder
