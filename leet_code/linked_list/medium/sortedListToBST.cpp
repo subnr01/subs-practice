538d580fb268f57fcb640d12989520e535af9c5a
Subbu <subnr01@gmail.com>
2018-08-26 21:44:11 -0400

    Update sortedListToBST.cpp

--- leet_code/linked_list/medium/sortedListToBST.cpp
+++ leet_code/linked_list/medium/sortedListToBST.cpp
@@ -15,73 +15,30 @@ One possible answer is: [0,-3,9,-10,null,5], which represents the following heig
 
 
 /*
-
-First solution
+Solution
 */
 
 class Solution {
 public:
-    TreeNode *sortedListToBST(ListNode *head)
-    {
-    	return sortedListToBST( head, NULL );
+    TreeNode* sortedListToBST(ListNode* head) {
+        if (!head) return nullptr;
+        if (!head->next) return new TreeNode(head->val);
+        ListNode* mid = findMid(head);
+        TreeNode* root = new TreeNode(mid->val);
+        root->left = sortedListToBST(head);
+        root->right = sortedListToBST(mid->next);
+        return root;
     }
     
-private:
-    TreeNode *sortedListToBST(ListNode *head, ListNode *tail)
-    {
-    	if( head == tail )
-    		return NULL;
-    	if( head->next == tail )    // 
-    	{	
-    		TreeNode *root = new TreeNode( head->val );
-    		return root;
-    	}
-    	ListNode *mid = head, *temp = head;
-    	while( temp->next != tail && temp->next->next != tail )    // 寻找中间节点
-    	{
-    		mid = mid->next;
-    		temp = temp->next->next;
-    	}
-        cout<<mid->val<<endl;
-    	TreeNode *root = new TreeNode( mid->val );
-    	root->left = sortedListToBST( head, mid );
-    	root->right = sortedListToBST( mid->next, tail );
-    	return root;
-    }
-};
-
-
-
-
-/* 
-Another solution
-*/
-
-class Solution {
-public:
-    ListNode *list;
-    int count(ListNode *node){
-        int size = 0;
-        while (node) {
-            ++size;
-            node = node->next;
+    ListNode* findMid(ListNode* head) {
+        if (!head || !head->next) return head;
+        ListNode *prev = nullptr, *slow = head, *fast = head;
+        while (fast && fast->next) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        return size;
-    }
-    
-    TreeNode *generate(int n){
-        if (n == 0)
-            return NULL;
-        TreeNode *node = new TreeNode(0);
-        node->left = generate(n / 2);
-        node->val = list->val;
-        list = list->next;
-        node->right = generate(n - n / 2 - 1);
-        return node;
-    }
-    
-    TreeNode *sortedListToBST(ListNode *head) {
-        this->list = head;
-        return generate(count(head));
+        prev->next = nullptr; // have to break the linked list apart
+        return slow;
     }
 };
