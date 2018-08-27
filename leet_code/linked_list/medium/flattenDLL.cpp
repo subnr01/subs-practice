b24571f93655b517e678ef74b9d2319cf2900809
Subbu <subnr01@gmail.com>
2018-08-26 20:36:25 -0400

    Update flattenDLL.cpp

--- leet_code/linked_list/medium/flattenDLL.cpp
+++ leet_code/linked_list/medium/flattenDLL.cpp
@@ -96,7 +96,34 @@ public:
 };
 
 
+//Another solution
+class Solution {
+public:
+    Node* flatten(Node* head) {
+        Node *p = head, *nxt;
+        
+        while(p){
+            if(p->child){
+                nxt = p -> next;
+                p->next = flatten(p->child);
+                p->child = NULL;
+                p->next->prev = p;
+                while(p->next){
+                    p = p->next;    
+                }
+                p->next = nxt;
+                if(nxt) {
+                 nxt->prev = p;
+                }
 
+            } else{
+                p = p->next;
+            }
+        }
+        
+        return head;
+    }
+};
 
 
 
