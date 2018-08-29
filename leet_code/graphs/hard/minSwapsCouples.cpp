e27c49697c65c92075e41a4768f5c6e7525bb925
Subbu <subnr01@gmail.com>
2018-08-28 22:17:32 -0400

    Update minSwapsCouples.cpp

--- leet_code/graphs/hard/minSwapsCouples.cpp
+++ leet_code/graphs/hard/minSwapsCouples.cpp
@@ -89,3 +89,32 @@ class Solution {
         return ans;
     }
 }
+
+
+//fastest soln
+class Solution {
+public:
+    int minSwapsCouples(vector<int>& row) {
+        // i's partner is i%2 ? i+1:i-1
+        // i's position is k if row[k] = i
+        int n = row.size();
+        vector<int> pos(n, 0);
+        vector<int> ptn(n, 0);
+        for(int i = 0 ; i < n; ++i){
+            ptn[i] = (i % 2 == 0?i+1:i-1); 
+            pos[row[i]] = i;
+        }
+        
+        int res = 0;
+        for(int i = 0; i < n; ++i){
+            // ptn of (pos of row[i]'s ptn) shoud == i
+            for(int j = ptn[pos[ptn[row[i]]]]; i != j; j = ptn[pos[ptn[row[i]]]]){
+                swap(row[i], row[j]);
+                swap(pos[row[i]], pos[row[j]]);
+                ++res;
+            }
+        }
+        
+        return res;
+    }
+};
