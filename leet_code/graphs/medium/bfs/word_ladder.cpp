e694dd7c36d3d90e762732889fb1701d92ec84d4
Subbu <subnr01@gmail.com>
2018-08-29 11:08:25 -0400

    Update word_ladder.cpp

--- leet_code/graphs/medium/bfs/word_ladder.cpp
+++ leet_code/graphs/medium/bfs/word_ladder.cpp
@@ -32,3 +32,52 @@ private:
         } 
     } 
 };
+
+
+//Another soln
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        
+        unordered_set<string> wordDict(wordList.begin(), wordList.end());
+        if (wordDict.find(endWord) == wordDict.end()) {
+            return 0;
+        }
+  
+        unordered_set<string> head, tail, *phead, *ptail;
+        head.insert(beginWord);
+        tail.insert(endWord);
+        int dist = 2;
+        while (!head.empty() && !tail.empty()) {
+            if (head.size() < tail.size()) {
+                phead = &head;
+                ptail = &tail;
+            }
+            else {
+                phead = &tail; 
+                ptail = &head;
+            }
+            unordered_set<string> temp; 
+            for (auto itr = phead -> begin(); itr != phead -> end(); itr++) {
+                string word = *itr;
+                wordDict.erase(word);
+                for (int p = 0; p < (int)word.length(); p++) {
+                    char letter = word[p];
+                    for (int k = 0; k < 26; k++) {
+                        word[p] = 'a' + k;
+                        if (ptail -> find(word) != ptail -> end())
+                            return dist;
+                        if (wordDict.find(word) != wordDict.end()) {
+                            temp.insert(word);
+                            wordDict.erase(word);
+                        }
+                    }
+                    word[p] = letter;
+                }
+            }
+            dist++;
+            swap(*phead, temp);
+        }
+        return 0; 
+    }
+};
