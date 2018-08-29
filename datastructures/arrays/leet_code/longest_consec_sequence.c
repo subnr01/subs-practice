30c2530c6dc2635eb5629fd3ce85d7c08cf343f1
Subbu <subnr01@gmail.com>
2018-08-28 22:22:24 -0400

    Update longest_consec_sequence.c

--- datastructures/arrays/leet_code/longest_consec_sequence.c
+++ datastructures/arrays/leet_code/longest_consec_sequence.c
@@ -8,7 +8,7 @@ Longest consecutive sequence
 Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
 For example, Given [100, 4, 200, 1, 3, 2], The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
 
-
+//very fast
 int longestConsecutive(const vector<int> &num) {
           unordered_map<int, bool> used;
           for (auto i : num) used[i] = false;
@@ -27,4 +27,35 @@ int longestConsecutive(const vector<int> &num) {
           }
           return longest;
       }
+};
+
+//Using sort instead of map
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        
+        if(nums.size() == 0)
+            return 0;
+        
+        int currentCons = 1;
+        int longestCons = 1;
+        
+        sort(nums.begin(), nums.end());
+        
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] != nums[i+1]){
+                if(nums[i]+1 == nums[i+1])
+                    currentCons ++;
+                else {
+                    if(currentCons > longestCons)
+                        longestCons = currentCons;
+                    currentCons = 1;
+                }
+            }
+        }
+        if(currentCons > longestCons)
+            longestCons = currentCons;
+        
+        return longestCons;
+    }
 };
