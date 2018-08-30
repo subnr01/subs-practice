2e03d0b22ce5fc95645c685440800b617afbaa84
Subbu <subnr01@gmail.com>
2018-08-30 07:05:03 -0400

    Update splitIntoFibonacci.cpp

--- leet_code/strings/medium/new/splitIntoFibonacci.cpp
+++ leet_code/strings/medium/new/splitIntoFibonacci.cpp
@@ -34,3 +34,67 @@ Input: "1101111"
 Output: [110, 1, 111]
 Explanation: The output [11, 0, 11, 11] would also be accepted.
 */
+
+
+class Solution {
+public:
+    vector<int> splitIntoFibonacci(string S) {
+        int n = S.size();
+        for(int i = 1; i < n-2; i++){
+            
+            if(i > 1 && S[0] == '0') {
+                break;
+            }
+            
+            long long first = stoll(S.substr(0, i));
+            if(first > INT_MAX) {
+                break;
+            }
+            
+            for(int j = 1; j < n-i-1; j++)
+            {
+               
+                if(j > 1 && S[i] == '0') {
+                    break;
+                }
+                
+                long long second = stoll(S.substr(i, j));
+                if(second > INT_MAX) {
+                    break;
+                }
+                
+                vector<int> res;
+                res.push_back(first);
+                res.push_back(second);
+                if(isValid(first, second, S, i+j, res)) {
+                    return res;
+                }
+            }
+        }
+        return {};
+    }
+    
+    bool isValid(int first, int second, string &S, int start, vector<int> &res){
+        if (start == S.size()) {
+            return true;
+        }
+        
+        long long sum = first + second;
+        if(sum > INT_MAX) {
+            return false;
+        }
+        
+        string target = to_string(sum);
+        if(S.size()-start < target.size()) {
+            return false;
+        }
+        
+        string curr = S.substr(start, target.size());
+        if(curr != target) {
+            return false;
+        }
+        
+        res.push_back(sum);
+        return isValid(second, sum, S, start+target.size(), res);
+     }
+};
