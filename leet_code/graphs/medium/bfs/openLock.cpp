222d358d27eddbca6abba0e592770c693bbab2e7
Subbu <subnr01@gmail.com>
2018-08-31 15:20:33 -0400

    Update openLock.cpp

--- leet_code/graphs/medium/bfs/openLock.cpp
+++ leet_code/graphs/medium/bfs/openLock.cpp
@@ -6,24 +6,44 @@ class Solution {
 public:
     int openLock(vector<string>& deadends, string target) {
         set<string> dead(deadends.begin(), deadends.end());
-        if (dead.count("0000")) return -1;
-        if (target == "0000") return 0;
-        set<string> v;
+        
+        if (dead.find("0000") != dead.end()) {
+            return -1;
+        }
+        
+        if (target == "0000") {
+            return 0;
+        }
+        
         queue<string> q;
+        unordered_set<string> v;
         q.push("0000");
-        for (int d = 1; !q.empty(); d++) {
-            for (int n = q.size(); n > 0; n--) {
-                string cur = q.front(); q.pop();
-                for (int i = 0; i < 4; i++) {
-                    for (int dif = 1; dif <= 9; dif += 8) {
+        int step = 1;
+        
+        while (!q.empty())
+        {
+            int n = q.size();
+            for (int i = 0; i < n; i++)
+            {
+                string cur = q.front();
+                q.pop();
+                for (int j = 0; j < 4; j++)
+                {
+                    for (int dif = 1; dif <= 9; dif += 8)
+                    {
                         string s = cur;
-                        s[i] = (s[i] - '0' + dif) % 10 + '0';
-                        if (s == target) return d;
-                        if (!dead.count(s) && !v.count(s)) q.push(s);
+                        s[j] = (s[j] - '0' + dif) % 10 + '0';
+                        if (s == target) {
+                            return step;
+                        }
+                        if (!dead.count(s) && !v.count(s)) {
+                            q.push(s);
+                        }
                         v.insert(s);
                     }
                 }
             }
+            step++;
         }
         return -1;
     }
