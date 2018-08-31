7ae9a1419791e0cc26bb2ff964d6f87d30fea6a2
Subbu <subnr01@gmail.com>
2018-08-31 18:03:51 -0400

    Update openLock.cpp

--- leet_code/graphs/medium/bfs/openLock.cpp
+++ leet_code/graphs/medium/bfs/openLock.cpp
@@ -2,6 +2,27 @@
 
 //alexander (closer to the standard soln)
 
+/*
+The solution in brief:
+
+We push a String into the queue and find all its neighbors:
+For example:
+"0000" has 8 neighbors where every digit can have two values "1" or "9".
+We push all the neigbors into the queue: So that is level 1 and so a step:
+
+And we follow again and put neighbors of all these 8 strings, so we have 64 neighbors 
+into the queue. That is step 2.
+
+We also have a visited(hash) and dead(hash) to make sure we do not push
+strings which are in these maps into the queue.
+
+Amdist all these, if we find our target we exit out.
+Every level we increase 8 times for all the elements in the queue, so at
+every level the number of children increases 8 times.
+Every time we cross a level, we increment a step.
+
+*/
+
 class Solution {
 public:
     int openLock(vector<string>& deadends, string target) {
