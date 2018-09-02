f1825ed3761b941b3bc5fc3f0f09d2866b643a49
Subbu <subnr01@gmail.com>
2018-09-02 17:54:40 -0400

    Update crackSafe.cpp

--- leet_code/graphs/hard/search/crackSafe.cpp
+++ leet_code/graphs/hard/search/crackSafe.cpp
@@ -22,4 +22,76 @@ Note: "01100", "10011", "11001" will be accepted too.
 */
 
 
-//THERE ARE SPECIFIC ALGORITHMS, CHECK THEM OUT...
+
+
+//CHECK OUT THE SOLNS
+//Hierholzer's Algorithm and Inverse Burrows-Wheeler Transform
+
+
+
+// Inverse Burrows-Wheeler Transform
+class Solution {
+public:
+    string crackSafe(int n, int k) {
+        // Compute the length. Total length will be s * k
+        int s = 1;
+        for (int i = 0; i < n - 1; ++i)
+            s *= k;
+
+        // Construct the permutation according to wiki
+        std::vector<int> inds(s * k);
+        for (int i = 0; i < k * s; ++i)
+            inds[i] = i / s + (i % s) * k;
+
+        std::string seq;
+        for (int off = 0; off < s * k; ++off)
+        {
+            // Find cycles in the permutation
+            int c = off, tmp = 0;
+            while (inds[c] != -1)
+            {
+                seq.push_back(c / s + '0');
+                tmp = inds[c];
+                inds[c] = -1;
+                c = tmp;
+            }
+        }
+
+        // Need to append n - 1 more characters
+        for (int i = 0; i < n - 1; ++i)
+            seq.push_back(seq[i]);
+        return seq;
+    }
+};
+
+
+
+//DFS????
+class Solution {
+    bool Gene(const int n, const int k, unordered_map<string, bool>&vis, string& ans) {
+        if (vis.size() == pow(k, n)) {
+            return true;
+        }
+        for (int i = 0; i < k; ++i) {
+            ans += '0' + i;
+            string pwd = ans.substr(ans.length() - n);
+            if (vis.find(pwd) == vis.end()) {
+                vis[pwd] = 1;
+                if (Gene(n, k, vis, ans)) {
+                    return true;
+                }
+                vis.erase(pwd);
+            }
+            ans.pop_back();
+        }
+        return false;
+    }
+public:
+    string crackSafe(int n, int k) {
+        string ans = string(n, '0');
+        unordered_map<string, bool>vis;
+        vis[ans] = 1;
+        Gene(n, k, vis, ans);
+        return ans;
+    }
+};
