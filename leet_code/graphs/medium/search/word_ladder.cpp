e180dba8c9f097872aacd89bea69c0f6690a6097
Subbu <subnr01@gmail.com>
2018-09-03 11:55:56 -0400

    Update word_ladder.cpp

--- leet_code/graphs/medium/search/word_ladder.cpp
+++ leet_code/graphs/medium/search/word_ladder.cpp
@@ -1,40 +1,31 @@
-class Solution {
-public:
-    int ladderLength(string beginWord, string endWord, unordered_set<string>& wordDict) {
-        wordDict.insert(endWord);
-        queue<string> toVisit;
-        addNextWords(beginWord, wordDict, toVisit);
-        int dist = 2;
-        while (!toVisit.empty()) {
-            int num = toVisit.size();
-            for (int i = 0; i < num; i++) {
-                string word = toVisit.front();
-                toVisit.pop();
-                if (word == endWord) return dist;
-                addNextWords(word, wordDict, toVisit);
-            }
-            dist++;
-        }
-    }
-private:
-    void addNextWords(string word, unordered_set<string>& wordDict, queue<string>& toVisit) {
-        wordDict.erase(word);
-        for (int p = 0; p < (int)word.length(); p++) {
-            char letter = word[p];
-            for (int k = 0; k < 26; k++) { 
-                word[p] = 'a' + k;
-                if (wordDict.find(word) != wordDict.end()) {
-                    toVisit.push(word);
-                    wordDict.erase(word);
-                }
-            }
-            word[p] = letter;
-        } 
-    } 
-};
 
 
-//Another soln
+/*
+Example 1:
+
+Input:
+beginWord = "hit",
+endWord = "cog",
+wordList = ["hot","dot","dog","lot","log","cog"]
+
+Output: 5
+
+Explanation: As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+return its length 5.
+Example 2:
+
+Input:
+beginWord = "hit"
+endWord = "cog"
+wordList = ["hot","dot","dog","lot","log"]
+
+Output: 0
+
+Explanation: The endWord "cog" is not in wordList, therefore no possible transformation.
+*/
+
+
+//Solution
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
