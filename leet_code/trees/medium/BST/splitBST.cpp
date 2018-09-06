0d6dc1dfa5cceffc00a2efe71cace6642d515617
Subbu <subnr01@gmail.com>
2018-09-06 09:46:33 -0400

    Create splitBST.cpp

---
+++ leet_code/trees/medium/BST/splitBST.cpp
@@ -0,0 +1,25 @@
+/*
+Example 1:
+
+Input: root = [4,2,6,1,3,5,7], V = 2
+Output: [[2,1],[4,3,6,null,null,5,7]]
+Explanation:
+Note that root, output[0], and output[1] are TreeNode objects, not arrays.
+
+The given tree [4,2,6,1,3,5,7] is represented by the following diagram:
+
+          4
+        /   \
+      2      6
+     / \    / \
+    1   3  5   7
+
+while the diagrams for the outputs are:
+
+          4
+        /   \
+      3      6      and    2
+            / \           /
+           5   7         1
+
+*/
