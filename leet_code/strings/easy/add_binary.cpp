c71e590293a9be2dbf45bf01b399aabb4ffef644
Subbu <subnr01@gmail.com>
2018-10-01 08:09:14 -0400

    Update add_binary.cpp

--- leet_code/strings/easy/add_binary.cpp
+++ leet_code/strings/easy/add_binary.cpp
@@ -7,22 +7,19 @@ Return "100"
 */
 
 
-class Solution
-{
+class Solution {
 public:
-    string addBinary(string a, string b)
-    {
-        string s = "";
-        
-        int c = 0, i = a.size() - 1, j = b.size() - 1;
-        while(i >= 0 || j >= 0 || c == 1)
-        {
-            c += i >= 0 ? a[i --] - '0' : 0;
-            c += j >= 0 ? b[j --] - '0' : 0;
-            s = to_string(c % 2 ) + s;
-            c /= 2;
+    string addBinary(string a, string b) {
+        int carry=0;
+        string s="";
+        int i=a.size()-1;
+        int j=b.size()-1;
+        while(i>=0||j>=0||carry==1){
+            carry += i >= 0 ? a[i--]-'0': 0;
+            carry += j >= 0 ? b[j--]-'0':0;
+            s = to_string(carry % 2 ) + s;
+            carry = carry/2;
         }
-        
         return s;
     }
 };
