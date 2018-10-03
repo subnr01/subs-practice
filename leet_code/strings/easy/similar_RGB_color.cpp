8cd6a1ac23f509f465b33d82e11dc3b660fa0676
Subbu <subnr01@gmail.com>
2018-10-03 17:07:21 -0400

    Update similar_RGB_color.cpp

--- leet_code/strings/easy/similar_RGB_color.cpp
+++ leet_code/strings/easy/similar_RGB_color.cpp
@@ -19,3 +19,19 @@ This is the highest among any shorthand color.
 
 */
 
+class Solution {
+public:
+    string helper(string str) 
+    {
+    string dec_to_hex = "0123456789abcdef", res = "";
+
+    int dec_num = stoi(str, nullptr, 16);
+    int q = dec_num / 17;
+    if ((dec_num % 17) > 8) q = q + 1;
+    res = res + dec_to_hex[q] + dec_to_hex[q];
+    return res;
+   }
+
+string similarRGB(string color) {
+    return "#" + helper(color.substr(1, 2)) + helper(color.substr(3, 2)) + helper(color.substr(5, 2));
+}
