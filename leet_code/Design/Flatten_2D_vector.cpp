4709cc8a3cda41c8ab305cc59f6b102c196cd57a
Subbu <subnr01@gmail.com>
2018-10-10 07:07:08 -0400

    Update Flatten_2D_vector.cpp

--- leet_code/Design/Flatten_2D_vector.cpp
+++ leet_code/Design/Flatten_2D_vector.cpp
@@ -14,12 +14,17 @@ How many variables do you need to keep track?
 Two variables is all you need. Try with x and y.
 Beware of empty rows. It could be the first few rows.
 To write correct code, think about the invariant to maintain. What is it?
-The invariant is x and y must always point to a valid point in the 2d vector. Should you maintain your invariant ahead of time or right when you need it?
+The invariant is x and y must always point to a valid point in the 2d vector. 
+Should you maintain your invariant ahead of time or right when you need it?
 Not sure? Think about how you would implement hasNext(). Which is more complex?
 Common logic in two different places should be refactored into a common method.
+
+
 Understand the problem:
 The question itself is very easy to solve. Just several corner cases need to think of:
-  -- What if the 2d vector contains empty arrays, e.g. [ ], [ ], 1 2 3 ? In this case, the next() should not output anything, but the return type is int. There the hasNext() should be more complicated in which it handles this situation. 
+  -- What if the 2d vector contains empty arrays, e.g. [ ], [ ], 1 2 3 ? In this case, 
+     the next() should not output anything, but the return type is int. 
+     There the hasNext() should be more complicated in which it handles this situation. 
   -- What if the 2d vector itself is empty? Again, handle it in hasNext() 
 
 */
@@ -34,14 +39,13 @@ public:
     }
 
     int next() {
-        //hasNext();
+        hasNext();
         return (*i)[j++];
     }
 
     bool hasNext() {
-        if (i != iEnd && j == (*i).size()) {
+        while (i != iEnd && j == (*i).size())
             i++, j = 0;
-        }
         return i != iEnd;
     }
 };
