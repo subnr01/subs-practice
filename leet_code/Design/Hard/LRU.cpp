5aabc759cfd6e57eaa31114ce15cec15948fe53f
Subbu <subnr01@gmail.com>
2018-12-02 12:43:47 -0500

    Update LRU.cpp

--- leet_code/Design/Hard/LRU.cpp
+++ leet_code/Design/Hard/LRU.cpp
@@ -29,6 +29,58 @@ https://discuss.leetcode.com/topic/6812/c-11-code-74ms-hash-table-list
 
 https://discuss.leetcode.com/topic/25792/clean-short-standard-c-solution-not-writing-c-in-c-like-all-other-lengthy-ones
 
+
+class LRUCache{
+public:
+    LRUCache(int capacity):cap(capacity) {
+        
+    }
+    
+    int get(int key) {
+        if(map.find(key) == map.end()) {
+            return -1;
+        }
+        else {
+            //lst.splice(lst.begin(),lst,map[key]);
+            touch(map[key]);
+            return lst.begin()->second;
+        }
+    }
+    
+    void put(int key, int value) {
+        if(map.find(key) != map.end()) {
+            //lst.splice(lst.begin(),lst,map[key]);
+            touch(map[key]);
+            lst.begin()->second = value;
+        }
+        else {
+            if(map.size() == cap) {
+                int dkey = lst.back().first;
+                map.erase(dkey);
+                lst.pop_back();
+            }
+            lst.push_front({key,value});
+            map[key] = lst.begin();
+        }
+    }
+private:
+    typedef std::list<std::pair<int,int>> PList; // PAIR IS THE (KEY,VALUE) PAIR
+    
+    int cap;
+    PList lst;
+    std::unordered_map<int,PList::iterator> map;
+    
+    // UPDATE THE LIST AND THE MAP (AFTER UPDATE, IT WILL BE THE LIST HEAD)
+    void touch(PList::iterator it) {
+        int key = it->first;
+        int value = it->second;
+        lst.erase(it);
+        lst.push_front({key,value});
+        map[key] = lst.begin();
+    }
+};
+
+//Soluiotn uses splice so may not be O(1)
 class LRUCache {
 public:
     LRUCache(int capacity) {
