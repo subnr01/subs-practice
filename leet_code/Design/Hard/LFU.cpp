1eece04f2aa2aad2f0d43588d9859eff5d7f858a
Subbu <subnr01@gmail.com>
2018-12-02 13:20:10 -0500

    Update LFU.cpp

--- leet_code/Design/Hard/LFU.cpp
+++ leet_code/Design/Hard/LFU.cpp
@@ -11,7 +11,8 @@ when there is a tie (i.e., two or more keys that have the same frequency), the l
 
 Follow up:
 Could you do both operations in O(1) time complexity?
-
+*/
+/*
 Example:
 
 LFUCache cache = new LFUCache( 2 /* capacity */ );
@@ -136,8 +137,119 @@ private:
 };
 
 
-//Look at other solutions too
+//multiple hashmap Solution
+class LFUCache {
+    int cap;
+    int size;
+    int minFreq;
+    unordered_map<int, pair<int, int>> m; //key to {value,freq};
+    unordered_map<int, list<int>::iterator> mIter; //key to list iterator;
+    unordered_map<int, list<int>>  fm;  //freq to key list;
+public:
+    LFUCache(int capacity) {
+        cap=capacity;
+        size=0;
+    }
+    
+    int get(int key) {
+        if(m.count(key)==0) return -1;
+        
+        fm[m[key].second].erase(mIter[key]);
+        m[key].second++;
+        fm[m[key].second].push_back(key);
+        mIter[key]=--fm[m[key].second].end();
+        
+        if(fm[minFreq].size()==0 ) 
+              minFreq++;
+        
+        return m[key].first;
+    }
+    
+   void put(int key, int value) {
+        if(cap<=0) return;
+        
+        int storedValue=get(key);
+        if(storedValue!=-1)
+        {
+            m[key].first=value;
+            return;
+        }
+        
+        if(size>=cap )
+        {
+            m.erase( fm[minFreq].front() );
+            mIter.erase( fm[minFreq].front() );
+            fm[minFreq].pop_front();
+            size--;
+        }
+        
+        m[key]={value, 1};
+        fm[1].push_back(key);
+        mIter[key]=--fm[1].end();
+        minFreq=1;
+        size++;
+    }
+};
 
+
+//multiple hashmap faster solution
+class LFUCache {
+public:
+	LFUCache(int capacity) : space(capacity), empty(!capacity) {}
+	int get(int key);
+	void put(int key, int value);
+private:
+	void update(int key);
+	list<list<pair<int, int>>> values;
+	unordered_map<int, list<list<pair<int, int>>>::iterator> node;
+	unordered_map<int, list<pair<int, int>>::iterator> cache;
+	int space;
+	bool empty;
+};
+
+int LFUCache::get(int key) {
+	if (node.find(key) == node.end())
+		return -1;
+	update(key);
+	return cache[key]->second;
+}
+
+void LFUCache::put(int key, int value) {
+	if (empty)
+		return;
+	if (node.find(key) != node.end()) {
+		cache[key]->second = value;
+		update(key);
+		return;
+	}
+	if (space > 0)
+		--space;
+	else {
+		int evicted = (++values.front().begin())->first;
+		node[evicted]->erase(cache[evicted]);
+		if (node[evicted]->size() == 1)
+			values.erase(node[evicted]);
+		node.erase(evicted);
+		cache.erase(evicted);
+	}
+	if (values.size() && values.front().front().first == 1)
+		node[key] = values.begin();
+	else
+		node[key] = values.insert(values.begin(), list<pair<int, int>>({ { 1, 0 } }));
+	cache[key] = node[key]->insert(node[key]->end(), { key, value });
+}
+
+void LFUCache::update(int key) {
+	int frequency = node[key]->front().first + 1, value = cache[key]->second;
+	node[key]->erase(cache[key]);
+	if (node[key]->size() == 1)
+		node[key] = values.erase(node[key]);
+	else
+		++node[key];
+	if (node[key] == values.end() || node[key]->front().first > frequency)
+		node[key] = values.insert(node[key], list<pair<int, int>>({ { frequency, 0 } }));
+	cache[key] = node[key]->insert(node[key]->end(), { key, value });
+}
 
 
 
